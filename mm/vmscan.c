e5655b6dbf0027cac5445e1758da2beba95178f6
Minchan Kim <minchan@kernel.org>
2013-05-09 16:21:25 +0900

    Bug #1463446 mm: make shrink_page_list with pages work from multiple zones
    [root cause  ] new feature Per process reclaim
    [changes     ] process reclaim
    [side effects] process reclaim
    [self test   ]:
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:Yes
    [confirm dependent commit]:Yes
    [board]:sp9861e_1h10_spsec-userdebug-native
    [test case]:function and monkey test
    [reviewers   ] yuming.han@spreadtrum.com
    [change_type ] feature
    [tag_product ] common
    
    mm: make shrink_page_list with pages work from multiple zones
    
    Shrink_page_list expects all pages come from a same zone
    but it's too limited to use.
    
    This patch removes the dependency so next patch can use
    shrink_page_list with pages from multiple zones.
    
    cherry-picked from kernel/msm-4.14/
    
    Conflicts:
    	mm/vmscan.c
    
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:25
    [vinmenon@codeaurora.org: changes for node based lrus.
    shrink_page_list expects all pages come from same node]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    
    Conflicts:
    	mm/vmscan.c
    Change-Id: I34469b7f0a79f2b79e30e40033ba8b3e1dd5f2d0

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1148,6 +1148,8 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			goto keep;
 
 		VM_BUG_ON_PAGE(PageActive(page), page);
+		if (pgdat)
+			VM_BUG_ON_PAGE(page_pgdat(page) != pgdat, page);
 
 		nr_pages = compound_nr(page);
 
@@ -1368,7 +1370,8 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			 */
 			if (page_is_file_cache(page) &&
 			    (!current_is_kswapd() || !PageReclaim(page) ||
-			     !test_bit(PGDAT_DIRTY, &pgdat->flags))) {
+			    (pgdat &&
+			     !test_bit(PGDAT_DIRTY, &pgdat->flags)))) {
 				/*
 				 * Immediately reclaim when written back.
 				 * Similar in principal to deactivate_page()
