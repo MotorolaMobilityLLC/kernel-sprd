3fe1d543d8a69e40496b29ff019800d46a8bb0d6
Minchan Kim <minchan@kernel.org>
2015-01-09 18:36:55 +0530

    Bug #1070788 mm: prevent to write out dirty page in CMA by may_writepage
    [root cause  ] nr_reclaimed from shrink_page_list error
    [changes     ] shrink_page_list
    [side effects] no side effect
    [self test   ]:<iwhale2, function test>:Pass
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:iwhale2
    [test case]:function and monkey test
    [reviewers   ] yuming.han@spreadtrum.com
    [change_type ] feature
    [tag_product ] common
    
    mm: prevent to write out dirty page in CMA by may_writepage
    
    Now, local variable references in shrink_page_list is
    PAGEREF_RECLAIM_CLEAN as default. It is for preventing to reclaim
    dirty pages when CMA try to migrate pages.
    Strictly speaking, we don't need it because CMA already didn't allow
    to write out by .may_writepage = 0 in reclaim_clean_pages_from_list.
    
    Morever, it has a problem to prevent anonymous pages's swap out when
    we use force_reclaim = true in shrink_page_list(ex, per process reclaim
    can do it)
    
    So this patch makes references's default value to PAGEREF_RECLAIM
    and declare .may_writepage = 0 of scan_control in CMA part to make
    code more clear.
    
    cherry-picked from kernel/msm-4.14/
    
    Change-Id: I5edc3c955d106ecebc4949ce27daf5b7b7a18089
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Reported-by: Minkyung Kim <minkyung88@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:23
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -999,7 +999,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		struct address_space *mapping;
 		struct page *page;
 		int may_enter_fs;
-		enum page_references references = PAGEREF_RECLAIM_CLEAN;
+		enum page_references references = PAGEREF_RECLAIM;
 		bool dirty, writeback;
 
 		cond_resched();
@@ -1404,6 +1404,8 @@ unsigned long reclaim_clean_pages_from_list(struct zone *zone,
 		.gfp_mask = GFP_KERNEL,
 		.priority = DEF_PRIORITY,
 		.may_unmap = 1,
+		/* Doesn't allow to write out dirty page */
+		.may_writepage = 0,
 	};
 	unsigned long ret;
 	struct page *page, *next;
