fb3c7032127117c0890702f4aea1261f3c81070d
Janet Liu <janet.liu@spreadtrum.com>
2018-03-23 21:40:19 +0800

    Bug #764506 soc: sprd: debug: fixed wrong branch because compiler optimise
    [root cause  ] getting cpuid effect CPSR status,
                   but don't have "cc" clobber
    [changes     ] regs_debug.h
    [side effects] no
    [self test   ]:bring up
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:pike2,9853i,sharkl3
    [test case]:bringup
    [reviewers   ] xiongshan.an
    [change_type ] debug_log
    [tag_product ] common
    
    On arm platform, getting cpuid effect CPSR status,
    but don't have "cc" clobber, so that sometimes branch wrong because
    compiler optimise, eg #837736. getting cpuid is not compatible on all
    arm platform, affinity in MPDIR are three parts, cluster/core/MT,
    so change the method of getting cpu id on arm.
    
    Signed-off-by: Janet Liu <janet.liu@spreadtrum.com>
    
    Conflicts:
    	drivers/soc/sprd/debug/last_regs/regs_debug.h
    
    Change-Id: I7a3a69645fbc0690874a703452292fd7bc234d57

--- drivers/soc/sprd/debug/last_regs/regs_debug.h
+++ drivers/soc/sprd/debug/last_regs/regs_debug.h
@@ -15,7 +15,7 @@ struct sprd_debug_regs_access {
 	u32 value;
 };
 #ifdef CONFIG_ARM64
-#define sprd_debug_regs_read_start(a) ({  \
+#define sprd_debug_regs_read_start(a) ({	\
 	unsigned long cpu_id, stack, lr;	\
 	asm volatile(				\
 		"	mov %1, sp\n"		\
@@ -23,7 +23,7 @@ struct sprd_debug_regs_access {
 		: "=&r" (lr),			\
 		  "=&r" (stack)			\
 		:				\
-		: "memory");			\
+		: "memory", "cc");		\
 	if (sprd_debug_last_regs_access) {	\
 		cpu_id = raw_smp_processor_id();			\
 		sprd_debug_last_regs_access[cpu_id].value = 0;		\
@@ -31,9 +31,10 @@ struct sprd_debug_regs_access {
 		sprd_debug_last_regs_access[cpu_id].stack = stack;	\
 		sprd_debug_last_regs_access[cpu_id].pc = lr;		\
 		sprd_debug_last_regs_access[cpu_id].status = 0; }	\
+	barrier();							\
 	})
 
-#define sprd_debug_regs_write_start(v, a)	({		\
+#define sprd_debug_regs_write_start(v, a)	({	\
 	unsigned long cpu_id, stack, lr;		\
 	asm volatile(					\
 		"	mov %1, sp\n"			\
@@ -41,77 +42,79 @@ struct sprd_debug_regs_access {
 		: "=&r" (lr),				\
 		  "=&r" (stack)				\
 		:					\
-		: "memory");				\
+		: "memory", "cc");			\
 	if (sprd_debug_last_regs_access) {		\
-		cpu_id = raw_smp_processor_id();		\
+		cpu_id = raw_smp_processor_id();	\
 		sprd_debug_last_regs_access[cpu_id].value = (unsigned long)(v);\
 		sprd_debug_last_regs_access[cpu_id].vaddr = (unsigned long)(a);\
 		sprd_debug_last_regs_access[cpu_id].stack = stack;	\
 		sprd_debug_last_regs_access[cpu_id].pc = lr;		\
 		sprd_debug_last_regs_access[cpu_id].status = 0; }	\
+	barrier();							\
 	})
 
-#define sprd_debug_regs_access_done()	({				\
+#define sprd_debug_regs_access_done()	({			\
 	unsigned long cpu_id, lr;				\
 	asm volatile(						\
 		"	ldr %0, [x29, #8]\n"			\
 		: "=&r" (lr)					\
 		:						\
-		: "memory");					\
+		: "memory", "cc");				\
 	if (sprd_debug_last_regs_access) {			\
 		cpu_id = raw_smp_processor_id();			\
 		sprd_debug_last_regs_access[cpu_id].time = jiffies;     \
 		sprd_debug_last_regs_access[cpu_id].status = 1; }	\
+	barrier();							\
 	})
 
 #else
 #ifdef CONFIG_ARM
 #define sprd_debug_regs_read_start(a)	({u32 cpu_id, stack, lr;	\
 	asm volatile(						\
-		"	mrc	p15, 0, %0, c0, c0, 5\n"	\
-		"	ands %0, %0, #0xf\n"			\
-		"	mov %2, r13\n"				\
-		"	mov %1, pc\n"				\
-		: "=&r" (cpu_id), "=&r" (lr), "=&r" (stack)	\
+		"	mov %1, r13\n"				\
+		"	mov %0, pc\n"				\
+		: "=&r" (lr), "=&r" (stack)			\
 		:						\
-		: "memory");					\
+		: "memory", "cc");				\
 	if (sprd_debug_last_regs_access) {			\
+		cpu_id = raw_smp_processor_id();		\
 		sprd_debug_last_regs_access[cpu_id].value = 0;		\
 		sprd_debug_last_regs_access[cpu_id].vaddr = (u32)a;	\
 		sprd_debug_last_regs_access[cpu_id].stack = stack;	\
 		sprd_debug_last_regs_access[cpu_id].pc = lr;		\
 		sprd_debug_last_regs_access[cpu_id].status = 0; }	\
+	barrier();							\
 	})
 
-#define sprd_debug_regs_write_start(v, a)	({			\
+#define sprd_debug_regs_write_start(v, a)	({		\
 	u32 cpu_id, stack, lr;					\
 	asm volatile(						\
-		"	mrc	p15, 0, %0, c0, c0, 5\n"	\
-		"	ands %0, %0, #0xf\n"			\
-		"	mov %2, r13\n"				\
-		"	mov %1, pc\n"				\
-		: "=&r" (cpu_id), "=&r" (lr), "=&r" (stack)	\
+		"	mov %1, r13\n"				\
+		"	mov %0, pc\n"				\
+		: "=&r" (lr), "=&r" (stack)			\
 		:						\
-		: "memory");					\
+		: "memory", "cc");				\
 	if (sprd_debug_last_regs_access) {			\
+		cpu_id = raw_smp_processor_id();                \
 		sprd_debug_last_regs_access[cpu_id].value = (u32)(v);	\
 		sprd_debug_last_regs_access[cpu_id].vaddr = (u32)(a);	\
 		sprd_debug_last_regs_access[cpu_id].stack = stack;	\
 		sprd_debug_last_regs_access[cpu_id].pc = lr;		\
 		sprd_debug_last_regs_access[cpu_id].status = 0; }	\
+	barrier();							\
 	})
 
-#define sprd_debug_regs_access_done()	({u32 cpu_id, lr;		\
+#define sprd_debug_regs_access_done()	({u32 cpu_id, lr;	\
 	asm volatile(						\
-		"	mrc	p15, 0, %0, c0, c0, 5\n"	\
-		"	ands %0, %0, #0xf\n"			\
-		"	mov %1, pc\n"				\
-		: "=&r" (cpu_id), "=&r" (lr)			\
+		"	mov %0, pc\n"				\
+		: "=&r" (lr)					\
 		:						\
-		: "memory");					\
+		: "memory", "cc");				\
 	if (sprd_debug_last_regs_access) {			\
+		cpu_id = raw_smp_processor_id();		\
 		sprd_debug_last_regs_access[cpu_id].time = jiffies;     \
 		sprd_debug_last_regs_access[cpu_id].status = 1; }	\
+	barrier();							\
 	})
 
 #else
