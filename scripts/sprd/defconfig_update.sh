85a9af38141316c08efff686bfaa99b893841000
Orson Zhai <orson.zhai@spreadtrum.com>
2018-05-14 11:46:03 +0800

    Bug #658531 scrips: sprd: Add defconfig checking tool
    [self test   ] lava
    [change_type ] kernel
    
    This script is used for checking consitency rule of sprd defconfig files
    and fix them if any miss order problems are there.
    
    Change-Id: I706cc7e09b0c62046c063235bce7764afdb6f314
    Signed-off-by: Orson Zhai <orson.zhai@spreadtrum.com>

---
+++ scripts/sprd/defconfig_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DEFCONF_ARM64=`find arch/arm64/configs/ -name sprd_\*_defconfig -printf "%f\n"`
+
+export ARCH=arm64
+for def in $DEFCONF_ARM64;do
+	if [ -f arch/arm64/configs/$def ]; then
+		if  make $def ; then
+			if ! diff .config arch/arm64/configs/$def; then
+				echo "ERROR: arm64 defconfig $def miss order"
+				if [ "$1" != "dry" ]; then
+					cp -v .config arch/arm64/configs/$def
+					echo "arm64 defconfig $def updated"
+				fi
+			else
+				echo "arm64 defconfig $def equals"
+			fi
+		else
+			echo "ERROR: make defconfig $def failed"
+			exit 1
+		fi
+	fi
+done
