8490656b6a0812c95630c3b2801fe7c51da7c59f
Qingqing Wang <qingqing.wang@spreadtrum.com>
2018-08-02 17:14:19 +0800

    Bug #890695 i2c: repair I2C can't get enable clock
    [root cause  ] repair I2C can't get enable clock
    [changes     ] i2c-sprd.c
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: yes
    [confirm dependent commit]: yes
    [board]:ums510_haps_native-userdebug
    [test case]: power on is ok
    [reviewers   ]
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Repair I2C can't get enable clock.
    
    Change-Id: I7493986f438f014c7af2661ab824852093a15f64
    Signed-off-by: Qingqing Wang<qingqing.wang@spreadtrum.com>

--- drivers/i2c/busses/i2c-sprd.c
+++ drivers/i2c/busses/i2c-sprd.c
@@ -12,6 +12,7 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
@@ -469,9 +470,10 @@ static int sprd_i2c_clk_init(struct sprd_i2c *i2c_dev)
 
 	i2c_dev->clk = devm_clk_get(i2c_dev->dev, "enable");
 	if (IS_ERR(i2c_dev->clk)) {
-		dev_warn(i2c_dev->dev, "i2c%d can't get the enable clock\n",
-			 i2c_dev->adap.nr);
-		i2c_dev->clk = NULL;
+		if (PTR_ERR(i2c_dev->clk) != -EPROBE_DEFER)
+			dev_err(i2c_dev->dev, "i2c%d can't get the enable clock\n",
+				i2c_dev->adap.nr);
+		return PTR_ERR(i2c_dev->clk);
 	}
 
 	return 0;
@@ -524,7 +526,9 @@ static int sprd_i2c_probe(struct platform_device *pdev)
 	if (i2c_dev->bus_freq != 100000 && i2c_dev->bus_freq != 400000)
 		return -EINVAL;
 
-	sprd_i2c_clk_init(i2c_dev);
+	ret = sprd_i2c_clk_init(i2c_dev);
+	if (ret)
+		return ret;
 	platform_set_drvdata(pdev, i2c_dev);
 
 	ret = clk_prepare_enable(i2c_dev->clk);
@@ -655,8 +659,4 @@ static struct platform_driver sprd_i2c_driver = {
 	},
 };
 
-static int sprd_i2c_init(void)
-{
-	return platform_driver_register(&sprd_i2c_driver);
-}
-arch_initcall_sync(sprd_i2c_init);
+module_platform_driver(sprd_i2c_driver);
