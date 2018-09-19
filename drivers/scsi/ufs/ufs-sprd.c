29713c7e0bf4925cd62637539a18f3c8d8a0e69a
Zhiwei Bi <zhiwei.bi@unisoc.com>
2018-09-19 19:05:49 +0800

    Bug #933757 scsi:ufs:basic codes of sprd ufs driver
    [root cause  ] add sprd ufs driver
    [changes     ] ufs driver
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]:sprdroidp_trunk_k414_dev
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: ud710
    [test case]: none
    [reviewers   ] Hao.Zhou Jason.Wu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    This change adds support for Spreadtrum Communications Inc platforms
    that use UFS driver. And these codes are only the basic ones.
    
    Change-Id: I25c28195124bd2816fcf830f8ccb3a67cf69ff15
    Signed-off-by: Zhiwei Bi <zhiwei.bi@unisoc.com>

---
+++ drivers/scsi/ufs/ufs-sprd.c
@@ -0,0 +1,183 @@
+ /*
+ * Copyright (C) 2018 Spreadtrum Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/time.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+
+#include "ufshcd.h"
+#include "ufshcd-pltfrm.h"
+#include "unipro.h"
+#include "ufs-sprd.h"
+#include "ufshci.h"
+#include "ufs_quirks.h"
+
+static int ufs_sprd_init(struct ufs_hba *hba)
+{
+	struct device *dev = hba->dev;
+	struct ufs_sprd_host *host;
+
+	host = devm_kzalloc(dev, sizeof(*host), GFP_KERNEL);
+	if (!host)
+		return -ENOMEM;
+
+	host->hba = hba;
+	ufshcd_set_variant(hba, host);
+
+	return 0;
+}
+
+static void ufs_sprd_exit(struct ufs_hba *hba)
+{
+	struct device *dev = hba->dev;
+	struct ufs_sprd_host *host = ufshcd_get_variant(hba);
+
+	devm_kfree(dev, host);
+}
+
+static u32 ufs_sprd_get_ufs_hci_version(struct ufs_hba *hba)
+{
+	return UFSHCI_VERSION_21;
+}
+
+static int ufs_sprd_hce_enable_notify(struct ufs_hba *hba,
+				      enum ufs_notify_change_status status)
+{
+	int err = 0;
+
+	switch (status) {
+	case PRE_CHANGE:
+		break;
+	case POST_CHANGE:
+		break;
+	default:
+		dev_err(hba->dev, "%s: invalid status %d\n", __func__, status);
+		err = -EINVAL;
+		break;
+	}
+
+	return err;
+}
+
+static int ufs_sprd_link_startup_notify(struct ufs_hba *hba,
+					enum ufs_notify_change_status status)
+{
+	int err = 0;
+
+	switch (status) {
+	case PRE_CHANGE:
+		break;
+	case POST_CHANGE:
+		break;
+	default:
+		break;
+	}
+
+	return err;
+}
+
+static int ufs_sprd_pwr_change_notify(struct ufs_hba *hba,
+				enum ufs_notify_change_status status,
+				struct ufs_pa_layer_attr *dev_max_params,
+				struct ufs_pa_layer_attr *dev_req_params)
+{
+	int err = 0;
+
+	if (!dev_req_params) {
+		pr_err("%s: incoming dev_req_params is NULL\n", __func__);
+		err = -EINVAL;
+		goto out;
+	}
+
+	switch (status) {
+	case PRE_CHANGE:
+		break;
+	case POST_CHANGE:
+		break;
+	default:
+		err = -EINVAL;
+		break;
+	}
+
+out:
+	return err;
+}
+
+/**
+ * struct ufs_hba_sprd_vops - UFS sprd specific variant operations
+ *
+ * The variant operations configure the necessary controller and PHY
+ * handshake during initialization.
+ */
+static struct ufs_hba_variant_ops ufs_hba_sprd_vops = {
+	.name = "sprd",
+	.init = ufs_sprd_init,
+	.exit = ufs_sprd_exit,
+	.get_ufs_hci_version = ufs_sprd_get_ufs_hci_version,
+	.hce_enable_notify = ufs_sprd_hce_enable_notify,
+	.link_startup_notify = ufs_sprd_link_startup_notify,
+	.pwr_change_notify = ufs_sprd_pwr_change_notify,
+};
+
+/**
+ * ufs_sprd_probe - probe routine of the driver
+ * @pdev: pointer to Platform device handle
+ *
+ * Return zero for success and non-zero for failure
+ */
+static int ufs_sprd_probe(struct platform_device *pdev)
+{
+	int err;
+	struct device *dev = &pdev->dev;
+
+	/* Perform generic probe */
+	err = ufshcd_pltfrm_init(pdev, &ufs_hba_sprd_vops);
+	if (err)
+		dev_err(dev, "ufshcd_pltfrm_init() failed %d\n", err);
+
+	return err;
+}
+
+/**
+ * ufs_sprd_remove - set driver_data of the device to NULL
+ * @pdev: pointer to platform device handle
+ *
+ * Always returns 0
+ */
+static int ufs_sprd_remove(struct platform_device *pdev)
+{
+	struct ufs_hba *hba =  platform_get_drvdata(pdev);
+
+	pm_runtime_get_sync(&(pdev)->dev);
+	ufshcd_remove(hba);
+	return 0;
+}
+
+static const struct of_device_id ufs_sprd_of_match[] = {
+	{ .compatible = "sprd,ufshc"},
+	{},
+};
+
+static struct platform_driver ufs_sprd_pltform = {
+	.probe = ufs_sprd_probe,
+	.remove = ufs_sprd_remove,
+	.shutdown = ufshcd_pltfrm_shutdown,
+	.driver = {
+		.name = "ufshcd-sprd",
+		.of_match_table = of_match_ptr(ufs_sprd_of_match),
+	},
+};
+module_platform_driver(ufs_sprd_pltform);
+
+MODULE_DESCRIPTION("SPRD Specific UFSHCI driver");
+MODULE_LICENSE("GPL v2");
