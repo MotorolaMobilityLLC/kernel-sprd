b640a468513792b8bbd2acebe4b3f8bdcdbb002b
bruce.chen <bruce.chen@unisoc.com>
2018-09-30 16:11:09 +0800

    Bug #944472 WORKAROUND: fs: timerfd: Get the usage counter for power-off alarm
    [root cause  ] Get the usage counter for power-off alarm
    [changes     ] Get the usage counter for power-off alarm
    [side effects] No
    [self test   ]:Yes
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:Yes
    [board]:Sharkl5
    [test case]:N/A
    [reviewers   ] baolin.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    We have Spreadtrum special power-off alarm feature, which means when we set
    one alarm, then power off the system, the system should be power on when the
    power-off alarm is fired.
    
    But on Android P platform, when power off the system, the system will kill
    tasks by sending signals, which will put the timerfd (decrease timerfd counter)
    to release timerfd, and cancel the power-off alarm at the same time. Then the
    power-off alarm feature will be inactived though we have set power-off alarm.
    
    Thus to ensure the power-off alarm feature can work on Android P platfrom, we
    should get the usage counter for power-off alarm when creating the timerfd for
    power-off alarm, that will avoid issuing timerfd_release() to cancel power-off
    alarm since the timerfd usage counter is not zero now.
    
    Change-Id: Id971a1b0cd4eb80be3af53387bd2b631ce3d7c5d
    Signed-off-by: Baolin Wang <baolin.wang@spreadtrum.com>
    Signed-off-by: Mingming Ji <mingming.ji@spreadtrum.com>
    Signed-off-by: Bruce Chen <bruce.chen@unisoc.com>

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -418,8 +418,9 @@ static int timerfd_fget(int fd, struct fd *p)
 
 SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 {
-	int ufd;
+	int ufd, ret;
 	struct timerfd_ctx *ctx;
+	struct fd f;
 
 	/* Check the TFD_* constants for consistency.  */
 	BUILD_BUG_ON(TFD_CLOEXEC != O_CLOEXEC);
@@ -460,8 +461,24 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 
 	ufd = anon_inode_getfd("[timerfd]", &timerfd_fops, ctx,
 			       O_RDWR | (flags & TFD_SHARED_FCNTL_FLAGS));
-	if (ufd < 0)
+	if (ufd < 0) {
 		kfree(ctx);
+		return ufd;
+	}
+	/*
+	 * We need to get the usage counter for power-off alarm in case
+	 * system will put the timerfd file descriptor to cancel power-
+	 * off alarm.
+	 */
+	if (clockid == CLOCK_POWEROFF_WAKE ||
+	    clockid == CLOCK_POWERON_WAKE ||
+	    clockid == CLOCK_POWEROFF_ALARM) {
+		ret = timerfd_fget(ufd, &f);
+		if (ret) {
+			kfree(ctx);
+			return ret;
+		}
+	}
 
 	return ufd;
 }
