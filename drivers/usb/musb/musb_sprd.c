f0df79f75aa85fd5fe38f22d6c3e02a0c16e64f8
Hongbo Li <hongbo.li@unisoc.com>
2018-10-10 21:40:01 +0800

    Bug #898634 usb: musb: Add musb compatible for Roc1
    [root cause  ] add roc1 compatible
    [changes     ] add roc1 compatible
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:roc1
    [test case]:boot
    [reviewers   ] jiayong.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add musb compatible for Roc1 platform.
    
    Change-Id: Ic6c7c790c2ef5ecdb0fc2f9378eceea248624bb7
    Signed-off-by: Hongbo Li <hongbo.li@spreadtrum.com>

--- drivers/usb/musb/musb_sprd.c
+++ drivers/usb/musb/musb_sprd.c
@@ -1043,6 +1043,7 @@ static const struct dev_pm_ops musb_sprd_pm_ops = {
 static const struct of_device_id usb_ids[] = {
 	{ .compatible = "sprd,sharkl3-musb" },
 	{ .compatible = "sprd,sharkl5-musb" },
+	{ .compatible = "sprd,roc1-musb" },
 	{}
 };
 
