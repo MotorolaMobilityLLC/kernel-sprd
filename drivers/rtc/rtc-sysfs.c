e65eddc90f894cf508be643fe815f35acb952cc7
Bruce Chen <bruce.chen@unisoc.com>
2018-10-26 10:29:56 +0800

    Bug #944472 WORKAROUND: rtc: core: Remove rtc_hctosys_ret value condition
    [root cause  ] Getting rtc_hctosys_ret value fail lead to RTC init failed
    [changes     ] Remove rtc_hctosys_ret value condtion
    [side effects] No
    [self test   ]:Yes
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:Yes
    [board]:sharkl5
    [test case]:N/A
    [reviewers   ] baolin.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Since powerdown detection mechanism was added, which means RTC time is
    invalid when RTC controller is power down. Only user setting time can
    make rtc time valid.
    
    But when users want to set RTC time, it will be failed to validate the
    hctosys node by issuing hctosys_show() function, due to the rc_hctosys_ret
    has been set errors when reading RTC time failed in rtc_hctosys() function.
    
    So we should remove rtc_hctosys_ret value conditions as one workaround to
    avoid this issue.
    
    Change-Id: I3611e7c7c2fb2e0a05651ef9fc36ab253e8bb969
    Signed-off-by: Bruce Chen <bruce.chen@unisoc.com>

--- drivers/rtc/rtc-sysfs.c
+++ drivers/rtc/rtc-sysfs.c
@@ -118,9 +118,8 @@ static ssize_t
 hctosys_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 #ifdef CONFIG_RTC_HCTOSYS_DEVICE
-	if (rtc_hctosys_ret == 0 &&
-			strcmp(dev_name(&to_rtc_device(dev)->dev),
-				CONFIG_RTC_HCTOSYS_DEVICE) == 0)
+	if (strcmp(dev_name(&to_rtc_device(dev)->dev),
+		   CONFIG_RTC_HCTOSYS_DEVICE) == 0)
 		return sprintf(buf, "1\n");
 	else
 #endif
