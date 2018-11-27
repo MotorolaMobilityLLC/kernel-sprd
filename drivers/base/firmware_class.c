13008a545270ddff8fc36c4d788cd4db9ea6edac
zhaoxb <zhaoxb@spreadst.com>
2018-11-27 14:55:35 +0800

    Bug #816932 base: add firmware search path from android 9.x
    [root cause  ] maybe cause GPU load firmware error
    [changes     ] maybe cause GPU load firmware error
    [side effects]no
    [self test   ]:no
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl3
    [test case]:boot up is ok
    [reviewers   ] zhaiyf bianfu.bai
    
    Signed-off-by: zhaoxb <zhaoxb@spreadst.com>
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I827e2112e592b307e4f5900b5a6166ada4c7263d
    Signed-off-by: zhaoxb <zhaoxb@spreadst.com>
    (cherry picked from commit 96162ad68f84ef031a110000992edb6bdc0d0e5b)

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -367,6 +367,7 @@ static void fw_free_buf(struct firmware_buf *buf)
 /* direct firmware loading support */
 static char fw_path_para[256];
 static const char * const fw_path[] = {
+	"/vendor/firmware",
 	fw_path_para,
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
