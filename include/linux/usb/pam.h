2883b636e5d668909f7dd652c3e2288411cba3a1
Cheng.Zeng <cheng.zeng@unisoc.com>
2018-12-05 20:31:28 +0800

    Bug #966052 WORKAROUND: usb: pam: Roc1 USB PAMU3 feature support
    [root cause  ] PAMU3 device driver is required
    [changes     ] Add pamu3 driver
    [side effects] No
    [self test   ]:Yes
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:No
    [is there dependence]:No
    [confirm dependent commit]:Yes
    [board]:Roc1
    [test case]:USB tethering data transfer
    [reviewers   ] jiayong.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    To improved network data transfer speed and reduce
    AP system resources cost, PAMU3 hardware is added,
    it should be configured by software.
    
    Change-Id: I794985c98374aeb00d5f3f8db5e074e1cd7d7913
    Signed-off-by: Cheng.Zeng <cheng.zeng@unisoc.com>

---
+++ include/linux/usb/pam.h
@@ -0,0 +1,23 @@
+/*
+ * USB PAM defines
+ *
+ * These APIs may be used by controller or function drivers.
+ */
+
+#ifndef __LINUX_USB_PAM_H
+#define __LINUX_USB_PAM_H
+
+#include <linux/notifier.h>
+#include <linux/usb.h>
+#include <linux/usb/phy.h>
+
+/* associate a type with PAM */
+enum usb_pam_type {
+	USB_PAM_TYPE_UNDEFINED,
+	USB_PAM_TYPE_USB2,
+	USB_PAM_TYPE_USB3,
+};
+
+extern int pamu3_set_netid(u8 netid);
+
+#endif /* __LINUX_USB_PAM_H */
