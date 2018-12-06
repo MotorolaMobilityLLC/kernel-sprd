aa4221ae279e0ac35c22a6c4297e73696c6706d8
Ruifeng Zhang <zhangrf@spreadst.com>
2018-12-06 15:11:04 +0800

    Bug #976804 arm: config: add sharkl5 go 32b defconfig
    [root cause  ] add sharkl5 go 32b defconfig
    [changes     ] add sharkl5 go 32b defconfig
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: ums510-1h10-32b
    [test case]: kernel compile passed
    [reviewers   ] nianfu.bai
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add sharkl5 go 32b defconfig
    
    Change-Id: Ieb9f5ea80851c2c8bb3380feb68c5c1e17610def
    Signed-off-by: Ruifeng Zhang <zhangrf@spreadst.com>

--- scripts/sprd/defconfig_update.sh
+++ scripts/sprd/defconfig_update.sh
@@ -4,7 +4,28 @@ SRC_PATH="`dirname $0`/../.."
 
 cd $SRC_PATH
 
+DEFCONF_ARM=`find arch/arm/configs/ -name sprd_\*_defconfig -printf "%f\n"`
 DEFCONF_ARM64=`find arch/arm64/configs/ -name sprd_\*_defconfig -printf "%f\n"`
+
+export ARCH=arm
+for def in $DEFCONF_ARM;do
+	if [ -f arch/arm/configs/$def ]; then
+		if  make $def ; then
+			if ! diff .config arch/arm/configs/$def; then
+				echo "ERROR: arm defconfig $def miss order"
+				if [ "$1" != "dry" ]; then
+					cp -v .config arch/arm/configs/$def
+					echo "arm defconfig $def updated"
+				fi
+			else
+				echo "arm defconfig $def equals"
+			fi
+		else
+			echo "ERROR: make defconfig $def failed"
+			exit 1
+		fi
+	fi
+done
 
 export ARCH=arm64
 for def in $DEFCONF_ARM64;do
