4bde565fffcfba0193ab798d473a3acc3e229c5a
Jiayong Yang <jiayong.yang@spreadtrum.com>
2018-12-26 14:25:46 +0800

    Bug #989622 WORKAROUND usb: gadget: MTP driver cannot be installed by windows
    [root cause ] The MTP descriptor is not recognized by windows
    [changes ] Change function class, subclass and protocol values
    [side effects] no side effect.
    [self test ]:USB tethering
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums510_1h10
    [test case]: sharkl5:MTP; OK
    [reviewers ] ben.li
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    The bDeviceClass and function Class, SubClass, Protocol attributes
    are not recognized by windows.
    
    Change-Id: Id23155eb2966343db646e4212652c4506558ef39
    Signed-off-by: Jiayong Yang <jiayong.yang@spreadtrum.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1560,6 +1560,7 @@ int
 composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 {
 	struct usb_composite_dev	*cdev = get_gadget_data(gadget);
+	struct usb_configuration        *c = NULL;
 	struct usb_request		*req = cdev->req;
 	int				value = -EOPNOTSUPP;
 	int				status = 0;
@@ -1638,8 +1639,18 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 		case USB_DT_STRING:
 			value = get_string(cdev, req->buf,
 					w_index, w_value & 0xff);
-			if (value >= 0)
+			if (value >= 0) {
 				value = min(w_length, (u16) value);
+			} else if (cdev->desc.bcdDevice == 0xffff) {
+				list_for_each_entry(c, &cdev->configs, list) {
+					list_for_each_entry(f, &c->functions, list) {
+						if (f->req_match &&
+							f->req_match(f, ctrl, false))
+							goto try_fun_setup;
+					}
+				}
+				f = NULL;
+			}
 			break;
 		case USB_DT_BOS:
 			if (gadget_is_superspeed(gadget) ||
