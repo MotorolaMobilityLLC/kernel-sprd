2dcd0057680cc156618092b06182f13fcf4c840a
Lanqing Liu <lanqing.liu@spreadtrum.com>
2018-12-27 09:43:26 +0800

    Bug #990318 hwspinlock: sprd: Add the ROC1 compatible
    [root cause  ] Add the ROC1 compatible.
    [changes     ] sprd_hwspinlock.c
    [side effects]
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:roc1
    [test case]:power on is ok
    [reviewers   ]
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add the ROC1 compatible.
    
    Change-Id: I1943f190e1c4b930aa296d05279c5770f92c0ef0
    Signed-off-by: Lanqing Liu <lanqing.liu@spreadtrum.com>

--- drivers/hwspinlock/sprd_hwspinlock.c
+++ drivers/hwspinlock/sprd_hwspinlock.c
@@ -152,6 +152,7 @@ static int sprd_hwspinlock_remove(struct platform_device *pdev)
 static const struct of_device_id sprd_hwspinlock_of_match[] = {
 	{ .compatible = "sprd,hwspinlock-r3p0", },
 	{ .compatible = "sprd,sharkl5-hwspinlock", },
+	{ .compatible = "sprd,roc1-hwspinlock", },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, sprd_hwspinlock_of_match);
