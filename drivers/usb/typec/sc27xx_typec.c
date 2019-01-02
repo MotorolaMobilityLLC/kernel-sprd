2493029af15290aee3eed6fcaca2475b988535c8
Xiaofeng Yang <xiaofeng.yang@spreadtrum.com>
2019-01-02 15:49:43 +0800

    Bug #871013 usb: typec: Add sc2721 typec driver
    [root cause  ] Add sc2721 typec driver
    [changes     ] drivers/usb/typec/sc27xx_typec.c
    [side effects]No
    [self test   ]:No
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:No
    [board]:sharkl3
    [test case]:usb/typec
    [reviewers   ] jiayong.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add sc2721 typec driver for sharkl3.
    
    Change-Id: Ib8efef2718bc1ab7e766784877199834ef442e01
    Signed-off-by: Xiaofeng Yang <xiaofeng.yang@spreadtrum.com>

--- drivers/usb/typec/sc27xx_typec.c
+++ drivers/usb/typec/sc27xx_typec.c
@@ -18,17 +18,15 @@
 #include <linux/nvmem-consumer.h>
 #include <linux/slab.h>
 
-#define SC27XX_TYPEC_REG(n)			(sc->base + (n))
-
 /* registers definitions for controller REGS_TYPEC */
-#define SC27XX_EN			SC27XX_TYPEC_REG(0x00)
-#define SC27XX_MODE			SC27XX_TYPEC_REG(0x04)
-#define SC27XX_INT_EN			SC27XX_TYPEC_REG(0x0c)
-#define SC27XX_INT_CLR			SC27XX_TYPEC_REG(0x10)
-#define SC27XX_INT_RAW			SC27XX_TYPEC_REG(0x14)
-#define SC27XX_INT_MASK			SC27XX_TYPEC_REG(0x18)
-#define SC27XX_STATUS			SC27XX_TYPEC_REG(0x1c)
-#define SC27XX_RTRIM			SC27XX_TYPEC_REG(0x3c)
+#define SC27XX_EN			0x00
+#define SC27XX_MODE			0x04
+#define SC27XX_INT_EN			0x0c
+#define SC27XX_INT_CLR			0x10
+#define SC27XX_INT_RAW			0x14
+#define SC27XX_INT_MASK			0x18
+#define SC27XX_STATUS			0x1c
+#define SC27XX_RTRIM			0x3c
 
 /* SC27XX_TYPEC MODE */
 #define SC27XX_MODE_SNK			0
@@ -59,6 +57,18 @@
 #define SC27XX_CC1_SHIFT(n)		(5 + (n))
 #define SC27XX_CC2_SHIFT		5
 
+/* sc2721 registers definitions for controller REGS_TYPEC */
+#define SC2721_EN			0x00
+#define SC2721_CLR			0x04
+#define SC2721_MODE			0x08
+
+/* SC2721_INT_EN */
+#define SC2721_ATTACH_INT_EN		BIT(5)
+#define SC2721_DETACH_INT_EN		BIT(6)
+
+#define SC2721_STATE_MASK		GENMASK(3, 0)
+#define SC2721_EVENT_MASK		GENMASK(6, 0)
+
 enum sc27xx_typec_connection_state {
 	SC27XX_DETACHED_SNK,
 	SC27XX_ATTACHWAIT_SNK,
@@ -80,6 +90,39 @@ enum sc27xx_typec_connection_state {
 	SC27XX_ORIENTED_DEBUG,
 };
 
+struct sprd_typec_variant_data {
+	u32 efuse_shift;
+	u32 int_en;
+	u32 int_clr;
+	u32 mode;
+	u32 attach_en;
+	u32 detach_en;
+	u32 state_mask;
+	u32 event_mask;
+};
+
+static const struct sprd_typec_variant_data sharkl5_data = {
+	.efuse_shift = SC2730_EFUSE_SHIFT,
+	.int_en = SC27XX_INT_EN,
+	.int_clr = SC27XX_INT_CLR,
+	.mode = SC27XX_MODE,
+	.attach_en = SC27XX_ATTACH_INT_EN,
+	.detach_en = SC27XX_DETACH_INT_EN,
+	.state_mask = SC27XX_STATE_MASK,
+	.event_mask = SC27XX_EVENT_MASK,
+};
+
+static const struct sprd_typec_variant_data sharkl3_data = {
+	.efuse_shift = SC27XX_EFUSE_SHIFT_DEFAULT,
+	.int_en = SC2721_EN,
+	.int_clr = SC2721_CLR,
+	.mode = SC2721_MODE,
+	.attach_en = SC2721_ATTACH_INT_EN,
+	.detach_en = SC2721_DETACH_INT_EN,
+	.state_mask = SC2721_STATE_MASK,
+	.event_mask = SC2721_EVENT_MASK,
+};
+
 struct sc27xx_typec {
 	struct device *dev;
 	struct regmap *regmap;
@@ -92,6 +135,7 @@ struct sc27xx_typec {
 	struct typec_port *port;
 	struct typec_partner *partner;
 	struct typec_capability typec_cap;
+	const struct sprd_typec_variant_data *var_data;
 };
 
 static int sc27xx_typec_connect(struct sc27xx_typec *sc, u32 status)
@@ -196,17 +240,17 @@ static irqreturn_t sc27xx_typec_interrupt(int irq, void *data)
 	u32 event;
 	int ret;
 
-	ret = regmap_read(sc->regmap, SC27XX_INT_MASK, &event);
+	ret = regmap_read(sc->regmap, sc->base + SC27XX_INT_MASK, &event);
 	if (ret)
 		return ret;
 
-	event &= SC27XX_EVENT_MASK;
+	event &= sc->var_data->event_mask;
 
-	ret = regmap_read(sc->regmap, SC27XX_STATUS, &sc->state);
+	ret = regmap_read(sc->regmap, sc->base + SC27XX_STATUS, &sc->state);
 	if (ret)
 		goto clear_ints;
 
-	sc->state &= SC27XX_STATE_MASK;
+	sc->state &= sc->var_data->state_mask;
 
 	if (event & SC27XX_ATTACH_INT) {
 		ret = sc27xx_typec_connect(sc, sc->state);
@@ -217,7 +261,7 @@ static irqreturn_t sc27xx_typec_interrupt(int irq, void *data)
 	}
 
 clear_ints:
-	regmap_write(sc->regmap, SC27XX_INT_CLR, event);
+	regmap_write(sc->regmap, sc->base + sc->var_data->int_clr, event);
 
 	dev_info(sc->dev, "now works as DRP and is in %d state, event %d\n",
 		sc->state, event);
@@ -230,7 +274,7 @@ static int sc27xx_typec_enable(struct sc27xx_typec *sc)
 	u32 val;
 
 	/* Set typec mode */
-	ret = regmap_read(sc->regmap, SC27XX_MODE, &val);
+	ret = regmap_read(sc->regmap, sc->base + sc->var_data->mode, &val);
 	if (ret)
 		return ret;
 
@@ -249,17 +293,17 @@ static int sc27xx_typec_enable(struct sc27xx_typec *sc)
 		return -EINVAL;
 	}
 
-	ret = regmap_write(sc->regmap, SC27XX_MODE, val);
+	ret = regmap_write(sc->regmap, sc->base + sc->var_data->mode, val);
 	if (ret)
 		return ret;
 
 	/* Enable typec interrupt and enable typec */
-	ret = regmap_read(sc->regmap, SC27XX_INT_EN, &val);
+	ret = regmap_read(sc->regmap, sc->base + sc->var_data->int_en, &val);
 	if (ret)
 		return ret;
 
-	val |= SC27XX_ATTACH_INT_EN | SC27XX_DETACH_INT_EN;
-	return regmap_write(sc->regmap, SC27XX_INT_EN, val);
+	val |= sc->var_data->attach_en | sc->var_data->detach_en;
+	return regmap_write(sc->regmap, sc->base + sc->var_data->int_en, val);
 }
 
 static const u32 sc27xx_typec_cable[] = {
@@ -302,7 +346,7 @@ static int typec_set_rtrim(struct sc27xx_typec *sc, unsigned long efuse_offset)
 	rtrim = (calib_data & SC27XX_CC1_MASK(efuse_offset)) >> SC27XX_CC1_SHIFT(efuse_offset);
 	rtrim |= ((calib_data & SC27XX_CC2_MASK(efuse_offset)) >> efuse_offset) << SC27XX_CC2_SHIFT;
 
-	return regmap_write(sc->regmap, SC27XX_RTRIM, rtrim);
+	return regmap_write(sc->regmap, sc->base + SC27XX_RTRIM, rtrim);
 }
 
 static int sc27xx_typec_probe(struct platform_device *pdev)
@@ -310,10 +354,15 @@ static int sc27xx_typec_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct device_node *node = pdev->dev.of_node;
 	struct sc27xx_typec *sc;
-	unsigned long data;
+	const struct sprd_typec_variant_data *pdata;
 	int ret;
 
-	data = (unsigned long)of_device_get_match_data(dev);
+	pdata = of_device_get_match_data(dev);
+	if (!pdata) {
+		dev_err(&pdev->dev, "No matching driver data found\n");
+		return -EINVAL;
+	}
+
 	sc = devm_kzalloc(&pdev->dev, sizeof(*sc), GFP_KERNEL);
 	if (!sc)
 		return -ENOMEM;
@@ -349,6 +398,7 @@ static int sc27xx_typec_probe(struct platform_device *pdev)
 		return ret;
 	}
 
+	sc->var_data = pdata;
 	sc->typec_cap.type = TYPEC_PORT_DRP;
 	sc->typec_cap.dr_set = sc27xx_typec_dr_set;
 	sc->typec_cap.pr_set = sc27xx_typec_pr_set;
@@ -361,7 +411,7 @@ static int sc27xx_typec_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	ret = typec_set_rtrim(sc, data);
+	ret = typec_set_rtrim(sc, sc->var_data->efuse_shift);
 	if (ret < 0) {
 		dev_err(sc->dev, "failed to set typec rtrim %d\n", ret);
 		goto error;
@@ -399,7 +449,8 @@ static int sc27xx_typec_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id typec_sprd_match[] = {
-	{.compatible = "sprd,sc2730-typec", .data = (void *)SC2730_EFUSE_SHIFT},
+	{.compatible = "sprd,sc2730-typec", .data = &sharkl5_data},
+	{.compatible = "sprd,sc2721-typec", .data = &sharkl3_data},
 	{},
 };
 MODULE_DEVICE_TABLE(of, typec_sprd_match);
