2d93eadc55266c6ae32163f5e1c8c15d4c17e37e
Lei Ning <lei.ning@unisoc.com>
2019-01-08 15:54:32 +0800

    Bug #996596 asoc: sprd: init iis tx/rx lr mode
    [root cause  ] iis tx lr mode set default val
    [changes     ] iis tx lr mode set default val
    [side effects] no
    [self test   ]: sound for wired headphone and typec usb are not inverted
    [download normally]:ok
    [power on/off normally]:ok
    [do common repository/branch inspection]: ok
    [is there dependence]:no
    [confirm dependent commit]:ok
    [board]:ums510_1h10
    [test case]: sound for wired headphone and typec usb are not inverted
    [reviewers   ] peter.huang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    iis tx/rx lr mode set default val.
    iis tx/rx lr mode must be initialized, otherwise dsp will get wrong vals.
    
    Change-Id: I2a8993b5fcf5f81ba9030264dbda45e6f15c341c
    Signed-off-by: Lei Ning <lei.ning@unisoc.com>

--- sound/soc/sprd/dai/vbc/v4/vbc-codec.c
+++ sound/soc/sprd/dai/vbc/v4/vbc-codec.c
@@ -3614,6 +3614,21 @@ static void init_vbc_codec_data(struct vbc_codec_priv *vbc_codec)
 	vbc_codec->iis_rx_wd[VBC_MUX_IIS_RX_ADC2].value = WD_16BIT;
 	vbc_codec->iis_rx_wd[VBC_MUX_IIS_RX_ADC3].id = VBC_MUX_IIS_RX_ADC3;
 	vbc_codec->iis_rx_wd[VBC_MUX_IIS_RX_ADC3].value = WD_16BIT;
+	/* iis lr mod */
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC0].id = VBC_MUX_IIS_TX_DAC0;
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC0].value = RIGHT_HIGH;
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC1].id = VBC_MUX_IIS_TX_DAC1;
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC1].value = RIGHT_HIGH;
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC2].id = VBC_MUX_IIS_TX_DAC2;
+	vbc_codec->iis_tx_lr_mod[VBC_MUX_IIS_TX_DAC2].value = RIGHT_HIGH;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC0].id = VBC_MUX_IIS_RX_ADC0;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC0].value = LEFT_HIGH;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC1].id = VBC_MUX_IIS_RX_ADC1;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC1].value = LEFT_HIGH;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC2].id = VBC_MUX_IIS_RX_ADC2;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC2].value = LEFT_HIGH;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC3].id = VBC_MUX_IIS_RX_ADC3;
+	vbc_codec->iis_rx_lr_mod[VBC_MUX_IIS_RX_ADC3].value = LEFT_HIGH;
 	/* default vbc iis2 connect to aon usb audio */
 	vbc_codec->vbc_iis_inf_sys_sel = 1;
 
