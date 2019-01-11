6c8dbf43b854703ba720fbe1fa7e10e4b3ea2c39
junqiang.wang <junqiang.wang@unisoc.com>
2019-01-11 20:39:13 +0800

    Bug #999252 soc:sprd:merge kernel4.14 dmc_drv support
    [root cause  ] dmc_drv merge from k4.4
    [changes     ] new drv
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums510_native_userdebug
    [test case]:cat /proc/sprd_dmc directory
    [reviewers   ] mingmin.ling/yuelin.tang/suwen.chong
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add dmc_drv for follow reasons:
    1.Some customer need get the external DDR device's
      capacity and MR registers from file system, this driver add
      a proc file interface for this request.
    2.Add DDR status statistics for power optimize, for example
      a) Calculate the DDR read/write/self-fresh/light sleep time.
      b) Calculate each frequency running time, etc.
    
    Change-Id: Ia69be9d14419a3e5861fbe9ae2ef5fc973f2df32
    Signed-off-by: junqiang.wang <junqiang.wang@unisoc.com>

---
+++ drivers/soc/sprd/pub/dmc_drv.c
@@ -0,0 +1,448 @@
+/*copyright (C) 2019 Spreadtrum Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+#include <linux/io.h>
+#include <linux/mfd/syscon.h>
+#include <linux/of.h>
+#include <linux/regmap.h>
+#include <linux/of.h>
+#include <linux/debugfs.h>
+#include <linux/uaccess.h>
+#include <linux/platform_device.h>
+#include <linux/of_device.h>
+
+#define PUB_MONITOR_CYCLE_TO_NS		(1000 / 128)	/* 128MHz */
+#define PUB_DFS_MONITOR_CYCLE_TO_NS	(10000 / 65)	/* 6.5MHz */
+#define PUB_STATUS_MON_CTRL_OFFSET	0x0
+#define DDR_MAX_SUPPORT_CS_NUM		2
+#define DMC_PROC_NAME "sprd_dmc"
+#define DDR_PROPERTY_NAME "property"
+#define DDR_INFO_NAME "ddr_info"
+/*
+ * new requirement: all register value is clear to 0,
+ * when enable signle change from 0 to 1;
+ */
+struct pub_monitor_dbg {
+	u32 pub_status_mon_ctrl;
+	u32 idle_time;		/* clk 128M */
+	u32 write_time;		/* clk 128M */
+	u32 read_time;		/* clk 128M */
+	u32 sref_time;		/* clk 128M */
+	u32 light_time;		/* clk 6.5M */
+	u16 light_cnt;
+	u16 sref_cnt;
+	u32 fx_time[8];
+	u32 dfs_cnt;
+};
+
+struct dmc_data {
+	u32 size_l_offset;
+	u32 size_h_offset;
+	u32 type_offset;
+	u32 mr_offset[DDR_MAX_SUPPORT_CS_NUM];
+};
+
+struct dmc_drv_data {
+	struct pub_monitor_dbg new_val;
+	struct pub_monitor_dbg old_val;
+	struct proc_dir_entry *proc_dir;
+	struct dentry *monitor_dir;
+	struct proc_dir_entry *property;
+	struct proc_dir_entry *info;
+	void __iomem *mon_base;
+	int pub_mon_enabled;
+	u32 type;
+	u32 mr_val[DDR_MAX_SUPPORT_CS_NUM];
+	u64 size;
+};
+
+#define SHARKL3_SIZE_L_OFFSET	0x1c4
+#define SHARKL3_SIZE_H_OFFSET	0x1c8
+#define SHARKL3_TYPE_OFFSET	0x1b8
+#define SHARKL3_CS0_MR_OFFSET	0x1b0
+#define SHARKL3_CS1_MR_OFFSET	0x1b4
+
+#define SHARKL5_SIZE_L_OFFSET	0x0
+#define SHARKL5_SIZE_H_OFFSET	0x4
+#define SHARKL5_TYPE_OFFSET	0x8
+#define SHARKL5_CS0_MR_OFFSET	0xc
+#define SHARKL5_CS1_MR_OFFSET	0x10
+
+static const struct dmc_data sharkl3_data = {
+	.size_l_offset = SHARKL3_SIZE_L_OFFSET,
+	.size_h_offset = SHARKL3_SIZE_H_OFFSET,
+	.type_offset = SHARKL3_TYPE_OFFSET,
+	.mr_offset = {
+		SHARKL3_CS0_MR_OFFSET,
+		SHARKL3_CS1_MR_OFFSET,
+	},
+};
+
+static const struct dmc_data sharkl5_data = {
+	.size_l_offset = SHARKL5_SIZE_L_OFFSET,
+	.size_h_offset = SHARKL5_SIZE_H_OFFSET,
+	.type_offset = SHARKL5_TYPE_OFFSET,
+	.mr_offset = {
+		SHARKL5_CS0_MR_OFFSET,
+		SHARKL5_CS1_MR_OFFSET,
+	},
+};
+
+static struct dmc_drv_data drv_data;
+
+static const char *const ddr_type_to_str[] = {
+	"Invalid",
+	"LPDDR1",
+	"LPDDR2",
+	"LPDDR3",
+	"LPDDR4",
+	"LPDDR4X",
+	"LPDDR4Y",
+	"LPDDR5",
+};
+
+#ifdef CONFIG_PROC_FS
+static int sprd_ddr_size_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%llu\n", (long long int)(drv_data.size / 1024 / 1024));
+	return 0;
+}
+
+static int sprd_ddr_size_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, sprd_ddr_size_show, PDE_DATA(inode));
+}
+
+static const struct file_operations sprd_ddr_size_fops = {
+	.open = sprd_ddr_size_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int sprd_ddr_info_show(struct seq_file *m, void *v)
+{
+	int cs_num, i, type;
+
+	type = (drv_data.type & 0x0f);
+	if (type < ARRAY_SIZE(ddr_type_to_str))
+		seq_printf(m, "%s\n", ddr_type_to_str[type]);
+	else
+		return -EINVAL;
+
+	cs_num = (drv_data.type >> 4) & 0x0f;
+	if (cs_num > DDR_MAX_SUPPORT_CS_NUM)
+		cs_num = DDR_MAX_SUPPORT_CS_NUM;
+	seq_printf(m, "CS_NUM:%d\n", cs_num);
+
+	for (i = 0; i < cs_num; i++) {
+		seq_printf(m,
+			   "CS%d MR Value: MR5=0x%x,MR6=0x%x, MR7=0x%x,MR8=0x%x\n",
+			   i, (drv_data.mr_val[i] & 0xff),
+			   ((drv_data.mr_val[i] >> 8) & 0xff),
+			   ((drv_data.mr_val[i] >> 16) & 0xff),
+			   ((drv_data.mr_val[i] >> 24) & 0xff));
+	}
+	return 0;
+}
+
+static int sprd_ddr_info_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, sprd_ddr_info_show, PDE_DATA(inode));
+}
+
+static const struct file_operations sprd_ddr_info_fops = {
+	.open = sprd_ddr_info_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int sprd_ddr_proc_creat(void)
+{
+	drv_data.proc_dir = proc_mkdir(DMC_PROC_NAME, NULL);
+	if (!drv_data.proc_dir)
+		return -ENOMEM;
+
+	drv_data.property = proc_create_data(DDR_PROPERTY_NAME, 0444,
+					     drv_data.proc_dir,
+					     &sprd_ddr_size_fops, NULL);
+	if (!drv_data.property) {
+		remove_proc_entry(DMC_PROC_NAME, NULL);
+		return -ENOMEM;
+	}
+	drv_data.info = proc_create_data(DDR_INFO_NAME, 0444,
+					 drv_data.proc_dir, &sprd_ddr_info_fops,
+					 NULL);
+	if (!drv_data.info) {
+		remove_proc_entry(DDR_PROPERTY_NAME, drv_data.proc_dir);
+		remove_proc_entry(DMC_PROC_NAME, NULL);
+		return -ENOMEM;
+	}
+	return 0;
+}
+#endif
+
+static int sprd_pub_monitor_reg_get(void)
+{
+	u32 *ptr = (u32 *) &drv_data.new_val;
+	int i;
+
+	for (i = 0; i < sizeof(drv_data.new_val) / sizeof(u32); i++) {
+		*ptr = readl_relaxed(drv_data.mon_base + i * 4);
+		ptr++;
+	}
+	return 0;
+}
+
+static u64 time_diff_get(u64 new_time, u64 old_time, u64 overflow_offset,
+			 u64 period)
+{
+	if (old_time > new_time)
+		return (new_time + overflow_offset - old_time) * period;
+
+	return (new_time - old_time) * period;
+}
+
+static int sprd_pub_monitor_status_show(struct seq_file *m, void *v)
+{
+	int i;
+	u64 idle_time, write_time, read_time, sref_time, light_time;
+	u64 fx_time[8];
+	u32 light_cnt, sref_cnt;
+
+	if (!drv_data.mon_base)
+		return -ENOMEM;
+
+	if (!drv_data.pub_mon_enabled)
+		return -ENODATA;
+
+	if (sprd_pub_monitor_reg_get())
+		return -ENODATA;
+	idle_time =
+	    time_diff_get(drv_data.new_val.idle_time,
+			  drv_data.old_val.idle_time, 0x100000000ULL,
+			  PUB_MONITOR_CYCLE_TO_NS);
+	write_time =
+	    time_diff_get(drv_data.new_val.write_time,
+			  drv_data.old_val.write_time, 0x100000000ULL,
+			  PUB_MONITOR_CYCLE_TO_NS);
+	read_time =
+	    time_diff_get(drv_data.new_val.read_time,
+			  drv_data.old_val.read_time, 0x100000000ULL,
+			  PUB_MONITOR_CYCLE_TO_NS);
+	sref_time =
+	    time_diff_get(drv_data.new_val.sref_time,
+			  drv_data.old_val.sref_time, 0x100000000ULL,
+			  PUB_MONITOR_CYCLE_TO_NS);
+	light_time =
+	    time_diff_get(drv_data.new_val.light_time,
+			  drv_data.old_val.light_time, 0x100000000ULL,
+			  PUB_MONITOR_CYCLE_TO_NS);
+	light_cnt =
+	    time_diff_get(drv_data.new_val.light_cnt,
+			  drv_data.old_val.light_cnt, 0x10000UL,
+			  PUB_DFS_MONITOR_CYCLE_TO_NS);
+	sref_cnt =
+	    time_diff_get(drv_data.new_val.sref_cnt, drv_data.old_val.sref_cnt,
+			  0x10000UL, PUB_MONITOR_CYCLE_TO_NS);
+
+	for (i = 0; i < 8; i++) {
+		fx_time[i] =
+		    drv_data.new_val.fx_time[i] * PUB_DFS_MONITOR_CYCLE_TO_NS;
+	}
+	memcpy(&drv_data.old_val, &drv_data.new_val, sizeof(drv_data.old_val));
+
+	seq_printf(m, "idle_time: %llu ns\n", idle_time);
+	seq_printf(m, "write_time: %llu ns\n", write_time);
+	seq_printf(m, "read_time: %llu ns\n", read_time);
+	seq_printf(m, "sref_time: %llu ns\n", sref_time);
+	seq_printf(m, "light_time: %llu ns\n", light_time);
+	seq_printf(m, "light_cnt: %d\n", light_cnt);
+	seq_printf(m, "sref_cnt: %d\n", sref_cnt);
+
+	for (i = 0; i < 8; i++)
+		seq_printf(m, "F%d time: %llu ns\n", i, fx_time[i]);
+	seq_printf(m, "dfs_cnt: %d\n", (drv_data.new_val.dfs_cnt & 0x3ff));
+	return 0;
+}
+
+static int sprd_pub_monitor_status_open(struct inode *inodep,
+					struct file *filep)
+{
+	return single_open(filep, sprd_pub_monitor_status_show, NULL);
+}
+
+static const struct file_operations pub_monitor_status_fops = {
+	.open = sprd_pub_monitor_status_open,
+	.read = seq_read,
+	.write = NULL,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int sprd_pub_monitor_enable_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%d\n", drv_data.pub_mon_enabled);
+	return 0;
+}
+
+static int sprd_pub_monitor_enable_open(struct inode *inodep,
+					struct file *filep)
+{
+	return single_open(filep, sprd_pub_monitor_enable_show, NULL);
+}
+
+static int sprd_pub_monitor_enable(int enable)
+{
+	if (!drv_data.mon_base)
+		return -ENOMEM;
+	if (enable) {
+		writel_relaxed(0x6,
+			       drv_data.mon_base + PUB_STATUS_MON_CTRL_OFFSET);
+		writel_relaxed(0x307,
+			       drv_data.mon_base + PUB_STATUS_MON_CTRL_OFFSET);
+	} else {
+		writel_relaxed(0x0,
+			       drv_data.mon_base + PUB_STATUS_MON_CTRL_OFFSET);
+	}
+	return 0;
+}
+
+static ssize_t sprd_pub_monitor_enable_write(struct file *filep,
+					     const char __user *buf,
+					     size_t len, loff_t *ppos)
+{
+	char buf_tmp[8];
+	int ret;
+
+	if (len < 1)
+		return -EINVAL;
+	if (copy_from_user(buf_tmp, buf, 1))
+		return -EFAULT;
+
+	if (buf_tmp[0] == '1' || buf_tmp[0] == 1) {
+		drv_data.pub_mon_enabled = 1;
+		ret = sprd_pub_monitor_enable(0);
+		if (ret)
+			return ret;
+		ret = sprd_pub_monitor_enable(1);
+		if (ret)
+			return ret;
+	} else if (buf_tmp[0] == '0' || buf_tmp[0] == 0) {
+		drv_data.pub_mon_enabled = 0;
+		ret = sprd_pub_monitor_enable(0);
+		if (ret)
+			return ret;
+	} else {
+		return -EINVAL;
+	}
+	return len;
+}
+
+static const struct file_operations pub_monitor_enable_fops = {
+	.open = sprd_pub_monitor_enable_open,
+	.read = seq_read,
+	.write = sprd_pub_monitor_enable_write,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static int sprd_pub_monitor_init(void)
+{
+	drv_data.monitor_dir = debugfs_create_dir("sprd_pub_monitor", NULL);
+	if (!drv_data.monitor_dir) {
+		pr_err("pub_monitor creat dir error\n");
+		return -ENOMEM;
+	}
+	debugfs_create_file("enable", 0664, drv_data.monitor_dir, NULL,
+			    &pub_monitor_enable_fops);
+	debugfs_create_file("status", 0444, drv_data.monitor_dir, NULL,
+			    &pub_monitor_status_fops);
+
+	return 0;
+}
+
+static int sprd_dmc_probe(struct platform_device *pdev)
+{
+	const struct dmc_data *pdata = NULL;
+	struct resource *res = NULL;
+	void __iomem *io_addr;
+	int i;
+
+	pdata = of_device_get_match_data(&pdev->dev);
+	if (!pdata) {
+		dev_err(&pdev->dev, "No matching driver data found\n");
+		return -EINVAL;
+	}
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
+	io_addr = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(io_addr))
+		return PTR_ERR(io_addr);
+	drv_data.size = readl_relaxed(io_addr + pdata->size_l_offset)
+		+ ((u64) readl_relaxed(io_addr + pdata->size_h_offset) << 32);
+	drv_data.type = readl_relaxed(io_addr + pdata->type_offset);
+	for (i = 0; i < DDR_MAX_SUPPORT_CS_NUM; i++)
+		drv_data.mr_val[i] =
+		    readl_relaxed(io_addr + pdata->mr_offset[i]);
+	iounmap(io_addr);
+#ifdef CONFIG_PROC_FS
+	sprd_ddr_proc_creat();
+#endif
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	drv_data.mon_base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(drv_data.mon_base))
+		return PTR_ERR(drv_data.mon_base);
+	sprd_pub_monitor_init();
+	return 0;
+}
+
+static int sprd_dmc_remove(struct platform_device *pdev)
+{
+	debugfs_remove_recursive(drv_data.monitor_dir);
+	if (drv_data.property != NULL)
+		remove_proc_entry(DDR_PROPERTY_NAME, drv_data.proc_dir);
+	if (drv_data.info != NULL)
+		remove_proc_entry(DDR_INFO_NAME, drv_data.proc_dir);
+	if (drv_data.proc_dir != NULL)
+		remove_proc_entry(DMC_PROC_NAME, NULL);
+	return 0;
+}
+
+static const struct of_device_id sprd_dmc_of_match[] = {
+	{.compatible = "sprd,sharkl3-dmc", .data = &sharkl3_data},
+	{.compatible = "sprd,sharkl5-dmc", .data = &sharkl5_data},
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, sprd_dmc_of_match);
+
+static struct platform_driver sprd_dmc_driver = {
+	.probe = sprd_dmc_probe,
+	.remove = sprd_dmc_remove,
+	.driver = {
+		   .name = "sprd-dmc",
+		   .of_match_table = sprd_dmc_of_match,
+	},
+};
+
+module_platform_driver(sprd_dmc_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("junqiang wang<junqiang.wang@spreadtrum.com>");
+MODULE_DESCRIPTION("dmc drv for Spreadtrum");
