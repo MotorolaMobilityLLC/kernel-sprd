ad0dec48a308674411920a463391da04e67a3d26
freeman.liu <Freeman.Liu@spreadtrum.com>
2019-01-21 20:34:20 +0800

    Bug #700456 iio: adc: Modify adc driver support sc2721
    [root cause  ] project need
    [changes     ] modify adc driver
    [side effects] no
    [self test   ]: build ok
    [download normally]: ok
    [power on/off normally]: ok
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]:ums510
    [test case]: read adc ok
    [reviewers   ] baolin.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Modify adc driver support sc2721.
    
    Change-Id: Ie4f22ae9b3f77114602ae4e584aec995ad354ae6
    Signed-off-by: Freeman Liu <freeman.liu@spreadtrum.com>

--- drivers/iio/adc/sc27xx_adc.c
+++ drivers/iio/adc/sc27xx_adc.c
@@ -15,6 +15,7 @@
 #define SC2731_MODULE_EN		0xc08
 #define SC2730_MODULE_EN		0x1808
 #define SC27XX_MODULE_ADC_EN		BIT(5)
+#define SC2721_ARM_CLK_EN		0xc0c
 #define SC2731_ARM_CLK_EN		0xc10
 #define SC2730_ARM_CLK_EN		0x180c
 #define SC27XX_CLK_ADC_EN		BIT(5)
@@ -39,7 +40,9 @@
 /* Bits and mask definition for SC27XX_ADC_CH_CFG register */
 #define SC27XX_ADC_CHN_ID_MASK		GENMASK(4, 0)
 #define SC27XX_ADC_SCALE_MASK		GENMASK(10, 8)
+#define SC2721_ADC_SCALE_MASK		BIT(5)
 #define SC27XX_ADC_SCALE_SHIFT		8
+#define SC2721_ADC_SCALE_SHIFT		5
 
 /* Bits definitions for SC27XX_ADC_INT_EN registers */
 #define SC27XX_ADC_IRQ_EN		BIT(0)
@@ -91,6 +94,10 @@ struct sc27xx_adc_data {
 struct sc27xx_adc_variant_data {
 	u32 module_en;
 	u32 clk_en;
+	u32 scale_shift;
+	u32 scale_mask;
+	const struct sc27xx_adc_linear_graph *bscale_cal;
+	const struct sc27xx_adc_linear_graph *sscale_cal;
 	void (*init_scale)(struct sc27xx_adc_data *data);
 	int (*get_ratio)(int channel, int scale);
 };
@@ -116,6 +123,16 @@ static struct sc27xx_adc_linear_graph big_scale_graph = {
 static struct sc27xx_adc_linear_graph small_scale_graph = {
 	1000, 3413,
 	100, 341,
+};
+
+static const struct sc27xx_adc_linear_graph sc2721_big_scale_graph_calib = {
+	4200, 850,
+	3600, 728,
+};
+
+static const struct sc27xx_adc_linear_graph sc2721_small_scale_graph_calib = {
+	1000, 838,
+	100, 84,
 };
 
 static const struct sc27xx_adc_linear_graph big_scale_graph_calib = {
@@ -145,11 +162,11 @@ static int sc27xx_adc_scale_calibration(struct sc27xx_adc_data *data,
 	size_t len;
 
 	if (big_scale) {
-		calib_graph = &big_scale_graph_calib;
+		calib_graph = data->var_data->bscale_cal;
 		graph = &big_scale_graph;
 		cell_name = "big_scale_calib";
 	} else {
-		calib_graph = &small_scale_graph_calib;
+		calib_graph = data->var_data->sscale_cal;
 		graph = &small_scale_graph;
 		cell_name = "small_scale_calib";
 	}
@@ -249,6 +266,33 @@ static int sc2730_adc_get_ratio(int channel, int scale)
 	return SC27XX_VOLT_RATIO(1, 1);
 }
 
+static int sc2721_adc_get_ratio(int channel, int scale)
+{
+	switch (channel) {
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+		return scale ? SC27XX_VOLT_RATIO(400, 1025) :
+			SC27XX_VOLT_RATIO(1, 1);
+	case 5:
+		return SC27XX_VOLT_RATIO(7, 29);
+	case 7:
+	case 9:
+		return scale ? SC27XX_VOLT_RATIO(100, 125) :
+			SC27XX_VOLT_RATIO(1, 1);
+	case 14:
+		return SC27XX_VOLT_RATIO(68, 900);
+	case 16:
+		return SC27XX_VOLT_RATIO(48, 100);
+	case 19:
+		return SC27XX_VOLT_RATIO(1, 3);
+	default:
+		return SC27XX_VOLT_RATIO(1, 1);
+	}
+	return SC27XX_VOLT_RATIO(1, 1);
+}
+
 static int sc2731_adc_get_ratio(int channel, int scale)
 {
 	switch (channel) {
@@ -325,7 +369,7 @@ static int sc27xx_adc_read(struct sc27xx_adc_data *data, int channel,
 		goto disable_adc;
 
 	/* Configure the channel id and scale */
-	tmp = (scale << SC27XX_ADC_SCALE_SHIFT) & SC27XX_ADC_SCALE_MASK;
+	tmp = (scale << data->var_data->scale_shift) & data->var_data->scale_mask;
 	tmp |= channel & SC27XX_ADC_CHN_ID_MASK;
 	ret = regmap_update_bits(data->regmap, data->base + SC27XX_ADC_CH_CFG,
 				 SC27XX_ADC_CHN_ID_MASK | SC27XX_ADC_SCALE_MASK,
@@ -603,13 +647,32 @@ static void sc27xx_adc_free_hwlock(void *_data)
 static const struct sc27xx_adc_variant_data sc2731_data = {
 	.module_en = SC2731_MODULE_EN,
 	.clk_en = SC2731_ARM_CLK_EN,
+	.scale_shift = SC2721_ADC_SCALE_SHIFT,
+	.scale_mask = SC2721_ADC_SCALE_MASK,
+	.bscale_cal = &sc2721_big_scale_graph_calib,
+	.sscale_cal = &sc2721_small_scale_graph_calib,
 	.init_scale = sc2731_adc_scale_init,
 	.get_ratio = sc2731_adc_get_ratio,
+};
+
+static const struct sc27xx_adc_variant_data sc2721_data = {
+	.module_en = SC2731_MODULE_EN,
+	.clk_en = SC2721_ARM_CLK_EN,
+	.scale_shift = SC2721_ADC_SCALE_SHIFT,
+	.scale_mask = SC2721_ADC_SCALE_MASK,
+	.bscale_cal = &sc2721_big_scale_graph_calib,
+	.sscale_cal = &sc2721_small_scale_graph_calib,
+	.init_scale = sc2731_adc_scale_init,
+	.get_ratio = sc2721_adc_get_ratio,
 };
 
 static const struct sc27xx_adc_variant_data sc2730_data = {
 	.module_en = SC2730_MODULE_EN,
 	.clk_en = SC2730_ARM_CLK_EN,
+	.scale_shift = SC27XX_ADC_SCALE_SHIFT,
+	.scale_mask = SC27XX_ADC_SCALE_MASK,
+	.bscale_cal = &big_scale_graph_calib,
+	.sscale_cal = &small_scale_graph_calib,
 	.init_scale = sc2730_adc_scale_init,
 	.get_ratio = sc2730_adc_get_ratio,
 };
@@ -705,6 +768,7 @@ static int sc27xx_adc_probe(struct platform_device *pdev)
 static const struct of_device_id sc27xx_adc_of_match[] = {
 	{ .compatible = "sprd,sc2731-adc", .data = &sc2731_data},
 	{ .compatible = "sprd,sc2730-adc", .data = &sc2730_data},
+	{ .compatible = "sprd,sc2721-adc", .data = &sc2721_data},
 	{ }
 };
 
