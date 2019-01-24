8d66345ca1e5cb726193c0e69942b42af651e16d
Jingxiang Li <jingxiang.li@spreadtrum.com>
2019-01-24 11:10:15 +0800

    Bug #1003042 misc: sprdwcn: keep vddsim2 26m clock open
    [root cause  ] vddsim2 not support power when sys resume after deepsleep
    [changes     ] keep vddsim2 26m clock open
    [side effects] no
    [self test   ]: Yes
    [download normally]: Yes
    [power on/off normally]: Yes
    [do common repository/branch inspection]: No
    [is there dependence]: No
    [confirm dependent commit]: No
    [board]: ums510-1h10
    [test case]: open wifi, test deepsleep case
    [reviewers   ] Songhe.Wei, Xiaodong.Bi
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    Test: make test-art
    
    This patch is to fix the wcn assert when sys resume after deepsleep.
    Keep the vddsim2 26m clock open.
    
    Change-Id: I446c3a0ba1b88123c78296b74e3ce95cec66350a
    Signed-off-by: Jingxiang Li <jingxiang.li@spreadtrum.com>

--- drivers/misc/sprdwcn/platform/wcn_boot.c
+++ drivers/misc/sprdwcn/platform/wcn_boot.c
@@ -1071,7 +1071,9 @@ static int check_cp_clock_mode(void)
 		WCN_INFO("clock mode: TSX\n");
 	} else {
 		WCN_INFO("clock mode: TCXO, outside clock\n");
+#ifndef CONFIG_UMW2652
 		marlin_avdd18_dcxo_enable(false);
+#endif
 	}
 
 	return ret;
