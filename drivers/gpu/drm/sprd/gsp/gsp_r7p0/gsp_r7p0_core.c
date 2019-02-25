1d1d1d8529bae8ba8d16423a9c96ecef0785d5da
ashley.chen <ashley.chen@spreadtrum.com>
2019-02-25 16:48:13 +0800

    Bug #876846 drm/sprd: enable dpu core clk and pm runtime for gsp
    [root cause  ] enable dpu core clk and pm runtime for gsp
    [changes     ] gsp driver file
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ud710_3h10u
    [test case]:display ok when use gsp blend
    [reviewers   ] chen.he
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Enable dpu core clk and pm runtime for gsp.
    
    Change-Id: I5afa7ca6bab9c688dfec1993a3d22c46a6d283ea
    Signed-off-by: Ashley Chen <ashley.chen@spreadtrum.com>

--- drivers/gpu/drm/sprd/gsp/gsp_r7p0/gsp_r7p0_core.c
+++ drivers/gpu/drm/sprd/gsp/gsp_r7p0/gsp_r7p0_core.c
@@ -682,7 +682,6 @@ static irqreturn_t gsp_r7p0_core_irq_handler(int irq, void *data)
 
 int gsp_r7p0_core_enable(struct gsp_core *c)
 {
-#if 0
 	int ret = -1;
 	struct gsp_r7p0_core *core = NULL;
 
@@ -701,6 +700,7 @@ int gsp_r7p0_core_enable(struct gsp_core *c)
 		goto dpu_clk_unprepare;
 	}
 
+	gsp_r7p0_int_clear(c);
 	gsp_r7p0_core_irq_enable(c);
 	goto exit;
 
@@ -708,13 +708,6 @@ dpu_clk_unprepare:
 	clk_disable_unprepare(core->dpu_clk);
 exit:
 	return ret;
-#endif
-	struct gsp_r7p0_core *core = NULL;
-
-	core = (struct gsp_r7p0_core *)c;
-	gsp_r7p0_int_clear(c);
-	gsp_r7p0_core_irq_enable(c);
-	return 0;
 }
 
 void gsp_r7p0_core_disable(struct gsp_core *c)
@@ -723,12 +716,11 @@ void gsp_r7p0_core_disable(struct gsp_core *c)
 
 	core = (struct gsp_r7p0_core *)c;
 	gsp_r7p0_int_clear_and_disable(c);
-	/*clk_disable_unprepare(core->dpu_clk);*/
+	clk_disable_unprepare(core->dpu_clk);
 }
 
 static int gsp_r7p0_core_parse_clk(struct gsp_r7p0_core *core)
 {
-#if 0
 	int status = 0;
 	core->dpu_clk = of_clk_get_by_name(core->common.node,
 			R3P0_DPU_CLOCK_NAME);
@@ -741,8 +733,6 @@ static int gsp_r7p0_core_parse_clk(struct gsp_r7p0_core *core)
 		status = -1;
 	}
 	return status;
-#endif
-	return 0;
 }
 
 static int gsp_r7p0_core_parse_irq(struct gsp_core *core)
