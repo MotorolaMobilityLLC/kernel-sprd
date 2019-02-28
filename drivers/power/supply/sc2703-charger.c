d3e243c68ddb80a47df0b64e9c6d31a12d1eb3db
Baolin Wang <baolin.wang@unisoc.com>
2019-02-28 20:27:36 +0800

    Bug #1000157 power: supply: sc2703: Allow to set current and terminate voltage in not charging status
    [root cause  ] Allow to set current and terminate voltage in not charging status
    [changes     ] Allow to set current and terminate voltage in not charging status
    [side effects] No
    [self test   ]: Charging
    [download normally]: Yes
    [power on/off normally]: Yes
    [do common repository/branch inspection]: Yes
    [is there dependence]: NO
    [confirm dependent commit]: Yes
    [board]: Sharkl5
    [test case]: Charging
    [reviewers   ] Yuanjiang Yu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Allow the charger manager can set the charging current and terminate voltage
    in not charging status, so we can re-start the charging when the battery
    temperature getting normal.
    
    Change-Id: I560b4e143f0682fa829ce77566ed85fdebab28ec
    Signed-off-by: Baolin Wang <baolin.wang@unisoc.com>

--- drivers/power/supply/sc2703-charger.c
+++ drivers/power/supply/sc2703-charger.c
@@ -915,7 +915,7 @@ sc2703_charger_usb_set_property(struct power_supply *psy,
 
 	mutex_lock(&info->lock);
 
-	if (!info->charging && psp != POWER_SUPPLY_PROP_STATUS) {
+	if (!info->charging && psp == POWER_SUPPLY_PROP_FEED_WATCHDOG) {
 		mutex_unlock(&info->lock);
 		return -ENODEV;
 	}
