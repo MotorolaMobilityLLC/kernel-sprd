27fd88ae1edaf961e1ddd468c55367dafb50da2c
Qingsheng.Li <Qingsheng.Li@spreadtrum.com>
2019-03-12 21:06:43 +0800

    Bug #854065 staging: ipa: Optimize the interrupt handle process
    [root cause  ] Optimize the interrupt handle process
    [changes     ] ipa
    [side effects] no
    [self test   ]: ipa
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: orca
    [test case]: ipa
    [reviewers   ] fei.zhang zhiwei.wu alvin.zhou xiaogang.liang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    ipa optimize the interrupt handle process, in order to solve the
    following problems：
    If a new interrupt is coming in during the processing,
    the interrupt handler will fall into an infinite loop.
    
    Change-Id: I4b3a4b938e350b7460574c99957b6010008bd519
    Signed-off-by: Qingsheng Li <qingsheng.li@spreadtrum.com>

--- drivers/staging/sprd/sipa/sipa_phy_v0/sipa_fifo_irq_hal.c
+++ drivers/staging/sprd/sipa/sipa_phy_v0/sipa_fifo_irq_hal.c
@@ -135,73 +135,46 @@ static inline u32 ipa_fifo_traverse_int_bit(
 {
 	void __iomem *fifo_base;
 	u32 clr_sts = 0;
-	u32 int_status = 0, int_status1 = 0;
+	u32 int_status = 0;
 
 	fifo_base = ipa_cfg->fifo_reg_base;
 	int_status = ipa_phy_get_fifo_all_int_sts(fifo_base);
 	IPA_LOG("The fifo intr sts = 0x%x\n", int_status);
-traverse_again:
 
-	if (int_status & IPA_INT_EXIT_FLOW_CTRL_STS) {
-		int_status1 |= IPA_INT_EXIT_FLOW_CTRL_STS;
+	if (int_status & IPA_INT_EXIT_FLOW_CTRL_STS)
 		clr_sts |= IPA_EXIT_FLOW_CONTROL_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_ERRORCODE_IN_TX_FIFO_STS) {
-		int_status1 |= IPA_INT_ERRORCODE_IN_TX_FIFO_STS;
+	if (int_status & IPA_INT_ERRORCODE_IN_TX_FIFO_STS)
 		clr_sts |= IPA_ERROR_CODE_INTR_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_ENTER_FLOW_CTRL_STS) {
-		int_status1 |= IPA_INT_ENTER_FLOW_CTRL_STS;
+	if (int_status & IPA_INT_ENTER_FLOW_CTRL_STS)
 		clr_sts |= IPA_ENTRY_FLOW_CONTROL_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_INTR_BIT_STS) {
-		int_status1 |= IPA_INT_INTR_BIT_STS;
+	if (int_status & IPA_INT_INTR_BIT_STS)
 		clr_sts |= IPA_TX_FIFO_INTR_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_TX_FIFO_THRESHOLD_SW_STS) {
-		int_status1 |= IPA_INT_TX_FIFO_THRESHOLD_SW_STS;
+	if (int_status & IPA_INT_TX_FIFO_THRESHOLD_SW_STS)
 		clr_sts |= IPA_TX_FIFO_THRESHOLD_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_DELAY_TIMER_STS) {
-		int_status1 |= IPA_INT_DELAY_TIMER_STS;
+	if (int_status & IPA_INT_DELAY_TIMER_STS)
 		clr_sts |= IPA_TX_FIFO_TIMER_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_DROP_PACKT_OCCUR) {
-		int_status1 |= IPA_INT_DROP_PACKT_OCCUR;
+	if (int_status & IPA_INT_DROP_PACKT_OCCUR)
 		clr_sts |= IPA_DROP_PACKET_INTR_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_TXFIFO_OVERFLOW_STS) {
-		int_status1 |= IPA_INT_TXFIFO_OVERFLOW_STS;
+	if (int_status & IPA_INT_TXFIFO_OVERFLOW_STS)
 		clr_sts |= IPA_TX_FIFO_OVERFLOW_CLR_BIT;
-	}
 
-	if (int_status & IPA_INT_TXFIFO_FULL_INT_STS) {
-		int_status1 |= IPA_INT_TXFIFO_FULL_INT_STS;
+	if (int_status & IPA_INT_TXFIFO_FULL_INT_STS)
 		clr_sts |= IPA_TX_FIFO_FULL_INT_CLR_BIT;
-	}
 
 	if (ipa_cfg->fifo_irq_callback)
 		ipa_cfg->fifo_irq_callback(ipa_cfg->priv,
-								   int_status1, id);
+					int_status, id);
 	else
 		IPA_ERR("Don't register this fifo(%d) irq callback\n", id);
 
-	int_status = ipa_phy_get_fifo_all_int_sts(fifo_base);
-
-	if ((int_status & (~IPA_INT_THRESHOLD_STS)) != int_status1) {
-		int_status &= (~int_status1);
-		int_status1 = 0;
-		goto traverse_again;
-	} else {
-		ipa_phy_clear_int(ipa_cfg->fifo_reg_base, clr_sts);
-	}
+	ipa_phy_clear_int(ipa_cfg->fifo_reg_base, clr_sts);
 
 	return TRUE;
 }
