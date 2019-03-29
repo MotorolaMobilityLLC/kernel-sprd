e84eb0c5f21dffeb6e78b7df82e5c2302855a6a4
westbobo.zhou <westbobo.zhou@unisoc.com>
2019-03-29 13:14:43 +0800

    Bug #1035718 usb: phy: Detect the charger state earlier
    [root cause  ]lack feature of charging when shutdown
    [changes     ] add it
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:roc1+orca
    [test case]:shutdown and charging, ckeck charging icon shows or not.
    [reviewers   ] hongbo.li
    
    [change_type ] feature_bugfix
    [tag_product ] common
    
    We should detect the charger state earlier in case missing some connection
    event before we register the extcon device.
    
    Change-Id: Iadd9dc1e4721b0db8ec964bf0494db89387910d2
    Signed-off-by: Westbobo Zhou <westbobo.zhou@unisoc.com>

--- drivers/usb/phy/phy-sprd-usb3.c
+++ drivers/usb/phy/phy-sprd-usb3.c
@@ -589,6 +589,8 @@ static int sprd_ssphy_probe(struct platform_device *pdev)
 	if (ret)
 		dev_warn(dev, "failed to create usb ssphy attributes\n");
 
+	if (extcon_get_state(phy->phy.edev, EXTCON_USB) > 0)
+		usb_phy_set_charger_state(&phy->phy, USB_CHARGER_PRESENT);
 	return 0;
 }
 
