f5f54d738b653ad58ae7319aca365111dbcaa324
freeman.liu <Freeman.Liu@spreadtrum.com>
2019-04-09 11:26:20 +0800

    Bug #898040 mfd: sprd: Add power delivery cell for SC27XX PMIC
    [root cause  ] project need
    [changes     ] add power delivery driver
    [side effects] no
    [self test   ]: build ok
    [download normally]: ok
    [power on/off normally]: ok
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: ums312
    [test case]: build ok
    [reviewers   ] baolin.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    The fast charging of PD protocol needs the support of PD module.
    So this patch adds one pd cell to help to support pd protocol.
    
    Change-Id: Ic4259544a00de65f1879b878561cc56beeef9f67
    Signed-off-by: Freeman Liu <freeman.liu@spreadtrum.com>

--- drivers/mfd/sprd-sc27xx-spi.c
+++ drivers/mfd/sprd-sc27xx-spi.c
@@ -131,6 +131,9 @@ static const struct mfd_cell sprd_pmic_devs[] = {
 	}, {
 		.name = "sc27xx-tsensor",
 		.of_compatible = "sprd,sc27xx-tsensor",
+	}, {
+		.name = "sc27xx-pd",
+		.of_compatible = "sprd,sc27xx-pd",
 	},
 };
 
