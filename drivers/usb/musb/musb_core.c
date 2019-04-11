d76517c6e3f3d09ab6ce09b9e742cf3b84f32e13
Hongbo Li <hongbo.li@unisoc.com>
2019-04-11 15:03:43 +0800

    Bug #1026922 usb: musb: Fix usb suspend fail
    [root cause  ] fix usb suspend fail
    [changes     ] musb_core.c
    [side effects] none
    [self test   ]:usb test
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:roc1
    [test case]:usb test
    [reviewers   ] jiayong.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    The musb devctl session bit is set until vbus is invlid
    in unisoc platform. So the usb core can not enter suspend
    when we plug out usb, because musb check session bit is set
    and don't put block runtime PM in disconnect interrupt.
    
    Besides, we use typec to notify usb plug in/out event,
    and need not to check session bit.
    
    So we remove the musb_pm_runtime_check_session function.
    
    Change-Id: I060fd0f995b7d7eecd47fdf9efa0977bebc96923
    Signed-off-by: Hongbo Li <hongbo.li@unisoc.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1877,83 +1877,6 @@ static const struct attribute_group musb_attr_group = {
 	.attrs = musb_attributes,
 };
 
-#define MUSB_QUIRK_B_INVALID_VBUS_91	(MUSB_DEVCTL_BDEVICE | \
-					 (2 << MUSB_DEVCTL_VBUS_SHIFT) | \
-					 MUSB_DEVCTL_SESSION)
-#define MUSB_QUIRK_A_DISCONNECT_19	((3 << MUSB_DEVCTL_VBUS_SHIFT) | \
-					 MUSB_DEVCTL_SESSION)
-
-/*
- * Check the musb devctl session bit to determine if we want to
- * allow PM runtime for the device. In general, we want to keep things
- * active when the session bit is set except after host disconnect.
- *
- * Only called from musb_irq_work. If this ever needs to get called
- * elsewhere, proper locking must be implemented for musb->session.
- */
-static void musb_pm_runtime_check_session(struct musb *musb)
-{
-	u8 devctl, s;
-	int error;
-
-	devctl = musb_readb(musb->mregs, MUSB_DEVCTL);
-
-	/* Handle session status quirks first */
-	s = MUSB_DEVCTL_FSDEV | MUSB_DEVCTL_LSDEV |
-		MUSB_DEVCTL_HR;
-	switch (devctl & ~s) {
-	case MUSB_QUIRK_B_INVALID_VBUS_91:
-		if (musb->quirk_retries && !musb->flush_irq_work) {
-			musb_dbg(musb,
-				 "Poll devctl on invalid vbus, assume no session");
-			schedule_delayed_work(&musb->irq_work,
-					      msecs_to_jiffies(1000));
-			musb->quirk_retries--;
-			return;
-		}
-		/* fall through */
-	case MUSB_QUIRK_A_DISCONNECT_19:
-		if (musb->quirk_retries && !musb->flush_irq_work) {
-			musb_dbg(musb,
-				 "Poll devctl on possible host mode disconnect");
-			schedule_delayed_work(&musb->irq_work,
-					      msecs_to_jiffies(1000));
-			musb->quirk_retries--;
-			return;
-		}
-		if (!musb->session)
-			break;
-		musb_dbg(musb, "Allow PM on possible host mode disconnect");
-		pm_runtime_mark_last_busy(musb->controller);
-		pm_runtime_put_autosuspend(musb->controller);
-		musb->session = false;
-		return;
-	default:
-		break;
-	}
-
-	/* No need to do anything if session has not changed */
-	s = devctl & MUSB_DEVCTL_SESSION;
-	if (s == musb->session)
-		return;
-
-	/* Block PM or allow PM? */
-	if (s) {
-		musb_dbg(musb, "Block PM on active session: %02x", devctl);
-		error = pm_runtime_get_sync(musb->controller);
-		if (error < 0)
-			dev_err(musb->controller, "Could not enable: %i\n",
-				error);
-		musb->quirk_retries = 3;
-	} else {
-		musb_dbg(musb, "Allow PM with no session: %02x", devctl);
-		pm_runtime_mark_last_busy(musb->controller);
-		pm_runtime_put_autosuspend(musb->controller);
-	}
-
-	musb->session = s;
-}
-
 /* Only used to provide driver mode change events */
 static void musb_irq_work(struct work_struct *data)
 {
@@ -1966,8 +1889,6 @@ static void musb_irq_work(struct work_struct *data)
 
 		return;
 	}
-
-	musb_pm_runtime_check_session(musb);
 
 	if (musb->xceiv->otg->state != musb->xceiv_old_state) {
 		musb->xceiv_old_state = musb->xceiv->otg->state;
