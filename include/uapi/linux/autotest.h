bd9a82f2be21e5d1bd87128f0add1b4d04492b7a
Hongbo Li <hongbo.li@unisoc.com>
2019-04-28 18:20:36 +0800

    Bug #1057317 soc: sprd: Add otg autotest driver
    [root cause  ] Add otg autotest driver
    [changes     ] autotest otg
    [side effects] none
    [self test   ]:otg test
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:roc1
    [test case]:otg test
    [reviewers   ] jiayong.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add otg autotest driver for BBAT.
    
    Change-Id: Ie53c23cb6f3c75e8138c478ea14f7a01fde38a15
    Signed-off-by: Hongbo Li <hongbo.li@unisoc.com>

--- include/uapi/linux/autotest.h
+++ include/uapi/linux/autotest.h
@@ -5,5 +5,6 @@
 
 #define AT_PINCTRL	_IOWR(AUTOTEST_MAGIC, 0, int)/* Set at_pinctrl cmd*/
 #define AT_GPIO		_IOWR(AUTOTEST_MAGIC, 1, int)/* Set at_gpio cmd */
+#define AT_OTG		_IOWR(AUTOTEST_MAGIC, 2, int)/* Set at_otg cmd */
 
 #endif /* _UAPI_LINUX_AUTOTEST_H_ */
