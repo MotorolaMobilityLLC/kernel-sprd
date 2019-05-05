41e0e8aaec51e5e89ae8d126983266c76c46bdcb
ben.li <ben.li@unisoc.com>
2019-05-05 16:40:14 +0800

    Bug #1052249 clk: sprd: Add sharkl5pro clock driver
    [root cause  ] Add sharkl5pro clock driver
    [changes     ] clock
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums518_haps
    [test case]: power on
    [reviewers   ] laura.yu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add sharkl5pro clock driver for clock verification
    
    Change-Id: I5f252f3e7ce59ee171ac600e17ca222481e9e272
    Signed-off-by: Li Hao <ben.li@unisoc.com>

--- include/dt-bindings/clock/sprd,sharkl5pro-clk.h
+++ include/dt-bindings/clock/sprd,sharkl5pro-clk.h
@@ -7,6 +7,91 @@
 #ifndef _DT_BINDINGS_CLK_SHARKL5PRO_H_
 #define _DT_BINDINGS_CLK_SHARKL5PRO_H_
 
+#define CLK_ISPPLL_GATE			0
+#define CLK_DPLL0_GATE			1
+#define CLK_DPLL1_GATE			2
+#define CLK_LPLL_GATE			3
+#define CLK_TWPLL_GATE			4
+#define CLK_GPLL_GATE			5
+#define CLK_RPLL_GATE			6
+#define CLK_CPPLL_GATE			7
+#define CLK_MPLL0_GATE			8
+#define CLK_MPLL1_GATE			9
+#define CLK_MPLL2_GATE			10
+#define CLK_PMU_GATE_NUM		(CLK_MPLL2_GATE + 1)
+
+#define CLK_DPLL0			0
+#define CLK_ANLG_PHY_G0_NUM		(CLK_DPLL0 + 1)
+
+#define CLK_MPLL1			0
+#define CLK_ANLG_PHY_G2_NUM		(CLK_MPLL1 + 1)
+
+#define CLK_RPLL			0
+#define CLK_AUDIO_GATE			1
+#define CLK_MPLL0			2
+#define CLK_MPLL2			3
+#define CLK_ANLG_PHY_G3_NUM		(CLK_MPLL2 + 1)
+
+#define CLK_TWPLL			0
+#define CLK_TWPLL_768M			1
+#define CLK_TWPLL_384M			2
+#define CLK_TWPLL_192M			3
+#define CLK_TWPLL_96M			4
+#define CLK_TWPLL_48M			5
+#define CLK_TWPLL_24M			6
+#define CLK_TWPLL_12M			7
+#define CLK_TWPLL_512M			8
+#define CLK_TWPLL_256M			9
+#define CLK_TWPLL_128M			10
+#define CLK_TWPLL_64M			11
+#define CLK_TWPLL_307M2			12
+#define CLK_TWPLL_219M4			13
+#define CLK_TWPLL_170M6			14
+#define CLK_TWPLL_153M6			15
+#define CLK_TWPLL_76M8			16
+#define CLK_TWPLL_51M2			17
+#define CLK_TWPLL_38M4			18
+#define CLK_TWPLL_19M2			19
+#define CLK_LPLL			20
+#define CLK_LPLL_614M4			21
+#define CLK_LPLL_409M6			22
+#define CLK_LPLL_245M76			23
+#define CLK_ISPPLL			24
+#define CLK_ISPPLL_468M			25
+#define CLK_ISPPLL_78M			26
+#define CLK_GPLL			27
+#define CLK_CPPLL			28
+#define CLK_ANLG_PHY_GC_NUM		(CLK_CPPLL + 1)
+
+#define CLK_AP_APB			0
+#define CLK_AP_UART0			1
+#define CLK_AP_UART1			2
+#define CLK_AP_UART2			3
+#define CLK_AP_I2C0			4
+#define CLK_AP_I2C1			5
+#define CLK_AP_I2C2			6
+#define CLK_AP_I2C3			7
+#define CLK_AP_I2C4			8
+#define CLK_AP_SPI0			9
+#define CLK_AP_SPI1			10
+#define CLK_AP_SPI2			11
+#define CLK_AP_SPI3			12
+#define CLK_AP_IIS0			13
+#define CLK_AP_IIS1			14
+#define CLK_AP_IIS2			15
+#define CLK_AP_SIM			16
+#define CLK_AP_CE			17
+#define CLK_SDIO0_2X			18
+#define CLK_SDIO1_2X			19
+#define CLK_EMMC_2X			20
+#define CLK_VSP				21
+#define CLK_DISPC0			22
+#define CLK_DISPC0_DPI			23
+#define CLK_DSI_APB			24
+#define CLK_DSI_RXESC			25
+#define CLK_DSI_LANEBYTE		26
+#define CLK_AP_CLK_NUM			(CLK_DSI_LANEBYTE + 1)
+
 #define CLK_DSI_EB			0
 #define CLK_DISPC_EB			1
 #define CLK_VSP_EB			2
@@ -17,6 +102,69 @@
 #define CLK_AHB_CKG_EB			7
 #define CLK_BM_CLK_EB			8
 #define CLK_AP_AHB_GATE_NUM		(CLK_BM_CLK_EB + 1)
+
+#define CLK_AON_APB			0
+#define CLK_ADI				1
+#define CLK_AUX0			2
+#define CLK_AUX1			3
+#define CLK_AUX2			4
+#define CLK_PROBE			5
+#define CLK_PWM0			6
+#define CLK_PWM1			7
+#define CLK_PWM2			8
+#define CLK_PWM3			9
+#define CLK_EFUSE			10
+#define CLK_UART0			11
+#define CLK_UART1			12
+#define CLK_THM0			13
+#define CLK_THM1			14
+#define CLK_THM2			15
+#define CLK_THM3			16
+#define CLK_AON_I2C			17
+#define CLK_AON_IIS			18
+#define CLK_SCC				19
+#define CLK_APCPU_DAP			20
+#define CLK_APCPU_DAP_MTCK		21
+#define CLK_APCPU_TS			22
+#define CLK_DEBUG_TS			23
+#define CLK_DSI_TEST_S			24
+#define CLK_DJTAG_TCK			25
+#define CLK_DJTAG_TCK_HW		26
+#define CLK_AON_TMR			27
+#define CLK_AON_PMU			28
+#define CLK_DEBOUNCE			29
+#define CLK_APCPU_PMU			30
+#define CLK_TOP_DVFS			31
+#define CLK_OTG_UTMI			32
+#define CLK_OTG_REF			33
+#define CLK_CSSYS			34
+#define CLK_CSSYS_PUB			35
+#define CLK_CSSYS_APB			36
+#define CLK_AP_AXI			37
+#define CLK_AP_MM			38
+#define CLK_SDIO2_2X			39
+#define CLK_ANALOG_IO_APB		40
+#define CLK_DMC_REF_CLK			41
+#define CLK_EMC				42
+#define CLK_USB				43
+#define CLK_26M_PMU			44
+#define CLK_AON_APB_NUM			(CLK_26M_PMU + 1)
+
+#define CLK_MM_AHB			0
+#define CLK_MM_MTX			1
+#define CLK_SENSOR0			2
+#define CLK_SENSOR1			3
+#define CLK_SENSOR2			4
+#define CLK_CPP				5
+#define CLK_JPG				6
+#define CLK_FD				7
+#define CLK_DCAM_IF			8
+#define CLK_DCAM_AXI			9
+#define CLK_ISP				10
+#define CLK_MIPI_CSI0			11
+#define CLK_MIPI_CSI1			12
+#define CLK_MIPI_CSI2			13
+#define CLK_MM_CLK_NUM			(CLK_MIPI_CSI2 + 1)
 
 #define CLK_RC100M_CAL_EB		0
 #define CLK_DJTAG_TCK_EB		1
@@ -119,6 +267,28 @@
 #define CLK_DEBUG_TS_EN			98
 #define CLK_AON_APB_GATE_NUM		(CLK_DEBUG_TS_EN + 1)
 
+#define CLK_MM_CPP_EB			0
+#define CLK_MM_JPG_EB			1
+#define CLK_MM_DCAM_EB			2
+#define CLK_MM_ISP_EB			3
+#define CLK_MM_CSI2_EB			4
+#define CLK_MM_CSI1_EB			5
+#define CLK_MM_CSI0_EB			6
+#define CLK_MM_CKG_EB			7
+#define CLK_ISP_AHB_EB			8
+#define CLK_MM_DVFS_EB			9
+#define CLK_MM_FD_EB			10
+#define CLK_MM_SENSOR2_EB		11
+#define CLK_MM_SENSOR1_EB		12
+#define CLK_MM_SENSOR0_EB		13
+#define CLK_MM_MIPI_CSI2_EB		14
+#define CLK_MM_MIPI_CSI1_EB		15
+#define CLK_MM_MIPI_CSI0_EB		16
+#define CLK_DCAM_AXI_EB			17
+#define CLK_ISP_AXI_EB			18
+#define CLK_MM_CPHY_EB			19
+#define CLK_MM_GATE_CLK_NUM		(CLK_MM_CPHY_EB + 1)
+
 #define CLK_SIM0_EB			0
 #define CLK_IIS0_EB			1
 #define CLK_IIS1_EB			2
@@ -151,5 +321,38 @@
 #define CLK_EMMC_32K_EB			29
 #define CLK_AP_APB_GATE_NUM		(CLK_EMMC_32K_EB + 1)
 
-#endif /* _DT_BINDINGS_CLK_SHARKL5PRO_H_ */
+#define CLK_GPU_CORE_EB			0
+#define CLK_GPU_CORE			1
+#define CLK_GPU_MEM_EB			2
+#define CLK_GPU_MEM			3
+#define CLK_GPU_SYS_EB			4
+#define CLK_GPU_SYS			5
+#define CLK_GPU_CLK_NUM			(CLK_GPU_SYS + 1)
 
+#define CLK_AUDCP_IIS0_EB		0
+#define CLK_AUDCP_IIS1_EB		1
+#define CLK_AUDCP_IIS2_EB		2
+#define CLK_AUDCP_UART_EB		3
+#define CLK_AUDCP_DMA_CP_EB		4
+#define CLK_AUDCP_DMA_AP_EB		5
+#define CLK_AUDCP_SRC48K_EB		6
+#define CLK_AUDCP_MCDT_EB		7
+#define CLK_AUDCP_VBCIFD_EB		8
+#define CLK_AUDCP_VBC_EB		9
+#define CLK_AUDCP_SPLK_EB		10
+#define CLK_AUDCP_ICU_EB		11
+#define CLK_AUDCP_DMA_AP_ASHB_EB	12
+#define CLK_AUDCP_DMA_CP_ASHB_EB	13
+#define CLK_AUDCP_AUD_EB		14
+#define CLK_AUDCP_VBC_24M_EB		15
+#define CLK_AUDCP_TMR_26M_EB		16
+#define CLK_AUDCP_DVFS_ASHB_EB		17
+#define CLK_AUDCP_AHB_GATE_NUM		(CLK_AUDCP_DVFS_ASHB_EB + 1)
+
+#define CLK_AUDCP_WDG_EB		0
+#define CLK_AUDCP_RTC_WDG_EB		1
+#define CLK_AUDCP_TMR0_EB		2
+#define CLK_AUDCP_TMR1_EB		3
+#define CLK_AUDCP_APB_GATE_NUM		(CLK_AUDCP_TMR1_EB + 1)
+
+#endif /* _DT_BINDINGS_CLK_SHARKL5PRO_H_ */
