1cd124363b54465fa774c815e9afd9ed0b379abd
qingsheng.li <qingsheng.li@unisoc.com>
2019-05-23 21:18:39 +0800

    Bug #1058871 staging: sipa: adjust the timeout parameter to increase the aggregation capability of IPA
    [root cause  ] adjust the timeout parameter to increase the aggregation
    capability of IPA
    [changes     ] IPA
    [side effects] no
    [self test   ]: ipa
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: roc1
    [test case]: ipa
    [reviewers   ] zhiwei.wu alvin.zhou fei.zhang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Adjust the timeout and threshold parameters to increase the aggregation
    capability of IPA.
    
    Change-Id: Ia401f311d9c5555176903eb7eea0d33ac005bcb6
    Signed-off-by: Qingsheng Li <qingsheng.li@spreadtrum.com>

--- drivers/staging/sprd/sipa/sipa_skb_recv.c
+++ drivers/staging/sprd/sipa/sipa_skb_recv.c
@@ -312,8 +312,9 @@ void sipa_receiver_init(struct sipa_skb_receiver *receiver, u32 rsvd)
 	u32 depth;
 	struct sipa_comm_fifo_params attr;
 
-	attr.tx_intr_delay_us = 0x10;
-	attr.tx_intr_threshold = 0x20;
+	/* timeout = 1 / ipa_sys_clk * 1024 * value */
+	attr.tx_intr_delay_us = 0x64;
+	attr.tx_intr_threshold = 0x30;
 	attr.flowctrl_in_tx_full = false;
 	attr.flow_ctrl_cfg = flow_ctrl_rx_empty;
 	attr.flow_ctrl_irq_mode = enter_exit_flow_ctrl;
