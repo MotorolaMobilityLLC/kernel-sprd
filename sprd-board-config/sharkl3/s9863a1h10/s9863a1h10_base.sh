1bd3eaef7d9d850b6db5078b2f7a17a801b2d7bb
Ruifeng Zhang <zhangrf@spreadst.com>
2019-05-30 12:08:05 +0800

    Bug #1084924 bsp: change sharkl3 board name
    [root cause  ] change sharkl3 board name
    [changes     ] change sharkl3 board name
    [side effects] no
    [self test   ]:  yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: s9863a1h10
    [test case]: bsp compile passed
    [reviewers   ] nianfu.bai
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Iffaf0bfe3a2e9f0056a625ea2dbcd0d4feab5dd2
    Signed-off-by: Ruifeng Zhang <zhangrf@spreadst.com>

--- sprd-board-config/sharkl3/s9863a1h10/s9863a1h10_base.sh
+++ sprd-board-config/sharkl3/s9863a1h10/s9863a1h10_base.sh
@@ -5,7 +5,7 @@ if [ -z $BSP_KERNEL_PATH ]; then
 	BSP_KERNEL_PATH="."
 fi
 export BSP_KERNEL_CROSS_COMPILE="$BSP_KERNEL_PATH/../../toolchain/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-export BSP_BOARD_NAME="sp9863a_1h10"
+export BSP_BOARD_NAME="s9863a1h10"
 
 export BSP_BOARD_WCN_CONFIG=""
 export BSP_BOARD_EXT_PMIC_CONFIG=""
