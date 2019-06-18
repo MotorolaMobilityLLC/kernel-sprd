e9ab433eb37d30a9c39ea00f0562aaa18deb3572
Cixi.Geng <gengcx@spreadst.com>
2019-06-18 15:24:35 +0800

    Bug #1098493 arm: board-config: create new board for sp9863a1h10 go 32bit
    [root cause  ] create new board
    [changes     ] sprd-board-config
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sp9863a
    [test case]: build verify
    [reviewers   ] Nianfu.Bai Ruifeng.Zhang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I885b03caebe6e5b17712f88bbf2a29b7a4307bdb
    Signed-off-by: Cixi.Geng <gengcx@spreadst.com>

---
+++ sprd-board-config/sharkl3/s9863a1h10_go_32b/s9863a1h10_go_32b_base.sh
@@ -0,0 +1,16 @@
+export BSP_KERNEL_ARCH="arm"
+export BSP_KERNEL_DIFF_CONFIG_ARCH="sprd-diffconfig/sharkl3/$BSP_KERNEL_ARCH"
+export BSP_KERNEL_DIFF_CONFIG_COMMON="sprd-diffconfig/sharkl3/common"
+if [ -z $BSP_KERNEL_PATH ]; then
+	BSP_KERNEL_PATH="."
+fi
+export BSP_KERNEL_CROSS_COMPILE="$BSP_KERNEL_PATH/../../toolchain/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+export BSP_BOARD_NAME="s9863a1h10_go_32b"
+
+export BSP_BOARD_WCN_CONFIG=""
+export BSP_BOARD_EXT_PMIC_CONFIG=""
+export BSP_PRODUCT_GO_DEVICE="true"
+export BSP_BOARD_FEATUREPHONE_CONFIG=""
+export BSP_BOARD_TEE_CONFIG=""
+
+export BSP_BOARD_TEE_64BIT="true"
