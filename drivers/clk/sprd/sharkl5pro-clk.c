4339da3ac8a2eec8a79e855afc2a4b039bc88620
laura.yu <laura.yu@spreadtrum.com>
2019-06-21 20:31:28 +0800

    Bug #1079473 clk: sprd: Add ISPPLL_468M as dcam_if and dcam_axi's clock source
    [root cause  ] dcam need to use ISPPLL_468M
    [changes     ] add ISPPLL_468M as dcam_if and dcam_axi's clock source
    [side effects]none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:ums512
    [test case]:build ok
    [reviewers   ] ben.li xiaolong.zhang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Dcam need to use ISPPLL_468M, so we should add ISPPLL_468M
    as dcam_if and dcam_axi's clock source.
    
    Change-Id: Ie6835badd8fca28677a04a912a19772544bb28af
    Signed-off-by: Laura Yu <laura.yu@unisoc.com>

--- drivers/clk/sprd/sharkl5pro-clk.c
+++ drivers/clk/sprd/sharkl5pro-clk.c
@@ -573,14 +573,16 @@ static SPRD_MUX_CLK(fd_clk, "fd-clk", fd_parents, 0x3c,
 		    0, 2, SHARKL5PRO_MUX_FLAG);
 
 static const char * const dcam_if_parents[] = { "twpll-192m", "twpll-256m",
-					"twpll-307m2", "twpll-384m" };
+						"twpll-307m2", "twpll-384m",
+						"isppll-468m" };
 static SPRD_MUX_CLK(dcam_if_clk, "dcam-if-clk", dcam_if_parents, 0x40,
-		    0, 2, SHARKL5PRO_MUX_FLAG);
+		    0, 3, SHARKL5PRO_MUX_FLAG);
 
 static const char * const dcam_axi_parents[] = { "twpll-192m", "twpll-256m",
-					"twpll-307m2", "twpll-384m" };
+						 "twpll-307m2", "twpll-384m",
+						 "isppll-468m" };
 static SPRD_MUX_CLK(dcam_axi_clk, "dcam-axi-clk", dcam_axi_parents, 0x44,
-		    0, 2, SHARKL5PRO_MUX_FLAG);
+		    0, 3, SHARKL5PRO_MUX_FLAG);
 
 static const char * const isp_parents[] = { "twpll-256m", "twpll-307m2",
 					    "twpll-384m", "isppll-468m",
