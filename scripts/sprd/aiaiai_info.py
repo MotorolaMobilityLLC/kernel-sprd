56e1ee7ce88e763aa1cd8ead94d5c6c96538eccb
Ruifeng Zhang <zhangrf@spreadst.com>
2019-07-01 20:02:33 +0800

    Bug #1105229 scripts: supply a script for aiaiai get compile info
    [root cause  ] different defconfig may use different toolchain
    [changes     ] supply a script for aiaiai get compile info
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: s9863a1h10
    [test case]: compile passed
    [reviewers   ] nianfu.bai maggie.zhang
    [change_type ] feature
    [tag_product ] common
    
    In the AndroidQ, aiaiai need support compile with clang,
    So the original way compile with gcc in default doesn't work.
    
    This modify supply a script for aiaiai get information
    include defconfig_name,arch,cross_compile,toolchain
    
    e.g.
    sprd_pike2_defconfig,arm,arm-linux-androideabi-,gcc
    
    Change-Id: I6ed4c2736778baf854485c57aca41feefbbecdf9
    Signed-off-by: Ruifeng Zhang <zhangrf@spreadst.com>

---
+++ scripts/sprd/aiaiai_info.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import re
+
+defconfig_list=[]
+boardconfig_list=[]
+def main():
+	result=[]
+	for maindir,subdir,file_name_list in os.walk("arch"):
+		"""
+		print("1:",maindir) #current dir
+		print("2:",subdir) # all subdirs
+		print("3:",file_name_list)  #all subfiles
+		"""
+		for filename in file_name_list:
+			if "sprd" in filename and "defconfig" in filename:
+				apath = os.path.join(maindir, filename)
+				result.append(apath)
+
+				#apath value example: arch/arm64/configs/sprd_all_defconfig
+				arch = (apath.split("/").pop(1))
+				name = (apath.split("/").pop(3))
+				path = apath
+
+				defconfig_list.append(name+","+arch)
+
+	for maindir,subdir,file_name_list in os.walk("sprd-board-config"):
+		toolchain=""
+		cross_compile=""
+		kernel_defconfig=""
+		"""
+		print("1:",maindir) #current dir
+		print("2:",subdir) # all subdirs
+		print("3:",file_name_list)  #all subfiles
+		"""
+		for filename in file_name_list:
+			apath = os.path.join(maindir, filename)
+			result.append(apath)
+
+			f=open(apath,'r')
+			lines = f.readlines()
+			if '#' in lines:
+				continue
+			for j in range(len(lines)):
+				if '"' in lines[j][-2:]:
+					tmpline = lines[j][:-2]
+				else:
+					tmpline = lines[j][:-1]
+
+				matchObj = re.search( r'sprd(.*)defconfig', tmpline)
+				if matchObj:
+					kernel_defconfig=matchObj.group()
+
+				matchObj = re.search( r'KERNEL_ARCH(.*)=(.*)', tmpline)
+				if matchObj:
+					arch=matchObj.group(2).lstrip('"').lstrip()
+
+				if 'CROSS_COMPILE' in tmpline:
+					matchObj = re.search(r'(.*)-' ,tmpline.split("/").pop())
+					if matchObj:
+						cross_compile=matchObj.group(0)
+
+				if 'CC_LD_ARG' in tmpline and toolchain != 'clang':
+					toolchain="clang"
+				elif toolchain=="":
+					toolchain="gcc"
+
+				if 'CLANG_TRIPLE' in tmpline:
+					clang_toolchain=tmpline.split("=").pop().lstrip('"').lstrip()
+
+		if toolchain=="clang":
+			cross_compile=clang_toolchain
+		if kernel_defconfig!="" and arch!="" and cross_compile!="" and toolchain!="":
+			boardconfig_list.append(kernel_defconfig+","+arch+","+cross_compile+","+toolchain)
+
+	formatList = list(set(boardconfig_list))
+	formatList.sort()
+
+	string_output=""
+	for boardconfig in formatList:
+		for defconfig in defconfig_list:
+			two_head_string=boardconfig.split(",").pop(0)+","+boardconfig.split(",").pop(1)
+			if defconfig == two_head_string:
+				string_output+=boardconfig+" "
+
+	print(string_output)
+if __name__ == '__main__':
+    main()
