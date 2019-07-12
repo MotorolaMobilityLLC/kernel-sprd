da276033d5e889a59b42347c20cf84e704eea78d
Kevin Tang <kevin.tang@spreadtrum.com>
2019-07-12 20:40:21 +0800

    Bug #1089531 dirty: backlight: verify brightness value is valid or not
    [root cause  ] brightness out of range
    [changes     ] backlight.c
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums312_1h10
    [test case]:suspend and resume test ok
    [reviewers   ] leon.he baolin.wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Because our thermal module may modify max brightness, but the android
    does not know that max brightness has changed, still adjust the backlight
    value according to the previous max brightness.
    This may cause the backlight is unblank when waking up.
    
    Maybe the brightness is out of range, so verify brightness value
    is valid or not.
    
    Change-Id: If17f6bb8cb543cd298e1b03a20a7d24484d0151b
    Signed-off-by: Kevin Tang <kevin.tang@spreadtrum.com>
    (cherry picked from commit 30541a9d283e31f5e14a7c69e715d2b03dcff47e)

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -178,12 +178,13 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops) {
 		if (brightness > bd->props.max_brightness)
-			rc = -EINVAL;
-		else {
-			pr_debug("set brightness to %lu\n", brightness);
-			bd->props.brightness = brightness;
-			rc = backlight_update_status(bd);
-		}
+			brightness = bd->props.max_brightness;
+		else if (brightness < 0)
+			brightness = 0;
+
+		pr_debug("set brightness to %lu\n", brightness);
+		bd->props.brightness = brightness;
+		rc = backlight_update_status(bd);
 	}
 	mutex_unlock(&bd->ops_lock);
 
