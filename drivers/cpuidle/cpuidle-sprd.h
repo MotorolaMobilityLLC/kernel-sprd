bb6d3b9f2050c331db1ee3beba5774432fca6f9f
Jingchao Ye <jingchao.ye@spreadtrum.com>
2019-07-25 17:44:47 +0800

    Bug #1122398 cpuidle: sprd: Add SPRD cpuidle driver for pike2
    [root cause  ] lack of sprd cpuidle driver
    [changes     ] add sprd cpuidle driver
    [side effects] NO
    [self test   ]:bootup
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:No
    [confirm dependent commit]:Yes
    [board]:sc7731e-1h10
    [test case]: bootup and hotplug
    [reviewers   ] jingchao.ye
    [change_type ] feature
    [tag_product ] specific
    
    To support pike2 light sleep function, we should add
    Spreadtrum cpuidle driver.
    
    Change-Id: I5e2290b41e36be98999558b40a07c5d41f76417f
    Signed-off-by: Jingchao Ye <jingchao.ye@spreadtrum.com>

---
+++ drivers/cpuidle/cpuidle-sprd.h
@@ -0,0 +1,13 @@
+#ifndef __CPUIDLE_SPRD_H
+#define __CPUIDLE_SPRD_H
+
+struct sprd_cpuidle_operations {
+	char name[16];
+
+	void (*init)(void);
+	void (*light_en)(void);
+	void (*light_dis)(void);
+	void (*doze_en)(void);
+	void (*doze_dis)(void);
+};
+#endif
