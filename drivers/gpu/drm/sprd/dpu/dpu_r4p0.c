28ffb2a7b57871451affd2a1beea8dffd1d77f33
Albert Zhang <albert.zhang@spreadtrum.com>
2019-08-02 15:23:21 +0800

    Bug #1134080 drm/sprd: add dpu r4p0 round corner param
    [root cause  ] add dpu r4p0 round corner param
    [changes     ] display driver
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums512-1h10
    [test case]:display work ok
    [reviewers   ] thomas.chai
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add dpu r4p0 round corner param.
    
    Change-Id: I8d0b93330405e46e72110f7509e846171892cabb
    Signed-off-by: Albert Zhang <albert.zhang@spreadtrum.com>

--- drivers/gpu/drm/sprd/dpu/dpu_r4p0.c
+++ drivers/gpu/drm/sprd/dpu/dpu_r4p0.c
@@ -327,7 +327,7 @@ static int max_vsync_count;
 static int vsync_count;
 static struct sprd_dpu_layer wb_layer;
 static int wb_xfbc_en = 1;
-static int sprd_corner_radius;
+static int corner_radius;
 module_param(wb_xfbc_en, int, 0644);
 module_param(max_vsync_count, int, 0644);
 
@@ -351,10 +351,10 @@ static int dpu_parse_dt(struct dpu_context *ctx,
 	struct device_node *qos_np = NULL;
 
 	ret = of_property_read_u32(np, "sprd,corner-radius",
-					&sprd_corner_radius);
+					&corner_radius);
 	if (!ret)
 		pr_info("round corner support, radius = %d.\n",
-					sprd_corner_radius);
+					corner_radius);
 
 	qos_np = of_parse_phandle(np, "sprd,qos", 0);
 	if (!qos_np)
@@ -388,15 +388,15 @@ static void dpu_corner_init(struct dpu_context *ctx)
 	struct dpu_reg *reg = (struct dpu_reg *)ctx->base;
 	int i;
 
-	reg->corner_config = (sprd_corner_radius << 24) |
-				(sprd_corner_radius << 8);
+	reg->corner_config = (corner_radius << 24) |
+				(corner_radius << 8);
 
-	for (i = 0; i < sprd_corner_radius; i++) {
+	for (i = 0; i < corner_radius; i++) {
 		reg->top_corner_lut_addr = i;
-		reg->top_corner_lut_wdata = corner_param[sprd_corner_radius][i];
+		reg->top_corner_lut_wdata = corner_param[corner_radius][i];
 		reg->bot_corner_lut_addr = i;
 		reg->bot_corner_lut_wdata =
-			corner_param[sprd_corner_radius][sprd_corner_radius - i - 1];
+			corner_param[corner_radius][corner_radius - i - 1];
 	}
 
 	reg->corner_config |= (TOP_CORNER_EN | BOT_CORNER_EN);
@@ -843,7 +843,7 @@ static int dpu_init(struct dpu_context *ctx)
 
 	dpu_write_back_config(ctx);
 
-	if (sprd_corner_radius)
+	if (corner_radius)
 		dpu_corner_init(ctx);
 
 	dpu_dvfs_task_init(ctx);
