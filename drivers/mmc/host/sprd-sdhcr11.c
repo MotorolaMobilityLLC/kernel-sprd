9ed6b54c2e3ef79ad3f588abae382b00eedb60ec
Lin Zhao <zhaolin@spreadst.com>
2019-09-19 18:45:41 +0800

    Bug #1170240 mmc: sprd: free the resource when error occurs
    [root cause  ] resource leak
    [changes     ] drivers/mmc/host/sprd-sdhcr11.c
    [side effects] mmc
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sp9863a
    [test case]: selftest
    [reviewers   ] Orson.Zhai
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Free the resource.
    
    Change-Id: Ic23d889e9e663ebf36b67ab1fa9e4a42a8d2de56
    Signed-off-by: Lin Zhao <zhaolin@spreadst.com>

--- drivers/mmc/host/sprd-sdhcr11.c
+++ drivers/mmc/host/sprd-sdhcr11.c
@@ -1606,8 +1606,10 @@ static int sprd_sdhc_execute_tuning(struct mmc_host *mmc, u32 opcode)
 	if (!ranges)
 		return -ENOMEM;
 	value_t = kmalloc_array(length + 1, sizeof(*value_t), GFP_KERNEL);
-	if (!value_t)
+	if (!value_t) {
+		kfree(ranges);
 		return -ENOMEM;
+	}
 
 	spin_lock_irqsave(&host->lock, flags);
 	dll_dly = host->dll_dly;
