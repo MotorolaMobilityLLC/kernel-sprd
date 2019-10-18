d030a5f04457974b2511b69cb19b71414f80f626
Weidong Guan <weidong.guan@unisoc.com>
2019-10-18 10:06:39 +0800

    Bug #1181605 sprd: scripts: change the path of aiaiai to get compiled information
    [root cause  ] the path of achieving information will be deleted
    [changes     ] new logic of aiaiai_info.py
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: all
    [test case]: aiaiai run normally, compile pass
    [reviewers   ] ruifeng.zhang nianfu.bai
    [change_type ] feature
    [tag_product ] common
    
    the sprd-board-config will be removed, but aiaiai detection system
    obtains the compile information from this path, so the logic to getting
    compile information need to be changed.
    then, modify the logic to get information from sprd_*_defconfig,
    while toochian information config is defined in Kconfig and added to
    corresponding defconfigs.
    
    Change-Id: I7997559e75f744450473a7fbfb4d7e14a739a1ad
    Signed-off-by: Weidong Guan <weidong.guan@unisoc.com>

--- scripts/sprd/aiaiai_info.py
+++ scripts/sprd/aiaiai_info.py
@@ -2,9 +2,7 @@
 
 import os
 import sys
-import re
 
-defconfig_list=[]
 boardconfig_list=[]
 def main():
 	result=[]
@@ -15,72 +13,46 @@ def main():
 		print("3:",file_name_list)  #all subfiles
 		"""
 		for filename in file_name_list:
-			if "sprd" in filename and "defconfig" in filename:
+			if "sprd" in filename and "defconfig" in filename and "minimum" not in filename and "all" not in filename:
+				toolchain=""
+				cross_compile=""
+				kernel_defconfig=""
+
 				apath = os.path.join(maindir, filename)
 				result.append(apath)
 
-				#apath value example: arch/arm64/configs/sprd_all_defconfig
+				#apath value example: arch/arm64/configs/sprd_sharkl3_defconfig
 				arch = (apath.split("/").pop(1))
-				name = (apath.split("/").pop(3))
-				path = apath
+				if arch == "arm":
+					cross_compile= "arm-linux-androideabi-"
+				elif arch == "arm64":
+					cross_compile= "aarch64-linux-android-"
 
-				defconfig_list.append(name+","+arch)
+				kernel_defconfig = filename
 
-	for maindir,subdir,file_name_list in os.walk("sprd-board-config"):
-		toolchain=""
-		cross_compile=""
-		kernel_defconfig=""
-		"""
-		print("1:",maindir) #current dir
-		print("2:",subdir) # all subdirs
-		print("3:",file_name_list)  #all subfiles
-		"""
-		for filename in file_name_list:
-			apath = os.path.join(maindir, filename)
-			result.append(apath)
+				f=open(apath,'r')
+				lines = f.readlines()
+				for j in range(len(lines)):
+					if '#' in lines[j]:
+						continue
+					if 'CONFIG_COMPILE_TOOL' in lines[j]:
+						tmp_toolchain = lines[j].split("=").pop().strip('"\n ')
+						if tmp_toolchain == "all":
+							tmp_toolchain = "clang,gcc"
+						break
 
-			f=open(apath,'r')
-			lines = f.readlines()
-			for j in range(len(lines)):
-				if '#' in lines[j]:
-					continue
-
-				if '"' in lines[j][-2:]:
-					tmpline = lines[j][:-2]
-				else:
-					tmpline = lines[j][:-1]
-
-				matchObj = re.search(r'sprd(.*)defconfig', tmpline)
-				if matchObj:
-					kernel_defconfig=matchObj.group()
-
-				matchObj = re.search(r'KERNEL_ARCH(.*)=(.*)', tmpline)
-				if matchObj:
-					arch=matchObj.group(2).strip('"').strip()
-
-				if 'CROSS_COMPILE' in tmpline:
-					matchObj = re.search(r'(.*)-' ,tmpline.split("/").pop())
-					if matchObj:
-						cross_compile=matchObj.group(0)
-
-				if 'BSP_MAKE_EXTRA_ARGS' in tmpline and toolchain != 'clang':
-					toolchain="clang"
-				elif toolchain=="":
-					toolchain="gcc"
-
-		if kernel_defconfig!="" and arch!="" and cross_compile!="" and toolchain!="":
-			boardconfig_list.append(kernel_defconfig+","+arch+","+cross_compile+","+toolchain)
+				if kernel_defconfig != "" and arch != "" and cross_compile != "" and tmp_toolchain != "":
+					for toolchain in tmp_toolchain.split(","):
+						boardconfig_list.append(kernel_defconfig+","+arch+","+cross_compile+","+toolchain)
 
 	formatList = list(set(boardconfig_list))
 	formatList.sort()
 
 	string_output=""
 	for boardconfig in formatList:
-		for defconfig in defconfig_list:
-			two_head_string=boardconfig.split(",").pop(0)+","+boardconfig.split(",").pop(1)
-			if defconfig == two_head_string:
-				string_output+=boardconfig+" "
+		string_output+=boardconfig+" "
 
 	print(string_output)
+
 if __name__ == '__main__':
     main()
