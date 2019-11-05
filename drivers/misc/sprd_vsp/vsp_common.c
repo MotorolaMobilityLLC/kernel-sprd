36751d75051448a0b925d6949a4bb0b86500f366
Lei Hu <lei.hu@spreadtrum.com>
2019-11-05 19:53:22 +0800

    Bug #1183725 misc: sprd_vsp: add protection to avoid VSP crash in monkey while accessing register when vsp_eb disabled
    [root cause  ] VSP crash in monkey while accessing reg with vsp_eb disabled
    [changes     ] add protection, if vsp_eb disabled, enable it firstly
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl3
    [test case]:video playback
    [reviewers   ]leon.li
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add protection to avoid VSP crash in monkey while accessing register when vsp_eb disabled.
    before iommu access vsp reg, check the status, if vsp_eb disabled,
    enable it firstly.
    
    Change-Id: I702b4efb6608cf0466f25534b463d40681d6a061
    Signed-off-by: Lei Hu <lei.hu@spreadtrum.com>

--- drivers/misc/sprd_vsp/vsp_common.c
+++ drivers/misc/sprd_vsp/vsp_common.c
@@ -22,6 +22,9 @@
 #include <linux/sprd_ion.h>
 #include <linux/uaccess.h>
 #include "vsp_common.h"
+#if IS_ENABLED(CONFIG_SPRD_VSP_PW_DOMAIN)
+#include <linux/mfd/syscon.h>
+#endif
 
 #ifdef pr_fmt
 #undef pr_fmt
@@ -176,9 +179,47 @@ int vsp_get_iova(struct vsp_dev_t *vsp_hw_dev,
 {
 	int ret = 0;
 	struct sprd_iommu_map_data iommu_map_data;
-	u32 power_state1, vsp_eb_reg;
+        u32 power_state1, vsp_eb_reg;
+#if IS_ENABLED(CONFIG_SPRD_VSP_PW_DOMAIN)
+	u32 mm_vsp_ahb_reg;
+#endif
 
 	vsp_clk_enable(vsp_hw_dev);
+#if IS_ENABLED(CONFIG_SPRD_VSP_PW_DOMAIN)
+	if (vsp_hw_dev->version == SHARKL3) {
+		regmap_read(regs[PMU_PWR_STATUS].gpr, regs[PMU_PWR_STATUS].reg,
+			&power_state1);
+		regmap_read(regs[VSP_DOMAIN_EB].gpr, regs[VSP_DOMAIN_EB].reg, &vsp_eb_reg);
+		if ((vsp_eb_reg & 0x4000) != 0x4000) {
+			ret = regmap_update_bits(regs[VSP_DOMAIN_EB].gpr, regs[VSP_DOMAIN_EB].reg,
+						BIT(14), BIT(14));
+			if (ret) {
+				pr_err("iova regmap_update_bits failed %s, %d\n",
+					__func__, __LINE__);
+			}
+			regmap_read(regs[VSP_DOMAIN_EB].gpr, regs[VSP_DOMAIN_EB].reg, &vsp_eb_reg);
+		}
+
+		if (((power_state1 & 0x1f) == 0x0) &&
+			((vsp_eb_reg & 0x4000) == 0x4000)) {
+			regmap_read(regs[RESET].gpr, 0x0, &mm_vsp_ahb_reg);
+			if ((mm_vsp_ahb_reg & 0x3) != 0x3) {
+                                pr_info("mm vsp ahb 0x%x\n", mm_vsp_ahb_reg);
+				ret = regmap_update_bits(regs[RESET].gpr, 0x0, BIT(1) | BIT(0),
+							BIT(1) | BIT(0));
+				if (ret) {
+					pr_err("vsp_iova regmap_update_bits failed %s, %d\n",
+						__func__, __LINE__);
+				}
+			}
+		} else {
+			pr_info("offset 0x%x,power 0x%x,offset 0x%x,vsp eb 0x%x\n",
+				regs[PMU_PWR_STATUS].reg, power_state1,
+				regs[VSP_DOMAIN_EB].reg, vsp_eb_reg);
+		}
+	}
+#endif
+
 	sprd_iommu_resume(vsp_hw_dev->vsp_dev);
 	ret = sprd_ion_get_buffer(mapdata->fd, NULL,
 					&(iommu_map_data.buf),
