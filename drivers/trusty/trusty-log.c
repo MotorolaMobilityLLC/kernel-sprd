51c886b26cf53811cc98b99cd21cdb62804185c0
aijun.sun <aijun.sun@spreadtrum.com>
2019-11-23 19:51:55 +0800

    Bug #1212975 trusty: support dynamic config for log size
    [root cause  ] firephoenix requirement
    [changes     ] refactor log buffer allocation
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:s9863a-1h10, sp7731e-1h10
    [test case]: system boot-up, trusty log config
    [reviewers   ]
    [change_type ] feature
    [tag_product ] common
    
    This refactors trusty log buffer allocator to use scatter memory
    pages. A sysfs file is provided to configure trusty log size
    dynamically.
    
    Change-Id: I59420c52fb3636a8395bd8f97c1d7d5d56f76d88
    Signed-off-by: aijun.sun <aijun.sun@spreadtrum.com>

--- drivers/trusty/trusty-log.c
+++ drivers/trusty/trusty-log.c
@@ -28,7 +28,8 @@
 #define SMC_SC_SYSCTL_SET_LOGLEVEL	SMC_STDCALL_NR(SMC_ENTITY_SYSCTL, 2)
 #define SMC_SC_SYSCTL_GET_LOGLEVEL	SMC_STDCALL_NR(SMC_ENTITY_SYSCTL, 3)
 
-#define TRUSTY_LOG_SIZE (PAGE_SIZE * 4)
+#define TRUSTY_LOG_DEFAULT_SIZE (PAGE_SIZE * 4)
+#define TRUSTY_LOG_MAX_SIZE	(PAGE_SIZE * 32)
 #define TRUSTY_LINE_BUFFER_SIZE 256
 
 struct trusty_log_state {
@@ -43,7 +44,11 @@ struct trusty_log_state {
 	struct log_rb *log;
 	uint32_t get;
 
-	struct page *log_pages;
+	uint64_t *pfn_list;
+	struct page **pages;
+	int num_pages;
+
+	int dumpable;
 
 	struct notifier_block call_notifier;
 	struct notifier_block panic_notifier;
@@ -72,6 +77,11 @@ static void trusty_dump_logs(struct trusty_log_state *s)
 	uint32_t get, put, alloc;
 	int read_chars;
 
+	if (!s->dumpable) {
+		pr_err_once("Trusty log currently is not dumpable\n");
+		return;
+	}
+
 	BUG_ON(!is_power_of_2(log->sz));
 
 	/*
@@ -98,7 +108,7 @@ static void trusty_dump_logs(struct trusty_log_state *s)
 		 * have been corrupted by the producer.
 		 */
 		if (alloc - get > log->sz) {
-			pr_err("trusty: log overflow.");
+			pr_err("trusty: log overflow(sz %d)\n", log->sz);
 			get = alloc - log->sz;
 			continue;
 		}
@@ -207,7 +217,8 @@ static ssize_t trusty_loglevel_show(struct device *dev,
 	return scnprintf(buf, PAGE_SIZE, "%d\n", result);
 }
 
-static DEVICE_ATTR(trusty_loglevel, 0660, trusty_loglevel_show, trusty_loglevel_store);
+static DEVICE_ATTR(trusty_loglevel, 0660,
+		   trusty_loglevel_show, trusty_loglevel_store);
 
 
 static ssize_t trusty_console_show(struct device *dev,
@@ -251,16 +262,219 @@ static ssize_t trusty_console_store(struct device *dev,
 	return strnlen(buf, count);
 
 }
-static DEVICE_ATTR(trusty_console, 0660, trusty_console_show, trusty_console_store);
+static DEVICE_ATTR(trusty_console, 0660,
+		   trusty_console_show, trusty_console_store);
 
+static void trusty_log_free_pages(struct page **pages, size_t num_pages,
+				  void *va)
+{
+	size_t n = num_pages;
 
+	if (va != NULL)
+		vm_unmap_ram(va, num_pages);
+
+	while (n--)
+		__free_page(pages[n]);
+	kfree(pages);
+}
+
+static struct page **trusty_log_alloc_pages(size_t size, void **va)
+{
+	struct page *page, **pages;
+	void *addr;
+	int i, n;
+	int result = 0;
+
+	if (size == 0 || size > (TRUSTY_LOG_MAX_SIZE >> PAGE_SHIFT))
+		return ERR_PTR(-EINVAL);
+
+	n = size;
+	pages = kmalloc_array(n, sizeof(struct page *), GFP_KERNEL);
+	if (!pages)
+		return ERR_PTR(-ENOMEM);
+
+	for (i = 0; i < n; i++) {
+		page = alloc_page(GFP_KERNEL | __GFP_HIGHMEM);
+		if (!page) {
+			result = -ENOMEM;
+			goto err;
+		}
+		pages[i] = page;
+	}
+
+	addr = vm_map_ram(pages, n, -1, PAGE_KERNEL);
+	if (!addr) {
+		result = -ENOMEM;
+		goto err;
+	}
+	*va = addr;
+
+	return pages;
+
+err:
+	trusty_log_free_pages(pages, i, NULL);
+	return ERR_PTR(result);
+}
+
+static void *trusty_log_register_buf(struct device *trusty_dev,
+				     struct page **pages, size_t num_pages)
+{
+	uint64_t *pfn_list;
+	u32 high_4bytes;
+	phys_addr_t pa;
+	int n = num_pages;
+	int result = 0;
+
+	if (n * sizeof(uint64_t) > PAGE_SIZE)
+		return ERR_PTR(-EINVAL);
+
+	pfn_list = (uint64_t *)__get_free_page(GFP_KERNEL | __GFP_ZERO);
+	if (!pfn_list)
+		return ERR_PTR(-ENOMEM);
+
+	while (n--)
+		pfn_list[n] = page_to_pfn(pages[n]);
+
+	pa = virt_to_phys(pfn_list);
+#ifdef CONFIG_PHYS_ADDR_T_64BIT
+	high_4bytes = (u32)(pa >> 32);
+#else
+	high_4bytes = 0;
+#endif
+	result = trusty_std_call32(trusty_dev, SMC_SC_SHARED_LOG_ADD,
+				   (u32)pa, high_4bytes, num_pages);
+	if (result < 0) {
+		pr_err("Trusty std call SMC_SC_SHARED_LOG_ADD failed(%d)\n",
+			result);
+		free_page((unsigned long)pfn_list);
+		return ERR_PTR(result);
+	}
+
+	return pfn_list;
+}
+
+static int trusty_log_unregister_buf(struct device *trusty_dev, void *buf)
+{
+	int result;
+	u32 high_4bytes;
+	phys_addr_t pa = virt_to_phys(buf);
+
+#ifdef CONFIG_PHYS_ADDR_T_64BIT
+	high_4bytes = (u32)(pa >> 32);
+#else
+	high_4bytes = 0;
+#endif
+	result = trusty_std_call32(trusty_dev, SMC_SC_SHARED_LOG_RM,
+				   (u32)pa, high_4bytes, 0);
+	if (result) {
+		pr_err("Trusty std call SMC_SC_SHARED_LOG_RM failed(%d)\n",
+		       result);
+		return result;
+	}
+
+	free_page((unsigned long)buf);
+
+	return 0;
+}
+
+#define trusty_log_dump_lock(s) ({ s->dumpable = 0; })
+#define trusty_log_dump_unlock(s) ({ s->dumpable = 1; })
+
+static ssize_t trusty_logsize_store(struct device *dev,
+		struct device_attribute  *attr, const char *buf, size_t count)
+{
+	struct trusty_log_state *s = NULL;
+	struct page **pages = NULL;
+	void *log_buf, *pfn_list;
+	size_t size, n;
+	int result;
+
+	result = kstrtoul(buf, 0, (unsigned long *)&size);
+	if (result) {
+		pr_err("Invalid value argument(%s)!\n", buf);
+		return -EINVAL;
+	}
+
+	s = platform_get_drvdata(to_platform_device(dev));
+
+	/* Disable log dumpping util log buffer resizing is done */
+	trusty_log_dump_lock(s);
+
+	n = size >> PAGE_SHIFT;
+	/*
+	 * Allocate and register new buffer pages. Doing this before free and
+	 * unregister the old buffer is for easily rollback in case of
+	 * allocation/register failure
+	 */
+	pages = trusty_log_alloc_pages(n, &log_buf);
+	if (IS_ERR(pages)) {
+		pr_err("Trusty log allocate buffer error(%ld)\n",
+		       PTR_ERR(pages));
+		goto _exit;
+	}
+
+	pfn_list = trusty_log_register_buf(s->trusty_dev,
+				pages, n);
+	if (IS_ERR(pfn_list)) {
+		pr_err("Trusty log register buffer failed(%ld)\n",
+		       PTR_ERR(pfn_list));
+		goto _exit1;
+	}
+
+	/* Unregister log buf and free buffer pages */
+	result = trusty_log_unregister_buf(s->trusty_dev, s->pfn_list);
+	if (result) {
+		pr_err("Trusty log unregister buffer error(%d)\n", result);
+		goto _exit1;
+	}
+	trusty_log_free_pages(s->pages, s->num_pages, s->log);
+
+	s->pages = pages;
+	s->num_pages = n;
+	s->pfn_list = pfn_list;
+	s->log = log_buf;
+	s->get = 0;
+_exit:
+	trusty_log_dump_unlock(s);
+	return count;
+_exit1:
+	trusty_log_free_pages(pages, n, log_buf);
+	trusty_log_dump_unlock(s);
+	return count;
+
+}
+
+static ssize_t trusty_logsize_show(struct device *dev,
+		struct device_attribute  *attr, char *buf)
+{
+	struct trusty_log_state *s = NULL;
+
+	s = platform_get_drvdata(to_platform_device(dev));
+
+	return scnprintf(buf, PAGE_SIZE, "0x%x\n", s->num_pages << PAGE_SHIFT);
+}
+
+static DEVICE_ATTR(trusty_logsize, 0660,
+		   trusty_logsize_show, trusty_logsize_store);
+
+static struct attribute *trusty_log_attrs[] = {
+	&dev_attr_trusty_logsize.attr,
+	&dev_attr_trusty_loglevel.attr,
+	&dev_attr_trusty_console.attr,
+	NULL
+};
+
+static const struct attribute_group trusty_log_attr_group = {
+	.attrs = trusty_log_attrs,
+};
 
 static int trusty_log_probe(struct platform_device *pdev)
 {
 	struct trusty_log_state *s;
+	struct page **pages;
+	void *log_buf, *pfn_list;
+	size_t num_pages;
 	int result;
-	phys_addr_t pa;
-	u32 high_4bytes;
 
 	dev_dbg(&pdev->dev, "%s\n", __func__);
 	if (!trusty_supports_logging(pdev->dev.parent)) {
@@ -277,29 +491,28 @@ static int trusty_log_probe(struct platform_device *pdev)
 	s->dev = &pdev->dev;
 	s->trusty_dev = s->dev->parent;
 	s->get = 0;
-	s->log_pages = alloc_pages(GFP_KERNEL | __GFP_ZERO,
-				   get_order(TRUSTY_LOG_SIZE));
-	if (!s->log_pages) {
-		result = -ENOMEM;
-		goto error_alloc_log;
-	}
-	s->log = page_address(s->log_pages);
 
-	pa = page_to_phys(s->log_pages);
-#ifdef CONFIG_PHYS_ADDR_T_64BIT
-	high_4bytes = (u32)(pa >> 32);
-#else
-	high_4bytes = 0;
-#endif
-	result = trusty_std_call32(s->trusty_dev,
-				   SMC_SC_SHARED_LOG_ADD,
-				   (u32)(pa), high_4bytes,
-				   TRUSTY_LOG_SIZE);
-	if (result < 0) {
-		pr_err("trusty std call (SMC_SC_SHARED_LOG_ADD) failed: %d %pa\n",
-		       result, &pa);
-		goto error_std_call;
+	num_pages = TRUSTY_LOG_DEFAULT_SIZE >> PAGE_SHIFT;
+	pages = trusty_log_alloc_pages(num_pages, &log_buf);
+	if (IS_ERR(pages)) {
+		result = PTR_ERR(pages);
+		goto error_alloc_pages;
 	}
+
+	pfn_list = trusty_log_register_buf(s->trusty_dev, pages, num_pages);
+	if (IS_ERR(pfn_list)) {
+		dev_err(&pdev->dev, "register failed(%ld)\n",
+			PTR_ERR(pfn_list));
+		result = PTR_ERR(pfn_list);
+		goto error_register_buf;
+	}
+
+	s->pages = pages;
+	s->num_pages = num_pages;
+	s->pfn_list = pfn_list;
+	s->log = log_buf;
+	s->get = 0;
+	s->dumpable = 1;
 
 	s->call_notifier.notifier_call = trusty_log_call_notify;
 	result = trusty_call_notifier_register(s->trusty_dev,
@@ -320,37 +533,28 @@ static int trusty_log_probe(struct platform_device *pdev)
 	}
 	platform_set_drvdata(pdev, s);
 
-	result = device_create_file(s->dev, &dev_attr_trusty_loglevel);
+	result = sysfs_create_group(&pdev->dev.kobj, &trusty_log_attr_group);
 	if (result)
-		dev_err(&pdev->dev, "failed to register device file\n");
-
-	result = device_create_file(s->dev, &dev_attr_trusty_console);
-	if (result)
-		dev_err(&pdev->dev, "failed to register device file\n");
-
-
+		dev_err(&pdev->dev, "Failed to create debug files\n");
 
 	return 0;
 
 error_panic_notifier:
 	trusty_call_notifier_unregister(s->trusty_dev, &s->call_notifier);
 error_call_notifier:
-	trusty_std_call32(s->trusty_dev, SMC_SC_SHARED_LOG_RM,
-			  (u32)pa, high_4bytes, 0);
-error_std_call:
-	__free_pages(s->log_pages, get_order(TRUSTY_LOG_SIZE));
-error_alloc_log:
+	trusty_log_unregister_buf(s->trusty_dev, pfn_list);
+error_register_buf:
+	trusty_log_free_pages(pages, num_pages, log_buf);
+error_alloc_pages:
 	kfree(s);
 error_alloc_state:
 	return result;
 }
 
+
 static int trusty_log_remove(struct platform_device *pdev)
 {
-	int result;
-	u32 high_4bytes;
 	struct trusty_log_state *s = platform_get_drvdata(pdev);
-	phys_addr_t pa = page_to_phys(s->log_pages);
 
 	dev_dbg(&pdev->dev, "%s\n", __func__);
 
@@ -358,19 +562,13 @@ static int trusty_log_remove(struct platform_device *pdev)
 					 &s->panic_notifier);
 	trusty_call_notifier_unregister(s->trusty_dev, &s->call_notifier);
 
-#ifdef CONFIG_PHYS_ADDR_T_64BIT
-	high_4bytes = (u32)(pa >> 32);
-#else
-	high_4bytes = 0;
-#endif
-	result = trusty_std_call32(s->trusty_dev, SMC_SC_SHARED_LOG_RM,
-				   (u32)pa, high_4bytes, 0);
-	if (result) {
-		pr_err("trusty std call (SMC_SC_SHARED_LOG_RM) failed: %d\n",
-		       result);
-	}
-	__free_pages(s->log_pages, get_order(TRUSTY_LOG_SIZE));
+	trusty_log_unregister_buf(s->trusty_dev, s->pfn_list);
+
+	trusty_log_free_pages(s->pages, s->num_pages, s->log);
+
 	kfree(s);
+
+	sysfs_remove_group(&pdev->dev.kobj, &trusty_log_attr_group);
 
 	return 0;
 }
