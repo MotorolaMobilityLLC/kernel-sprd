289f35f1977396cdadf3a3561c8f42ee8a7d658a
Jingxiang Li <jingxiang.li@spreadtrum.com>
2019-11-25 16:30:08 +0800

    Bug #1209438 misc: sprdwcn: wcn_check tool read more data erro
    [root cause  ] sdio DT read use the dma,so the buf need kmalloced
    [changes     ] change the heap mem to kmalloc
    [side effects] No
    [self test   ]: Yes
    [download normally]: Yes
    [power on/off normally]: Yes
    [do common repository/branch inspection]: Yes
    [is there dependence]: No
    [confirm dependent commit]: No
    [board]: ums512_1h10
    [test case]: wcn_check -l
    [reviewers   ] Songhe.Wei
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    This patch is to fix the wcn dt read erro.
    Sdio DT read use the dma, so the buf need kmalloced.
    
    Signed-off-by: Jingxiang Li <jingxiang.li@spreadtrum.com>
    
    Conflicts:
    	drivers/misc/sprdwcn/platform/wcn_debugfs.c
    
    Change-Id: I0db56ed3c9188d3a27406b4e06f627f74b7756f7

--- drivers/misc/sprdwcn/platform/wcn_debugfs.c
+++ drivers/misc/sprdwcn/platform/wcn_debugfs.c
@@ -23,6 +23,7 @@
 #include "../include/wcn_dbg.h"
 #include "../sleep/slp_mgr.h"
 
+#define WCN_REG_MAX_LEN 256
 struct wcn_reg_ctl {
 	unsigned int addr;
 	unsigned int len;
@@ -33,45 +34,62 @@ struct wcn_reg_ctl {
 	 * but the frame size is larger than 2048
 	 * bytes is not permission in kernel to android
 	 */
-	unsigned int value[256];
+	unsigned int value[WCN_REG_MAX_LEN];
 };
 
 static ssize_t read_wcn_reg(struct file *file, char __user *user_buf,
 			     size_t count, loff_t *ppos)
 {
-	struct wcn_reg_ctl reg_rd;
+	struct wcn_reg_ctl *reg_rd;
 	int i;
 
 	WCN_INFO("wsh __read_wcn_reg\n");
 
-	if (copy_from_user(&reg_rd, user_buf, sizeof(reg_rd))) {
+	reg_rd = kmalloc(sizeof(*reg_rd), GFP_KERNEL);
+	if (!reg_rd) {
+		WCN_INFO("%s alloc erro\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (copy_from_user(reg_rd, user_buf, sizeof(*reg_rd))) {
 		WCN_ERR("reg value copy's ret value is -eFAULT\n");
+		kfree(reg_rd);
 		return -EFAULT;
 	}
 
+	if (reg_rd->len > WCN_REG_MAX_LEN) {
+		WCN_INFO("%s len is to long:%d\n", __func__, reg_rd->len);
+		kfree(reg_rd);
+		return -EPERM;
+	}
+
 	/* rw_direct SDIO */
-	if (reg_rd.rw_extended == 0) {
-		for (i = 0; i < reg_rd.len; i++)
-			sprdwcn_bus_aon_readb(reg_rd.addr + i,
-					(unsigned char *) &reg_rd.value[i]);
+	if (reg_rd->rw_extended == 0) {
+		for (i = 0; i < reg_rd->len; i++)
+			sprdwcn_bus_aon_readb(
+					reg_rd->addr + i,
+					(unsigned char *)&reg_rd->value[i]);
 	} else {
 		/* rw_extended reg */
-		switch (reg_rd.len) {
+		switch (reg_rd->len) {
 		case 1:
-			sprdwcn_bus_reg_read(reg_rd.addr, &reg_rd.value[0], 4);
+			sprdwcn_bus_reg_read(reg_rd->addr,
+					     &reg_rd->value[0], 4);
 			break;
 		default:
-			sprdwcn_bus_direct_read(reg_rd.addr, reg_rd.value,
-						reg_rd.len * 4);
+			sprdwcn_bus_direct_read(reg_rd->addr, reg_rd->value,
+						reg_rd->len * sizeof(int));
 			break;
 		}
 	}
 
-
-	if (copy_to_user(user_buf, &reg_rd, sizeof(reg_rd))) {
+	if (copy_to_user(user_buf, reg_rd, sizeof(*reg_rd))) {
 		WCN_ERR("reg copy_to_user ret value is -eFAULT\n");
+		kfree(reg_rd);
 		return -EFAULT;
 	}
+
+	kfree(reg_rd);
 
 	return count;
 }
@@ -79,33 +97,49 @@ static ssize_t read_wcn_reg(struct file *file, char __user *user_buf,
 static ssize_t write_wcn_reg(struct file *file, const char __user *user_buf,
 			      size_t count, loff_t *ppos)
 {
-	struct wcn_reg_ctl reg_wr;
+	struct wcn_reg_ctl *reg_wr;
 	int i;
 
-	WCN_INFO("wsh _write_wcn_reg\n");
-	if (copy_from_user(&reg_wr, user_buf, sizeof(reg_wr))) {
+	WCN_INFO("_write_wcn_reg\n");
+
+	reg_wr = kmalloc(sizeof(*reg_wr), GFP_KERNEL);
+	if (!reg_wr) {
+		WCN_INFO("%s alloc erro\n", __func__);
+		return -ENOMEM;
+	}
+
+	if (copy_from_user(reg_wr, user_buf, sizeof(*reg_wr))) {
 		WCN_ERR("%s copy's ret value is -eFAULT\n", __func__);
+		kfree(reg_wr);
 		return -EFAULT;
 	}
 
+	if (reg_wr->len > WCN_REG_MAX_LEN) {
+		WCN_INFO("%s len is to long:%d\n", __func__, reg_wr->len);
+		kfree(reg_wr);
+		return -EPERM;
+	}
+
 	/* rw_direct SDIO */
-	if (reg_wr.rw_extended == 0) {
-		for (i = 0; i < reg_wr.len; i++)
-			sprdwcn_bus_aon_writeb(reg_wr.addr + i,
-					       (unsigned char)reg_wr.value[i]);
+	if (reg_wr->rw_extended == 0) {
+		for (i = 0; i < reg_wr->len; i++)
+			sprdwcn_bus_aon_writeb(reg_wr->addr + i,
+					       (unsigned char)reg_wr->value[i]);
 	} else {
 		/* rw_extended reg */
-		switch (reg_wr.len) {
+		switch (reg_wr->len) {
 		case 1:
-			sprdwcn_bus_reg_write(reg_wr.addr, &reg_wr.value[0], 4);
+			sprdwcn_bus_reg_write(reg_wr->addr,
+					      &reg_wr->value[0], 4);
 			break;
 		default:
-			sprdwcn_bus_direct_write(reg_wr.addr, reg_wr.value,
-						 reg_wr.len * 4);
+			sprdwcn_bus_direct_write(reg_wr->addr, reg_wr->value,
+						 reg_wr->len * sizeof(int));
 			break;
 		}
 	}
 
+	kfree(reg_wr);
 
 	return count;
 
