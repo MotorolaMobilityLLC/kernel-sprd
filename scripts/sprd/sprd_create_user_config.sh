2e183b2cf3ca96ff7c36b20997be14b931e43868
Ruifeng Zhang <ruifeng.zhang1@unisoc.com>
2019-12-13 14:24:16 +0800

    Bug #1221394 sprd: scripts: redefine the config scripts path
    [root cause  ] the opposite path has been changed
    [changes     ] redefine the config scripts path
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: all board
    [test case]: make config can add diffconfig normally
    [reviewers   ] nianfu.bai
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change the sprd_create_user_config.sh path, need redefine
    the config scripts path at the same time.
    
    Change-Id: I407263fe9ec3cef34dba56b18cc526747cdfa8c4
    Signed-off-by: Ruifeng Zhang <ruifeng.zhang1@unisoc.com>

--- scripts/sprd/sprd_create_user_config.sh
+++ scripts/sprd/sprd_create_user_config.sh
@@ -5,35 +5,37 @@ IFS=$'\n'
 FN="$2"
 SCR_PATH=`dirname $0`
 
+CONFIG_SCRIPTS=$SCR_PATH/../config
+
 for line in `cat "$FN"`
 do
 prefix=${line:0:3}
 
 if [ "$prefix" = "DEL" ]; then
 config=${line:11}
-./$SCR_PATH/config --file $1 -d $config
+./$CONFIG_SCRIPTS --file $1 -d $config
 
 elif [ "$prefix" = "VAL" ]; then
 len=`expr length $line`
 idx=`expr index $line "="`
 config=`expr substr "$line" 12 $[$idx-12]`
 val=`expr substr "$line" $[$idx+1] $len`
-./$SCR_PATH/config --file $1 --set-val $config $val
+./$CONFIG_SCRIPTS --file $1 --set-val $config $val
 
 elif [ "$prefix" = "STR" ]; then
 len=`expr length $line`
 idx=`expr index $line "="`
 config=`expr substr "$line" 12 $[$idx-12]`
 str=`expr substr "$line" $[$idx+1] $len`
-./$SCR_PATH/config --file $1 --set-str $config $str
+./$CONFIG_SCRIPTS --file $1 --set-str $config $str
 
 elif [ "$prefix" = "ADD" ]; then
 config=${line:11}
-./$SCR_PATH/config --file $1 -e $config
+./$CONFIG_SCRIPTS --file $1 -e $config
 
 elif [ "$prefix" = "MOD" ]; then
 config=${line:11}
-./$SCR_PATH/config --file $1 -m $config
+./$CONFIG_SCRIPTS --file $1 -m $config
 
 fi
 done
