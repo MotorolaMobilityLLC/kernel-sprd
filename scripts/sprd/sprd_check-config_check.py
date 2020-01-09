65c1149155676b4c094c089321a40576643932e9
Weidong Guan <weidong.guan@unisoc.com>
2020-01-09 10:13:42 +0800

    Bug #1232841 sprd: scripts: sprd-configs.txt temporarily does not count sharkle_64
    [root cause  ] sprd-configs.txt temporarily does not count sharkle_64
    [changes     ] sprd-configs.txt temporarily does not count sharkle_64
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sharkle
    [test case]: aiaiai pass
    [reviewers   ] ruifeng.zhang nianfu.bai
    [change_type ] feature_bugfix
    [tag_product ] common
    
    modify the logic of scripts/sprd/sprd_check-config_check.py,
    make sprd-configs.txt temporarily not count sharkle_64 now.
    
    Change-Id: I74635b9ca8179b7845a9683ffccfd88931ae1310
    Signed-off-by: Weidong Guan <weidong.guan@unisoc.com>

--- scripts/sprd/sprd_check-config_check.py
+++ scripts/sprd/sprd_check-config_check.py
@@ -132,6 +132,10 @@ def create_diffconfigs_dict():
                                 tmp_plat = 'sharkle32_fp'
                             else:
                                 tmp_plat = 'sharkle32'
+                        elif tmp_arch == 'arm64' and tmp_plat == 'sharkle':
+                            if kernel_version == 'kernel4.14':
+                                tmp_arch == ""
+                                tmp_plat == ""
                         elif tmp_arch == 'arm' and tmp_plat == 'sharkl3':
                             tmp_plat = 'sharkl3_32'
                         elif tmp_plat == 'pike2':
