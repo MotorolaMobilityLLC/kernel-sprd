b1c4ea7e5ff12c5aab808985bc4c6a7188bf4ccf
Yuri Yu <Yuri.Yu@unisoc.com>
2020-01-13 14:09:12 +0800

    Bug #1204267 drm/sprd: add prefix [drm] to all logs in sysfs_class.c
    [root cause  ] add prefix [drm] to all logs in sysfs_class.c
    [changes     ] sysfs_class.c
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:s9863a1h10
    [test case]:display ok.
    [reviewers   ] leon.he
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add prefix [drm] to all logs in sysfs_class.c
    
    Change-Id: I750205d33b9561afd6a7b67d9321180317cd0025
    Signed-off-by: Yuri Yu <Yuri.Yu@unisoc.com>

--- drivers/gpu/drm/sprd/sysfs/sysfs_class.c
+++ drivers/gpu/drm/sprd/sysfs/sysfs_class.c
@@ -20,11 +20,11 @@ struct class *display_class;
 
 static int __init display_class_init(void)
 {
-	pr_info("display class register\n");
+	pr_info("[drm] display class register\n");
 
 	display_class = class_create(THIS_MODULE, "display");
 	if (IS_ERR(display_class)) {
-		pr_err("Unable to create display class\n");
+		pr_err("[drm] Unable to create display class\n");
 		return PTR_ERR(display_class);
 	}
 
