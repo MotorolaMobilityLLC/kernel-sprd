48614e024065df51594fad8cf290714bd27fbdc0
zhanqing.zhu <zhanqing.zhu@unisoc.com>
2020-01-17 19:02:26 +0800

    Bug #1284884 trusty: configurate trusty driver to kernel module
    [root cause  ] meet the upgrade requirements for kernel5.4
    [changes     ] change CONFIG_TRUSTY=y to CONFIG_TRUSTY=m
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl3
    [test case]:abigail
    [reviewers   ]
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: Ib82ad3869681bff13e2ec46ad61dd186606f74d3
    Signed-off-by: zhanqing.zhu <zhanqing.zhu@unisoc.com>

---
+++ arch/arm64/include/asm/compiler.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_COMPILER_H
+#define __ASM_COMPILER_H
+
+/*
+ * This is used to ensure the compiler did actually allocate the register we
+ * asked it for some inline assembly sequences.  Apparently we can't trust the
+ * compiler from one version to another so a bit of paranoia won't hurt.  This
+ * string is meant to be concatenated with the inline asm string and will
+ * cause compilation to stop on mismatch.  (for details, see gcc PR 15089)
+ */
+#define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
+
+#endif	/* __ASM_COMPILER_H */
