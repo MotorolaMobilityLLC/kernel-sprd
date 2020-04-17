147cfa0fdf97bb334e0fa989045f196a12dbeb68
shiyao.suo <shiyao.suo@unisoc.com>
2020-04-17 15:44:41 +0800

    Bug #1297915 misc: sprd_vsp: add protection on get iova when domain_eb disabled
    [root cause  ] VSP crash in monkey while accessing reg with domain_eb disabled
    [changes     ] add protection, if domain_eb disabled, enable it firstly
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl5pro
    [test case]:video playback
    [reviewers   ] leon.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: Ica4efc22e3157993c9199d2a123cfc84008e2500

--- drivers/misc/sprd_vsp/vsp_common.c
+++ drivers/misc/sprd_vsp/vsp_common.c
@@ -179,9 +179,10 @@ int vsp_get_iova(struct vsp_dev_t *vsp_hw_dev,
 {
 	int ret = 0;
 	struct sprd_iommu_map_data iommu_map_data;
-        u32 power_state1, vsp_eb_reg;
+	u32 power_state1, vsp_eb_reg;
 #if IS_ENABLED(CONFIG_SPRD_VSP_PW_DOMAIN)
 	u32 mm_vsp_ahb_reg;
+	u32 ap_ahb_regs;
 #endif
 
 	vsp_clk_enable(vsp_hw_dev);
@@ -204,7 +205,7 @@ int vsp_get_iova(struct vsp_dev_t *vsp_hw_dev,
 			((vsp_eb_reg & 0x4000) == 0x4000)) {
 			regmap_read(regs[RESET].gpr, 0x0, &mm_vsp_ahb_reg);
 			if ((mm_vsp_ahb_reg & 0x3) != 0x3) {
-                                pr_info("mm vsp ahb 0x%x\n", mm_vsp_ahb_reg);
+				pr_info("mm vsp ahb 0x%x\n", mm_vsp_ahb_reg);
 				ret = regmap_update_bits(regs[RESET].gpr, 0x0, BIT(1) | BIT(0),
 							BIT(1) | BIT(0));
 				if (ret) {
@@ -216,6 +217,26 @@ int vsp_get_iova(struct vsp_dev_t *vsp_hw_dev,
 			pr_info("offset 0x%x,power 0x%x,offset 0x%x,vsp eb 0x%x\n",
 				regs[PMU_PWR_STATUS].reg, power_state1,
 				regs[VSP_DOMAIN_EB].reg, vsp_eb_reg);
+		}
+	}
+
+	if (vsp_hw_dev->version == SHARKL5Pro) {
+		regmap_read(regs[PMU_PWR_STATUS].gpr, regs[PMU_PWR_STATUS].reg,
+			&power_state1);
+
+		if ((power_state1 & regs[PMU_PWR_STATUS].mask) == 0x0) {
+			regmap_read(regs[RESET].gpr, 0x0, &ap_ahb_regs);
+			if ((ap_ahb_regs & (BIT(7) | BIT(2))) != (BIT(7) | BIT(2))) {
+				 pr_info("ap ahb 0x%x\n", ap_ahb_regs);
+				ret = regmap_update_bits(regs[RESET].gpr, 0x0, BIT(7) | BIT(2),
+							BIT(7) | BIT(2));
+				if (ret) {
+					pr_err("vsp_iova regmap_update_bits failed %s, %d\n",
+						__func__, __LINE__);
+				}
+			}
+		} else {
+			pr_info("offset 0x%x, power 0x%x\n", regs[PMU_PWR_STATUS].reg, power_state1);
 		}
 	}
 #endif
