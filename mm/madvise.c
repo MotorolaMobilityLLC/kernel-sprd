4e11bf9e01e53bcccd4c98af6a72e7bc22857ad6
Liangcai Fan <liangcai.fan@unisoc.com>
2020-04-22 13:35:15 +0800

    Bug #1470624 FROMLIST: mm: protect VMA modifications using VMA sequence count
    [root cause  ] protect VMA modifications using VMA sequence count
    [changes     ] mmap.c
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:skarkl3
    [test case]:no
    [reviewers   ]Liangcai.fan
    [change_type ] feature
    [tag_product ] common
    
    The VMA sequence count has been introduced to allow fast detection of
    VMA modification when running a page fault handler without holding
    the mmap_sem.
    
    This patch provides protection against the VMA modification done in :
    	- madvise()
    	- mpol_rebind_policy()
    	- vma_replace_policy()
    	- change_prot_numa()
    	- mlock(), munlock()
    	- mprotect()
    	- mmap_region()
    	- collapse_huge_page()
    	- userfaultd registering services
    
    In addition, VMA fields which will be read during the speculative fault
    path needs to be written using WRITE_ONCE to prevent write to be split
    and intermediate values to be pushed to other CPUs.
    
    Change-Id: I4d524587a32cef99394f93f88a7ea506322958a9
    Signed-off-by: Laurent Dufour <ldufour@linux.ibm.com>
    Reviewed-by: Jérôme Glisse <jglisse@redhat.com>
    Signed-off-by: Liangcai Fan <liangcai.fan@unisoc.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -167,7 +167,9 @@ success:
 	/*
 	 * vm_flags is protected by the mmap_sem held in write mode.
 	 */
-	vma->vm_flags = new_flags;
+	vm_write_begin(vma);
+	WRITE_ONCE(vma->vm_flags, new_flags);
+	vm_write_end(vma);
 
 out_convert_errno:
 	/*
@@ -723,10 +725,12 @@ static int madvise_free_single_vma(struct vm_area_struct *vma,
 	update_hiwater_rss(mm);
 
 	mmu_notifier_invalidate_range_start(&range);
+	vm_write_begin(vma);
 	tlb_start_vma(&tlb, vma);
 	walk_page_range(vma->vm_mm, range.start, range.end,
 			&madvise_free_walk_ops, &tlb);
 	tlb_end_vma(&tlb, vma);
+	vm_write_end(vma);
 	mmu_notifier_invalidate_range_end(&range);
 	tlb_finish_mmu(&tlb, range.start, range.end);
 
