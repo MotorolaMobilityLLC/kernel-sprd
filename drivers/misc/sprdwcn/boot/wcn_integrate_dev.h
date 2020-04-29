0d93663be5daff21987f58c757a0c5a192048fb4
chaofan.wang <chaofan.wang@unisoc.com>
2020-04-29 13:48:04 +0800

    Bug #1309822 misc: sprdwcn: Support loading firmware from a or b partitions
    [root cause  ] add a and b partitions
    [changes     ] support loading firmware from a or b partitions
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums512, 9863a, 9832e
    [test case]:wcn ok
    [reviewers   ] songhe.wei
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    AndroidR divides two partition a aand b, so wcn needs to support loading
    firmware from a or b partitions.
    
    Change-Id: I09220e03b1dc84f0c15b641b3b3a0dd6f5e79f12
    Signed-off-by: chaofan.wang <chaofan.wang@unisoc.com>

--- drivers/misc/sprdwcn/boot/wcn_integrate_dev.h
+++ drivers/misc/sprdwcn/boot/wcn_integrate_dev.h
@@ -36,6 +36,7 @@ enum wcn_gnss_sub_sys {
 #define WCN_BTWF_FILENAME "wcnmodem"
 #define WCN_GNSS_FILENAME "gpsgl"
 #define WCN_GNSS_BD_FILENAME "gpsbd"
+#define SUFFIX "androidboot.slot_suffix="
 
 /* NOTES:If DTS config more than REG_CTRL_CNT_MAX REGs */
 #define REG_CTRL_CNT_MAX 8
