527cfeaa9402d811d8b838f3f6c140a2dc37d1e1
baolei.yuan <baolei.yuan@spreadtrum.com>
2020-05-28 20:19:17 +0800

    Bug #1324439 net: adjust WPA3&WAPI definition
    [root cause  ] modify WPA3&WAPI definition for androidR
    [changes     ] modify WPA3&WAPI definition for androidR
    [side effects] no side effects
    [self test   ]: test wifi
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]: 9832E
    [test case]: test wifi
    [reviewers   ] baolei.yuan
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add definition of WPA version 3 for SAE authentication.
    
    Change-Id: Id969614a3b9a7ca196c5b2b7b6e7c2950a5f4ae8
    Signed-off-by: baolei.yuan <baolei.yuan@spreadtrum.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -8040,6 +8040,7 @@ static bool nl80211_valid_wpa_versions(u32 wpa_versions)
 {
 	return !(wpa_versions & ~(NL80211_WPA_VERSION_1 |
 				  NL80211_WPA_VERSION_2 |
+				  NL80211_WPA_VERSION_3 |
 				  NL80211_WAPI_VERSION_1));
 }
 
