7894537e2616fc04a382a898e0bce16d0fd3ef9d
jinfeng lin1 <jinfeng.lin1@unisoc.com>
2020-06-25 11:16:34 +0800

    Bug #1352108 power: supply: optimize full charged and low power calibration
    [root cause  ] optimize full charged and low power calibration
    [changes     ] supply
    [side effects] NA
    [self test   ]:charge
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:NA
    [is there dependence]:NA
    [confirm dependent commit]:NA
    [board]:ud710-2h10
    [test case]:charger
    [reviewers   ] yuanjiang.yu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add low power capacity calibration and full charged capacity calibration.
    
    Change-Id: I24bc05f434478ab65efee16232e4a7fa8e6d7465
    Signed-off-by: Jinfeng Lin1 <jinfeng.lin1@unisoc.com>

--- drivers/power/supply/charger-manager.c
+++ drivers/power/supply/charger-manager.c
@@ -52,6 +52,11 @@
 #define CM_TRACK_LOW_TEMP_THRESHOLD	150
 #define CM_TRACK_TIMEOUT_THRESHOLD	108000
 #define CM_TRACK_START_CAP_THRESHOLD	200
+#define CM_CAP_ONE_PERCENT		10
+#define CM_HCAP_DECREASE_STEP		8
+#define CM_HCAP_THRESHOLD		955
+#define CM_CAP_FULL_PERCENT		1000
+#define CM_CAP_MAGIC_NUM		0x5A5AA5A5
 
 #define CM_TRACK_FILE_PATH "/mnt/vendor/battery/calibration_data/.battery_file"
 
@@ -804,9 +809,11 @@ static bool is_full_charged(struct charger_manager *cm)
 		if (ret)
 			goto out;
 
-		if (uV >= desc->fullbatt_uV && uA <= desc->fullbatt_uA) {
+		if (uV >= desc->fullbatt_uV && uA <= desc->fullbatt_uA && uA > 0) {
 			if (++desc->trigger_cnt > 1) {
-				if (cm->desc->cap >= 1000) {
+				if (cm->desc->cap >= CM_CAP_FULL_PERCENT) {
+					if (desc->trigger_cnt == 2)
+						adjust_fuel_cap(cm, CM_FORCE_SET_FUEL_CAP_FULL);
 					is_full = true;
 				} else {
 					is_full = false;
@@ -1987,6 +1994,10 @@ static int charger_get_property(struct power_supply *psy,
 			break;
 		}
 		val->intval = DIV_ROUND_CLOSEST(cm->desc->cap, 10);
+		if (val->intval > 100)
+			val->intval = 100;
+		else if (val->intval < 0)
+			val->intval = 0;
 		break;
 	case POWER_SUPPLY_PROP_ONLINE:
 		if (is_ext_pwr_online(cm))
@@ -3368,6 +3379,7 @@ static void cm_batt_works(struct work_struct *work)
 	int batt_uV, batt_ocV, bat_uA, fuel_cap, chg_sts, ret;
 	int period_time, flush_time, cur_temp;
 	int chg_cur = 0, chg_limit_cur = 0;
+	static int last_fuel_cap = CM_CAP_MAGIC_NUM;
 
 	ret = get_batt_uV(cm, &batt_uV);
 	if (ret) {
@@ -3421,10 +3433,13 @@ static void cm_batt_works(struct work_struct *work)
 		cm->desc->low_temp_trigger_cnt = 0;
 	}
 
-	if (fuel_cap > 1000)
-		fuel_cap = 1000;
+	if (fuel_cap > CM_CAP_FULL_PERCENT)
+		fuel_cap = CM_CAP_FULL_PERCENT;
 	else if (fuel_cap < 0)
 		fuel_cap = 0;
+
+	if (last_fuel_cap == CM_CAP_MAGIC_NUM)
+		last_fuel_cap = fuel_cap;
 
 	cur_time = ktime_to_timespec64(ktime_get_boottime());
 
@@ -3474,6 +3489,7 @@ static void cm_batt_works(struct work_struct *work)
 
 	switch (cm->desc->charger_status) {
 	case POWER_SUPPLY_STATUS_CHARGING:
+		last_fuel_cap = fuel_cap;
 		if (fuel_cap < cm->desc->cap) {
 			if (bat_uA >= 0) {
 				fuel_cap = cm->desc->cap;
@@ -3506,7 +3522,7 @@ static void cm_batt_works(struct work_struct *work)
 		}
 
 		if (cm->desc->cap >= 985 && cm->desc->cap <= 994 &&
-		    fuel_cap >= 1000)
+		    fuel_cap >= CM_CAP_FULL_PERCENT)
 			fuel_cap = 994;
 		/*
 		 * Record 99% of the charging time.
@@ -3529,7 +3545,19 @@ static void cm_batt_works(struct work_struct *work)
 		 * the cap is not allowed to increase.
 		 */
 		if (fuel_cap >= cm->desc->cap) {
+			last_fuel_cap = fuel_cap;
 			fuel_cap = cm->desc->cap;
+		} else if (cm->desc->cap >= CM_HCAP_THRESHOLD) {
+			if (last_fuel_cap - fuel_cap >= CM_HCAP_DECREASE_STEP) {
+				if (cm->desc->cap - fuel_cap >= CM_CAP_ONE_PERCENT)
+					fuel_cap = cm->desc->cap - CM_CAP_ONE_PERCENT;
+				else
+					fuel_cap = cm->desc->cap - CM_HCAP_DECREASE_STEP;
+
+				last_fuel_cap -= CM_HCAP_DECREASE_STEP;
+			} else {
+				fuel_cap = cm->desc->cap;
+			}
 		} else {
 			if (period_time < cm->desc->cap_one_time &&
 			    (cm->desc->cap - fuel_cap) >= 5)
@@ -3542,17 +3570,20 @@ static void cm_batt_works(struct work_struct *work)
 			    (flush_time / cm->desc->cap_one_time) * 10)
 				fuel_cap = cm->desc->cap -
 					(flush_time / cm->desc->cap_one_time) * 10;
+			else if (cm->desc->cap - fuel_cap > CM_CAP_ONE_PERCENT)
+				fuel_cap = cm->desc->cap - CM_CAP_ONE_PERCENT;
 		}
 		break;
 
 	case POWER_SUPPLY_STATUS_FULL:
+		last_fuel_cap = fuel_cap;
 		cm->desc->update_capacity_time = cur_time.tv_sec;
 		if ((batt_ocV < (cm->desc->fullbatt_uV - cm->desc->fullbatt_vchkdrop_uV - 50000))
 		    && (bat_uA < 0))
 			cm->desc->force_set_full = false;
 		if (is_ext_pwr_online(cm)) {
-			if (fuel_cap != 1000)
-				fuel_cap = 1000;
+			if (fuel_cap != CM_CAP_FULL_PERCENT)
+				fuel_cap = CM_CAP_FULL_PERCENT;
 
 			if (fuel_cap > cm->desc->cap)
 				fuel_cap = cm->desc->cap + 1;
--- drivers/power/supply/sc27xx_fuel_gauge.c
+++ drivers/power/supply/sc27xx_fuel_gauge.c
@@ -83,6 +83,7 @@
 #define SC27XX_FGU_DENSE_CAPACITY_HIGH	3850000
 #define SC27XX_FGU_DENSE_CAPACITY_LOW	3750000
 #define SC27XX_FGU_BAT_NTC_THRESHOLD	50
+#define SC27XX_FGU_LOW_VBAT_REGION	3300
 
 #define interpolate(x, x1, y1, x2, y2) \
 	((y1) + ((((y2) - (y1)) * ((x) - (x1))) / ((x2) - (x1))));
@@ -140,6 +141,8 @@ struct sc27xx_fgu_data {
 	int vol_1000mv_adc;
 	int calib_resist_real;
 	int calib_resist_spec;
+	int first_calib_volt;
+	int first_calib_cap;
 	int comp_resistance;
 	int index;
 	int boot_vol;
@@ -179,6 +182,8 @@ static void sc27xx_fgu_low_capacity_calibration(struct sc27xx_fgu_data *data,
 						int chg_sts);
 static int sc27xx_fgu_get_temp(struct sc27xx_fgu_data *data, int *temp);
 static void sc27xx_fgu_adjust_cap(struct sc27xx_fgu_data *data, int cap);
+static int sc27xx_fgu_get_vbat_ocv(struct sc27xx_fgu_data *data, int *val);
+static int sc27xx_fgu_get_vbat_vol(struct sc27xx_fgu_data *data, int *val);
 
 static const char * const sc27xx_charger_supply_name[] = {
 	"sc2731_charger",
@@ -661,6 +666,10 @@ static int sc27xx_fgu_get_capacity(struct sc27xx_fgu_data *data, int *cap,
 	delta_cap = DIV_ROUND_CLOSEST(temp * 1000, data->total_cap);
 	*cap = delta_cap + data->init_cap;
 	data->normal_temperature_cap = *cap;
+	if (data->normal_temperature_cap < 0)
+		data->normal_temperature_cap = 0;
+	else if (data->normal_temperature_cap > 1000)
+		data->normal_temperature_cap = 1000;
 
 	if (data->cap_table_len > 0) {
 		temp_cap = sc27xx_fgu_temp_to_cap(data->cap_temp_table,
@@ -1163,6 +1172,40 @@ static void sc27xx_fgu_adjust_cap(struct sc27xx_fgu_data *data, int cap)
 		dev_err(data->dev, "failed to get init clbcnt\n");
 }
 
+static void sc27xx_fgu_low_capacity_match_ocv(struct sc27xx_fgu_data *data,
+					      int cap)
+{
+	int ret, ocv, batt_uV;
+
+	ret = sc27xx_fgu_get_vbat_ocv(data, &ocv);
+	if (ret) {
+		dev_err(data->dev, "get battery ocv error.\n");
+		return;
+	}
+
+	ret = sc27xx_fgu_get_vbat_vol(data, &batt_uV);
+	if (ret) {
+		dev_err(data->dev, "get battery vol error.\n");
+		return;
+	}
+
+	if ((batt_uV < SC27XX_FGU_LOW_VBAT_REGION || ocv < data->min_volt) &&
+	    cap > data->alarm_cap) {
+		data->init_cap -= 5;
+		if (data->init_cap < 0)
+			data->init_cap = 0;
+	} else if (ocv > data->min_volt && cap <= data->alarm_cap) {
+		sc27xx_fgu_adjust_cap(data, data->alarm_cap);
+	} else if (ocv <= data->cap_table[data->table_len - 1].ocv) {
+		sc27xx_fgu_adjust_cap(data, 0);
+	} else if (data->first_calib_volt > 0 && data->first_calib_cap > 0 &&
+		   ocv <= data->first_calib_volt && cap > data->first_calib_cap) {
+		data->init_cap -= 5;
+		if (data->init_cap < 0)
+			data->init_cap = 0;
+	}
+}
+
 static void sc27xx_fgu_low_capacity_calibration(struct sc27xx_fgu_data *data,
 						int cap, int int_mode,
 						int chg_sts)
@@ -1184,37 +1227,9 @@ static void sc27xx_fgu_low_capacity_calibration(struct sc27xx_fgu_data *data,
 	    data->bat_temp <= SC27XX_FGU_LOW_TEMP_REGION)
 		return;
 
-	if (ocv <= data->cap_table[data->table_len - 1].ocv) {
-		sc27xx_fgu_adjust_cap(data, 0);
-	} else if ((ocv > data->cap_table[data->table_len - 1].ocv && cap <= 0) ||
-		(ocv > data->min_volt && cap <= data->alarm_cap)) {
-		int cur_cap = power_supply_ocv2cap_simple(data->cap_table,
-							  data->table_len, ocv);
+	sc27xx_fgu_low_capacity_match_ocv(data, cap);
 
-		sc27xx_fgu_adjust_cap(data, cur_cap);
-	} else if (ocv <= data->min_volt) {
-		/*
-		 * If current OCV value is less than the low alarm voltage, but
-		 * current capacity is larger than the alarm capacity, we should
-		 * adjust the inititial capacity to alarm capacity.
-		 */
-		if (cap > data->alarm_cap) {
-			sc27xx_fgu_adjust_cap(data, data->alarm_cap);
-		} else {
-			int cur_cap;
-
-			/*
-			 * If current capacity is equal with 0 or less than 0
-			 * (some error occurs), we should adjust inititial
-			 * capacity to the capacity corresponding to current OCV
-			 * value.
-			 */
-			cur_cap = power_supply_ocv2cap_simple(data->cap_table,
-							      data->table_len,
-							      ocv);
-			sc27xx_fgu_adjust_cap(data, cur_cap);
-		}
-
+	if (ocv <= data->min_volt) {
 		if (!int_mode)
 			return;
 
@@ -1642,6 +1657,18 @@ static int sc27xx_fgu_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "failed to get fgu specification calibration resistance\n");
 		return ret;
 	}
+
+	ret = device_property_read_u32(&pdev->dev,
+				       "first-calib-voltage",
+				       &data->first_calib_volt);
+	if (ret)
+		dev_warn(&pdev->dev, "failed to get fgu first calibration voltage\n");
+
+	ret = device_property_read_u32(&pdev->dev,
+				       "first-calib-capacity",
+				       &data->first_calib_cap);
+	if (ret)
+		dev_warn(&pdev->dev, "failed to get fgu first calibration capacity\n");
 
 	ret = device_property_read_u32(&pdev->dev,
 				       "sprd,comp-resistance-mohm",
