902eac1a0172c2a9515d30a5b5d69cf7aa3ee4eb
chenxu.wei1 <chenxu.wei1@unisoc.com>
2020-07-27 10:48:25 +0800

    Bug #1372792 firmware: adjust tos cpu power down return
    [root cause  ] modifiy trusty code
    [changes     ] no
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl5pro
    [test case]:power on
    [reviewers   ] aijun,hongli
    
    [change_type ] feature
    [tag_product ] specific
    Signed-off-by: chenxu.wei1 <chenxu.wei1@unisoc.com>
    
    Change-Id: I9a356acbe897a46bc7065374d30f4041aae33ebe

--- drivers/firmware/psci.c
+++ drivers/firmware/psci.c
@@ -34,10 +34,7 @@
 #include <asm/smp_plat.h>
 #include <asm/suspend.h>
 
-#ifdef CONFIG_TRUSTY
 #include <linux/trusty/smcall.h>
-#include <linux/trusty/trusty.h>
-#endif
 
 /*
  * While a 64-bit OS can make calls with SMC32 calling conventions, for some
@@ -58,22 +55,6 @@
  * require cooperation with a Trusted OS driver.
  */
 static int resident_cpu = -1;
-
-bool psci_tos_resident_on(int cpu)
-{
-#ifdef CONFIG_TRUSTY
-	int ret;
-
-	ret = trusty_fast_call32_power(SMC_FC_CPU_CAN_DOWN, cpu, 0, 0);
-	if (ret < 0) {
-		return true;
-	} else {
-		return false;
-	}
-#else
-	return cpu == resident_cpu;
-#endif
-}
 
 struct psci_operations psci_ops = {
 	.conduit = PSCI_CONDUIT_NONE,
@@ -171,18 +152,26 @@ static u32 psci_get_version(void)
 	return invoke_psci_fn(PSCI_0_2_FN_PSCI_VERSION, 0, 0, 0);
 }
 
+bool psci_tos_resident_on(int cpu)
+{
+	/*
+	 * To diff the three case of no trusty, trusty can down,
+	 * trusty can't down. invert the return values of
+	 * psci_tos_resident_on() and trusty
+	 */
+	return !invoke_psci_fn(SMC_FC_CPU_CAN_DOWN, cpu, 0, 0);
+}
+
 static int psci_cpu_suspend(u32 state, unsigned long entry_point)
 {
 	int err;
 	u32 fn;
 
-#ifdef CONFIG_TRUSTY
 	u32 cpu_id;
 
 	cpu_id = smp_processor_id();
 	if (psci_tos_resident_on(cpu_id))
 		return -EPERM;
-#endif
 
 	fn = psci_function_id[PSCI_FN_CPU_SUSPEND];
 	err = invoke_psci_fn(fn, state, entry_point, 0);
