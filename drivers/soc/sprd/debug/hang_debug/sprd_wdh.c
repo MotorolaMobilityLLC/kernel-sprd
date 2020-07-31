26452b31e9154b4f6124bb390ebf38182721ec62
tianhui.wang <tianhui.wang@unisoc.com>
2020-07-31 13:41:44 +0800

    Bug #1314435 soc: sprd: debug: enable hang_debug uart log output
    [root cause  ] using uart for hang debug log output
    [changes     ] serial
    [side effects] no
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:696227
    [board]:sharkl3
    [test case]:hang debug
    [reviewers   ] ke.wang
    [change_type ] feature
    [tag_product ] common
    
    using uart for hang debug log output
    Change-Id: I4f08e57572d99e8cb12501e190def90e307d9422
    Signed-off-by: tianhui.wang <tianhui.wang@unisoc.com>

--- drivers/soc/sprd/debug/hang_debug/sprd_wdh.c
+++ drivers/soc/sprd/debug/hang_debug/sprd_wdh.c
@@ -114,6 +114,9 @@ enum smcc_regs_id{
 
 struct pt_regs cpu_context[NR_CPUS];
 static struct sprd_sip_svc_handle *svc_handle;
+#ifdef CONFIG_SPRD_HANG_DEBUG_UART
+extern void sprd_hangd_console_write(const char *s, unsigned int count);
+#endif
 
 #ifdef CONFIG_ARM64
 #define sprd_virt_addr_valid(kaddr) ((((void *)(kaddr) >= (void *)PAGE_OFFSET && \
@@ -137,7 +140,9 @@ static void sprd_write_print_buf(char *source, int size)
 		log_buf_pos = log_buf_pos + size - SPRD_PRINT_BUF_LEN;
 		memcpy(sprd_log_buf, source, log_buf_pos);
 	}
-
+#ifdef CONFIG_SPRD_HANG_DEBUG_UART
+	sprd_hangd_console_write(source, size);
+#endif
 	log_length += size;
 }
 
