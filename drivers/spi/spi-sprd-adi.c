0ef9dd362b7bd785ad9426994e8eab6612a7e32c
Lingling Xu <ling_ling.xu@spreadtrum.com>
2020-08-03 14:13:47 +0800

    Bug #1314446 PRIVATE: watchdog: sprd: add ap watchdog with fiq isr for L3
    [root cause  ] add ap watchdog with fiq isr for L3
    [changes     ] add ap watchdog with fiq isr for L3
    [side effects] no
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]: all
    [test case]: ap watchdog timerout and reset
    [reviewers   ] ke.wang feng_feng.wang tianhui.wang long.gao
    [change_type ] debug_log
    [tag_product ] common
    
    Add ap watchdog with fiq isr for L3.
    
    Change-Id: I6e1f5c744b3a178f2ccebbbada23f02e7d4bc3ef
    Signed-off-by: Lingling Xu <ling_ling.xu@spreadtrum.com>

--- drivers/spi/spi-sprd-adi.c
+++ drivers/spi/spi-sprd-adi.c
@@ -386,7 +386,7 @@ static int sprd_adi_transfer_one(struct spi_controller *ctlr,
 
 static void sprd_adi_set_wdt_rst_mode(struct sprd_adi *sadi)
 {
-#ifdef CONFIG_SPRD_WATCHDOG
+#if IS_ENABLED(CONFIG_SPRD_WATCHDOG) || IS_ENABLED(CONFIG_SPRD_WATCHDOG_FIQ)
 	u32 val;
 
 	/* Init watchdog reset mode */
