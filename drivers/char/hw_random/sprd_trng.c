537196e91d9988ee48107b92e00c5d58f1458fbe
zhiwei.song <zhiwei.song@unisoc.com>
2020-08-18 17:53:18 +0800

    Bug #1392077 char: hw_random: sprd: sprd hwrng driver
    [root cause  ] developping true random number driver
    [changes     ] add true random number driver
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:n6pro
    [test case]:rand
    [reviewers   ] shijiu.ren
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: If61a70663c68efc57b3a35800ce18c10f04bef77
    Signed-off-by: guozhu.xing <guozhu.xing@unisoc.com>

---
+++ drivers/char/hw_random/sprd_trng.c
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2020 SPRD Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/clk.h>
+#include <linux/err.h>
+#include <linux/kernel.h>
+#include <linux/hw_random.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/random.h>
+
+/**reg**/
+#define REG_CE_CLK              0x018
+#define REG_CE_VERSION          0x0c8
+#define REG_CE_RNG_EN           0x200
+#define REG_CE_RNG_CONFIG       0x204
+#define REG_CE_RNG_DATA         0x208
+#define REG_CE_RNG_WORK_STATUS  0x214
+#define REG_CE_RNG_POST         0x210
+#define REG_CE_RNG_MODE         0x228
+#define REG_CE_RNG_RING_L       0x248
+#define REG_CE_RNG_RING_H       0x24c
+
+/**cfg**/
+#define TRNG_CLK_EN         (0x1 << 24)
+#define CE_CFG              (0x1 << 24)
+#define RNG_ENABLE          0x1FF03
+#define RNG_DISABLE         0x0
+//#define RNG_CFG             0xFFF00363
+#define RNG_DATA_VALID      0x2
+#define TRNG_MODE           0x1
+
+#define to_sprd_trng(p)	container_of(p, struct sprd_trng, rng)
+
+struct sprd_trng {
+	void __iomem *base;
+	struct hwrng rng;
+	struct clk *clk;
+	struct clk *ce_eb;
+	struct clk *parent;
+};
+
+static int sprd_trng_init(struct hwrng *rng)
+{
+	struct sprd_trng *trng = to_sprd_trng(rng);
+	u32 val = 0;
+	u32 err = 0;
+	err = clk_prepare_enable(trng->clk);
+	if (err)
+		return err;
+	err = clk_prepare_enable(trng->ce_eb);
+	if (err)
+		return err;
+
+	pr_info("ce ip version = %x \n", readl_relaxed(trng->base + REG_CE_VERSION));
+	/*enable trng clk*/
+	val = readl_relaxed(trng->base + REG_CE_CLK);
+	val |= TRNG_CLK_EN;
+	writel_relaxed(val, trng->base + REG_CE_CLK);
+
+
+	/*enable rng*/
+	val = readl_relaxed(trng->base + REG_CE_RNG_EN);
+	val |= RNG_ENABLE;
+	writel_relaxed(val, trng->base + REG_CE_RNG_EN);
+
+	val = readl_relaxed(trng->base + REG_CE_RNG_MODE);
+	val |= TRNG_MODE;
+	writel_relaxed(val, trng->base + REG_CE_RNG_MODE);
+
+	return 0;
+}
+
+static void sprd_trng_cleanup(struct hwrng *rng)
+{
+	struct sprd_trng *trng = to_sprd_trng(rng);
+	u32 val = 0;
+
+	val = readl_relaxed(trng->base + REG_CE_RNG_EN);
+	val &= RNG_DISABLE;
+	writel_relaxed(val, trng->base + REG_CE_RNG_EN);
+}
+
+static int sprd_trng_read(struct hwrng *rng, void *buf, size_t max, bool wait)
+{
+	struct sprd_trng *trng = to_sprd_trng(rng);
+	u32 *data = (u32 *)buf;
+
+	*data = readl_relaxed(trng->base + REG_CE_RNG_DATA);
+
+	return 4;
+}
+
+static int sprd_trng_probe(struct platform_device *pdev)
+{
+	struct sprd_trng *rng;
+	struct resource *res;
+	int ret = 0;
+
+	rng = devm_kzalloc(&pdev->dev, sizeof(*rng), GFP_KERNEL);
+	if (!rng)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, rng);
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	rng->base = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(rng->base))
+		return PTR_ERR(rng->base);
+
+	/*set ce clk*/
+	rng->clk = devm_clk_get(&pdev->dev, "ap-ce-clk");
+	if (IS_ERR(rng->clk))
+		return PTR_ERR(rng->clk);
+
+	rng->ce_eb = devm_clk_get(&pdev->dev, "ce-pub-eb");
+	if (IS_ERR(rng->ce_eb))
+		return PTR_ERR(rng->ce_eb);
+
+	rng->parent = devm_clk_get(&pdev->dev, "source");
+	if (IS_ERR(rng->parent))
+		return PTR_ERR(rng->parent);
+
+	clk_set_parent(rng->clk, rng->parent);
+
+
+	rng->rng.name = pdev->name;
+	rng->rng.init = sprd_trng_init;
+	rng->rng.cleanup = sprd_trng_cleanup;
+	rng->rng.read = sprd_trng_read;
+
+	ret = devm_hwrng_register(&pdev->dev, &rng->rng);
+	if (ret) {
+		dev_err(&pdev->dev, "failed to register hwrng\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static const struct of_device_id sprd_trng_dt_ids[] = {
+	{ .compatible = "sprd,sprd-trng" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, sprd_trng_dt_ids);
+
+static struct platform_driver sprd_trng_driver = {
+	.probe		= sprd_trng_probe,
+	.driver		= {
+		.name	= "sprd_trng",
+		.of_match_table = of_match_ptr(sprd_trng_dt_ids),
+	},
+};
+
+module_platform_driver(sprd_trng_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Guozhu Xing <guozhu.xing@unisoc.com>");
+MODULE_DESCRIPTION("Unisoc true random number generator driver");
