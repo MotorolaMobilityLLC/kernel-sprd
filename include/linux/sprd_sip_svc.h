34297c381426ce33ac6e0c4f67cd1db12cccfb9e
zhanqing.zhu <zhanqing.zhu@unisoc.com>
2020-10-05 13:40:08 +0800

    Bug #1838974 firmware: sprd: add sip function for ufs crypto
    [root cause  ]cherry-pick old feature
    [changes     ]add sip function
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:qogirn6pro
    [test case]:boot-up
    [reviewers   ]
    
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: Ia521691fc9a3a740b06621052a5104a1142f5b51
    Signed-off-by: zhanqing.zhu <zhanqing.zhu@unisoc.com>

--- include/linux/sprd_sip_svc.h
+++ include/linux/sprd_sip_svc.h
@@ -149,10 +149,27 @@ struct sprd_sip_svc_npu_ops {
 };
 
 /**
+ * struct sprd_sip_svc_storage_ops - represents the various operations
+ * 	provided by SPRD SIP STORAGE
+ *
+ * @ufs_crypto_enable: make crypto cfg field configurable to normal world
+ * @ufs_crypto_disable: make crypto cfg field non-configurable to normal world
+ */
+struct sprd_sip_svc_storage_ops {
+	struct sprd_sip_svc_rev_info rev;
+
+#if IS_ENABLED(CONFIG_SCSI_UFS_CRYPTO)
+	int (*ufs_crypto_enable)(void);
+	int (*ufs_crypto_disable)(void);
+#endif
+};
+
+/**
  * struct sprd_sip_svc_handle - Handle returned to SPRD SIP clients for usage
  *
  * @perf_ops: pointer to set of performance operations
  * @dbg_ops: pointer to set of dbg operations
+ * @storage_ops: pointer to set of storage operations
  */
 struct sprd_sip_svc_handle {
 	struct sprd_sip_svc_perf_ops perf_ops;
@@ -160,6 +177,7 @@ struct sprd_sip_svc_handle {
 	struct sprd_sip_svc_pwr_ops pwr_ops;
 	struct sprd_sip_svc_dvfs_ops dvfs_ops;
 	struct sprd_sip_svc_npu_ops npu_ops;
+	struct sprd_sip_svc_storage_ops storage_ops;
 };
 
 /**
