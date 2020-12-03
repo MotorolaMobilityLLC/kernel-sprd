b95e17a4ad15ef6de67b3b60aa333d99519d83e7
samchiu <sam.chiu@spreadtrum.com>
2020-12-03 10:50:00 +0800

    Revert "Bug #1444425　do_epoll_ctl(): clean the failure exits up a bit"
    This reverts commit 5a6f0382fa6abffe8e9e5bf5b9ef6b0024b527d9.
    [change_type ] AOB --> google_original
    [tag_product ] common
    
    Change-Id: I7e99009392390c18a1aa9eebc35711b77aa6b135

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -2098,7 +2098,8 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 			mutex_lock(&epmutex);
 			if (is_file_epoll(tf.file)) {
 				error = -ELOOP;
-				if (ep_loop_check(ep, tf.file) != 0)
+				if (ep_loop_check(ep, tf.file) != 0) {
+					clear_tfile_check_list();
 					goto error_tgt_fput;
 				}
 			} else
@@ -2127,6 +2128,8 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 			error = ep_insert(ep, &epds, tf.file, fd, full_check);
 		} else
 			error = -EEXIST;
+		if (full_check)
+			clear_tfile_check_list();
 		break;
 	case EPOLL_CTL_DEL:
 		if (epi)
@@ -2149,10 +2152,8 @@ SYSCALL_DEFINE4(epoll_ctl, int, epfd, int, op, int, fd,
 	mutex_unlock(&ep->mtx);
 
 error_tgt_fput:
-	if (full_check) {
-		clear_tfile_check_list();
+	if (full_check)
 		mutex_unlock(&epmutex);
-	}
 
 	fdput(tf);
 error_fput:
