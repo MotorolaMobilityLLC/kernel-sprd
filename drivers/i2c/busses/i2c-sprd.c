ac225dcc192ae2b4355517ec5eafb32bb0c2a22d
Linhua Xu <linhua.xu@unisoc.com>
2020-12-07 16:35:14 +0800

    Bug #1463625 i2c: sprd: Add the configuration of 1M.
    [root cause  ] Add the configuration of 1M
    [changes     ] Add the configuration of 1M
    [side effects] no
    [self test   ]:yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: all
    [test case]: test ok
    [reviewers   ] chunyan.zhang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    With the increasing of the transmission rate of I2C peripherals,
    the transmission rate of 100K and 400K has not met the demand,
    so configure the 1M transmission frequency of I2C.
    
    Change-Id: Id7c30b60d21fdfa34b01c7abd91b31964b0a69f9
    Signed-off-by: Linhua Xu <linhua.xu@unisoc.com>

--- drivers/i2c/busses/i2c-sprd.c
+++ drivers/i2c/busses/i2c-sprd.c
@@ -347,6 +347,8 @@ static void sprd_i2c_set_clk(struct sprd_i2c *i2c_dev, u32 freq)
 		writel((14 * apb_clk) / 10000000, i2c_dev->base + ADDR_STA0_DVD);
 	else if (freq == 100000)
 		writel((4 * apb_clk) / 1000000, i2c_dev->base + ADDR_STA0_DVD);
+	else if (freq == 1000000)
+		writel((8 * apb_clk) / 10000000, i2c_dev->base + ADDR_STA0_DVD);
 }
 
 static void sprd_i2c_enable(struct sprd_i2c *i2c_dev)
@@ -522,7 +524,8 @@ static int sprd_i2c_probe(struct platform_device *pdev)
 		i2c_dev->bus_freq = prop;
 
 	/* We only support 100k and 400k now, otherwise will return error. */
-	if (i2c_dev->bus_freq != 100000 && i2c_dev->bus_freq != 400000)
+	if (i2c_dev->bus_freq != 100000 && i2c_dev->bus_freq != 400000 &&
+	    i2c_dev->bus_freq != 1000000)
 		return -EINVAL;
 
 	ret = sprd_i2c_clk_init(i2c_dev);
