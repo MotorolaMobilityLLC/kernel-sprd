7cf7f5fb45574d78f71bd920d9a1bfc0616d5df5
yangyuxiang <yuxiang.yang@chino-e.com>
2020-12-11 18:55:33 +0800

    S6301-2452:[S6031]bringup[1-1]: update firmware in kernel(easyquick-icnl9911c)
    Description:
    bringup[1-1]: update firmware in kernel(easyquick-icnl9911c)
    
    Solution:
    bringup[1-1]: update firmware in kernel(easyquick-icnl9911c)
    
    St-result: PASS
    Change-Id: I54ea38cf79dfd1aeb15344fe55a189572969a765

--- drivers/input/touchscreen/chipone-tddi/cts_builtin_firmware.h
+++ drivers/input/touchscreen/chipone-tddi/cts_builtin_firmware.h
@@ -1,5 +1,5 @@
 static u8 icnl9911_driver_builtin_firmware[] = {
-    #include"firmware/easyquick_608/Moto-Fiji-Lite-Easyquick-ICNL9911C_Long-V_V020b_20200924.h"
+    #include"firmware/easyquick_608/builtin_firmware.h"
 };
 
 const static struct cts_firmware cts_driver_builtin_firmwares[] = {
