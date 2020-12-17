c2948b52e698a68dba8900ded0059f579c8371ab
mingming <ming.ming@chino-e.com>
2020-12-17 15:48:11 +0800

    [S6302-209]: update tp fw in kernel [1/1]
    Description:update tp fw,version:0x0D
    
    Change-Id: If97c153b2d385e04426659ba4455cbce560c8199
    St-result:PASS

--- drivers/input/touchscreen/chipone-tddi-spi/cts_builtin_firmware.h
+++ drivers/input/touchscreen/chipone-tddi-spi/cts_builtin_firmware.h
@@ -3,7 +3,7 @@ static u8 icnl9911_driver_builtin_firmware_easyquick_608[] = {
 };
 
 static u8 icnl9911_driver_builtin_firmware_hlt[] = {
-    #include"firmware/hlt/ICNL9911C_HLT_MOTO_MALTA_20201204_V020C.h"
+    #include"firmware/hlt/ICNL9911C_HLT_MOTO_MALTA_20201204_V020D.h"
 };
 
 const static struct cts_firmware cts_driver_builtin_firmwares_easyquick_608[] = {
