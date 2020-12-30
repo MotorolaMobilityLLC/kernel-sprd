b8f30a55781002a5aee0dd5faf3fd95bf396159b
alm.yuxiang.yang <yuxiang.yang@chino-e.com>
2020-12-30 18:29:18 +0800

    S6301-2452:[S6031]bringup[1-1]: update fw to resolve MMI test(hlt-icnl9911c)
    Description:
    bringup[1-1]: update fw to resolve MMI test(hlt-icnl9911c)
    
    Solution:
    bringup[1-1]: update fw to resolve MMI test(hlt-icnl9911c)
    
    Change-Id: I9beb1df4e9467d82f11598b407d2a2b07c5f96e0
    St-result: PASS

--- drivers/input/touchscreen/chipone-tddi/cts_builtin_firmware.h
+++ drivers/input/touchscreen/chipone-tddi/cts_builtin_firmware.h
@@ -1,5 +1,5 @@
 static u8 icnl9911_driver_builtin_firmware[] = {
-    #include"firmware/easyquick_608/builtin_firmware.h"
+    #include"firmware/easyquick_608/Moto_FijiSC_ChinoE_S6301_Easyquick_MDT6.088_ICNL9911C_V020E.h"
 };
 
 const static struct cts_firmware cts_driver_builtin_firmwares[] = {
