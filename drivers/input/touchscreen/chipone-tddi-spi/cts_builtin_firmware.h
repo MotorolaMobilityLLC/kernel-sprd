f49a889f2f351602c1248f9fb845df260eeb4d89
mingming <ming.ming@chino-e.com>
2021-01-04 14:30:00 +0800

    [S6302-174/175/178/245] update 9911c-tp parameter in kernel[1/1]
    Description:update 9911c-tp parameter
    
    Change-Id: Ifb3abc492087dc5d0a6edbc460f37250c873bf32
    St-result:PASS

--- drivers/input/touchscreen/chipone-tddi-spi/cts_builtin_firmware.h
+++ drivers/input/touchscreen/chipone-tddi-spi/cts_builtin_firmware.h
@@ -3,7 +3,7 @@ static u8 icnl9911_driver_builtin_firmware_easyquick_608[] = {
 };
 
 static u8 icnl9911_driver_builtin_firmware_hlt[] = {
-    #include"firmware/hlt/ICNL9911C_HLT_MOTO_MALTA_20201204_V020D.h"
+    #include"firmware/hlt/Moto_MaltaSC_ChinoE_S6302_Holitech_MDT6.517_ICNL9911C_V0211_20201231.h"
 };
 
 const static struct cts_firmware cts_driver_builtin_firmwares_easyquick_608[] = {
