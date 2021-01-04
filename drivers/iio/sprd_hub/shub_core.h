2af53eaed6b47a3f380db3de28baec25dcedbc6d
a <a@a.com>
2021-01-04 17:43:01 +0800

    <REQ><PDFCCE-980><【FTM APK】FTM APK：ALS完全遮挡后数值范围显示不正确>
    Change-Id: If408bffea0a6bbaf227e01fc731f0fc6a9caa807

--- drivers/iio/sprd_hub/shub_core.h
+++ drivers/iio/sprd_hub/shub_core.h
@@ -30,10 +30,10 @@
 #define SIPC_PM_BUFID0             0
 #define SIPC_PM_BUFID1             1
 #define SHUB_IIO_CHN_BITS             64
-/* light sensor calibrate min value is 280lux */
+/* light sensor calibrate min value is 1lux */
 #define LIGHT_SENSOR_MIN_VALUE  1
-/* light sensor calibrate max value is 520lux */
-#define LIGHT_SENSOR_MAX_VALUE  520
+/* light sensor calibrate max value is 1500lux */
+#define LIGHT_SENSOR_MAX_VALUE  1500
 #define LIGHT_CALI_DATA_COUNT   5
 /* light sensor calibrate value is 400lux; Due to kernel seldom use
  * float data, so calibrate value multiply 10000
