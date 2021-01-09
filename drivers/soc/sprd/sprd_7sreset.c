32ab85488139b648f7dd7f67e2c61eb4fb9aaf43
odm.wentao.jia <wentao.jia@chino-e.com>
2021-01-09 16:01:16 +0800

    S6301-1415[3-2]: Add debug parameters for userdebug version(must revert before CF)
    Description:
    [3-2]: Add debug parameters for userdebug version(must revert before CF)
    
    Solution:
    [3-2]: Add debug parameters for userdebug version(must revert before CF)
    
    St-result: PASS
    Change-Id: I24a95c01a1e5636921e95d8385bf4be81d8c4435

--- drivers/soc/sprd/sprd_7sreset.c
+++ drivers/soc/sprd/sprd_7sreset.c
@@ -104,8 +104,7 @@ static int sprd_7sreset_get_keymode(struct device *dev)
 
 	return !(r_val & BIT_KEY2_7S_RST_EN);
 }
-
-#if 1
+#ifdef CONFIG_USERDEBUG_MODE
 static int sprd_7sreset_set_resetmode(struct device *dev, int mode)
 {
 	struct sprd_7sreset *sprd_7sreset_dev = dev_get_drvdata(dev);
@@ -228,7 +227,9 @@ static ssize_t hard_mode_store(struct device *dev,
 	if (ret < 0)
 		return ret;
 
+#ifdef CONFIG_USERDEBUG_MODE
 	ret = sprd_7sreset_set_resetmode(dev, set_value);
+#endif
 	if (ret)
 		return ret;
 
