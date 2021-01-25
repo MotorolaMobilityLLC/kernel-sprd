eb00a671988728fc494d020f7d0b5ec22fb6f74e
alm.yuxiang.yang <yuxiang.yang@chino-e.com>
2021-01-25 21:03:49 +0800

    S6302-2452:[S6302]bringup[1-1]: resolve esd problems in kernel(hx83102-sky)
    Description:
    bringup[1-1]: resolve esd problems in kernel(hx83102-sky)
    
    Solution:
    bringup[1-1]: resolve esd problems in kernel(hx83102-sky)
    
    Change-Id: I1eec1a2ae353c7c2dd2caeb87ac7c635ebc7197f
    St-result: PASS

--- drivers/input/touchscreen/hxchipset/himax_common.c
+++ drivers/input/touchscreen/hxchipset/himax_common.c
@@ -3031,6 +3031,7 @@ found_hx_chip:
 #endif
 
 	ts->suspended                      = false;
+	ts->hx_esd_flag                    = false;
 
 #if defined(HX_USB_DETECT_GLOBAL)
 	ts->usb_connected = 0x00;
@@ -3220,6 +3221,68 @@ void himax_chip_common_deinit(void)
 	I("%s: Common section deinited!\n", __func__);
 }
 
+#ifdef HX_UPDATE_FW_FROM_DISPLAY
+static void himax_notifie_resume_workqueue(struct work_struct *work)
+{
+	I("%s: Entering! \n", __func__);
+	himax_chip_common_resume(private_ts);
+	return;
+}
+
+static void himax_lcd_resume_func(void)
+{
+	schedule_work(&private_ts->notifie_resume_work_queue);
+	return;
+}
+
+static void himax_recovery_work_func(struct work_struct *work)
+{
+	I("%s: Entering! \n", __func__);
+#if defined(HX_EXCP_RECOVERY)
+	if (!mutex_trylock(&private_ts->fw_update_lock))
+	{
+		I("%s: already in esd resume! \n", __func__);
+		return;
+	}
+	himax_excp_hw_reset();
+	mutex_unlock(&private_ts->fw_update_lock);
+#endif
+	return;
+
+}
+
+static void himax_esd_resume_func(void)
+{
+
+	I("%s: Entering! \n", __func__);
+	queue_delayed_work(private_ts->ts_recovery_workqueue, &private_ts->recovery_work_queue,
+			msecs_to_jiffies(50));
+	I("%s: end! \n", __func__);
+}
+
+int himax_notifie_update_fw(unsigned long value)
+{
+	unsigned long update_fw_state = value;
+	I("update_fw_state == %ld \n",update_fw_state);
+	if(update_fw_state == 1) {
+		himax_lcd_resume_func();
+	}else if(update_fw_state == 2){
+		himax_esd_resume_func();
+	}else
+		E("invalid update_fw_state == %ld \n",update_fw_state);
+	return 0;
+}
+
+#endif
+
+void himax_esd_resume_func(void)
+{
+	I("%s: Entering! \n", __func__);
+	private_ts->hx_esd_flag = true;
+	queue_delayed_work(private_ts->ts_int_workqueue,&private_ts->ts_int_work,msecs_to_jiffies(DELAY_TIME));
+	I("%s: end! \n", __func__);
+}
+
 int himax_chip_common_suspend(struct himax_ts_data *ts)
 {
 	if (ts->suspended) {
@@ -3295,6 +3358,11 @@ int himax_chip_common_resume(struct himax_ts_data *ts)
 	int result = 0;
 #endif
 	I("%s: enter\n", __func__);
+
+	if (ts->hx_esd_flag == true) {
+		ts->suspended = true;
+		ts->hx_esd_flag = false;
+	}
 
 	if (ts->suspended == false) {
 		I("%s: It had entered resume, skip this step\n", __func__);
