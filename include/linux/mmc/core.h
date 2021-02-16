5fad72061531445608fadff7be72f559d8cb55a2
Zhongwu Zhu <zhongwu.zhu@unisoc.com>
2021-02-16 10:01:46 +0800

    Bug #1527279 mmc: sprd: add soft cmd queue function
    [root cause  ] customer request
    [changes     ] add soft cmd queue function
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums512_1h10
    [test case]:na
    [reviewers   ] yuelin.tang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Add soft cmd queue function with polling mode.
    Signed-off-by: Zhongwu Zhu <Zhongwu.Zhu@unisoc.com>
    
    Change-Id: Ie09b3a2836f3dfab76f40e0254d64c3807568ea5
    (cherry picked from commit 360af27d88edfafc5f8d382205c28c32f3a34c46)
    (cherry picked from commit d86fa9152556b2bf13253c1c1b413c7306675105)

--- include/linux/mmc/core.h
+++ include/linux/mmc/core.h
@@ -163,6 +163,12 @@ struct mmc_request {
 	 */
 	void			(*recovery_notifier)(struct mmc_request *);
 	struct mmc_host		*host;
+#ifdef CONFIG_EMMC_SOFTWARE_CQ_SUPPORT
+	struct mmc_async_req	*areq;
+	int			flags;
+	struct list_head	link;
+	struct list_head	hlist;
+#endif
 
 	/* Allow other commands during this ongoing data transfer or busy wait */
 	bool			cap_cmd_during_tfr;
