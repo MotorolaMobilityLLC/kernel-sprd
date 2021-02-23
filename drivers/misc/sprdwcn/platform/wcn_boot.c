84c7eadab5b44588230d83b8c937a99e95b8483a
congcong.wang <congcong.wang@unisoc.com>
2021-02-23 19:15:18 +0800

    Bug #1529634 misc: sprdwcn: Fix marlin power off too early when reboot
    [root cause  ] wifi tx rx work when pcie shutdown
    [changes     ]  Do not power down when reboot
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums9620_1h10
    [test case]:reboot many times
    [reviewers   ] steve.li
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    the patch fix marlin power off too early when reboot.
    
    Change-Id: Ia6bef7fdfa84f81e73996ed1586d12c5dd167f58
    Signed-off-by:congcong.wang <congcong.wang@unisoc.com>

--- drivers/misc/sprdwcn/platform/wcn_boot.c
+++ drivers/misc/sprdwcn/platform/wcn_boot.c
@@ -2580,7 +2580,6 @@ static void marlin_shutdown(struct platform_device *pdev)
 		wcn_avdd12_bound_xtl(false);
 		wcn_wifipa_bound_xtl(false);
 		wifipa_enable(0);
-		marlin_chip_en(false, false);
 	}
 	pr_info("%s end\n", __func__);
 }
