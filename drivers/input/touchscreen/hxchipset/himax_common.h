5f943c8dcb360d233f5886608098fc28f105024a
alm.yuxiang.yang <yuxiang.yang@chino-e.com>
2021-02-23 11:51:33 +0800

    S6302-378:bringup[2-1]: update parments about sensitivity in kernel(sky-hx83102)
    Description:
    bringup[2-1]: update parments about sensitivity in kernel(sky-hx83102)
    
    Solution:
    bringup[2-1]: update parments about sensitivity in kernel(sky-hx83102)
    
    Change-Id: I8416c4dbb0e4383cf4781b623b7649a10c2faa26
    St-result: PASS

--- drivers/input/touchscreen/hxchipset/himax_common.h
+++ drivers/input/touchscreen/hxchipset/himax_common.h
@@ -64,7 +64,7 @@
 #define HX_RESUME_SEND_CMD	/*Need to enable on TDDI chipset*/
 /*#define HX_HIGH_SENSE*/
 /*#define HX_PALM_REPORT*/
-/*#define HX_USB_DETECT_GLOBAL*/
+#define HX_USB_DETECT_GLOBAL
 
 /* for MTK special platform.If turning on,
  * it will report to system by using specific format.
--- drivers/usb/musb/musb_sprd.c
+++ drivers/usb/musb/musb_sprd.c
@@ -414,6 +414,10 @@ static struct musb_hdrc_config sprd_musb_hdrc_config = {
 };
 #pragma GCC diagnostic pop
 
+#ifdef CONFIG_TOUCHSCREEN_HIMAX_CHIPSET
+extern bool USB_detect_flag;
+#endif
+
 static int musb_sprd_vbus_notifier(struct notifier_block *nb,
 				unsigned long event, void *data)
 {
@@ -451,7 +455,7 @@ static int musb_sprd_vbus_notifier(struct notifier_block *nb,
 		dev_info(glue->dev,
 			"device disconnect detected from VBUS GPIO.\n");
 	}
-
+	USB_detect_flag = glue->vbus_active;
 	return 0;
 }
 
@@ -492,6 +496,7 @@ static int musb_sprd_id_notifier(struct notifier_block *nb,
 		dev_info(glue->dev,
 			"host disconnect detected from ID GPIO.\n");
 	}
+	USB_detect_flag = glue->vbus_active;
 
 	return 0;
 }
