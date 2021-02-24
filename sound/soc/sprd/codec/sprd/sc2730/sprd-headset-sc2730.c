6058c34ac679638379a9e4331a6c08c1fe9730e2
bo.wu <bo.wu@tinno.com>
2021-02-24 14:40:55 +0800

    <BUG><PDFCCE-4272><Disable micbias polling for moto factory version>
    Change-Id: I3e49d728cf07aea647a466e815df76416417f902

--- sound/soc/sprd/codec/sprd/sc2730/sprd-headset-sc2730.c
+++ sound/soc/sprd/codec/sprd/sc2730/sprd-headset-sc2730.c
@@ -1086,7 +1086,11 @@ static void sprd_enable_hmicbias_polling(bool enable, bool force_disable)
 			sprd_hmicbias_mode_set(hdst, REGULATOR_MODE_STANDBY);
 			sprd_headset_power_set(&hdst->power_manager,
 				"DIG_CLK_HID", true);
+#ifndef CONFIG_MOTO_FACTORY
 			headset_reg_set_bits(ANA_HID0, HID_EN);
+#else
+			headset_reg_clr_bits(ANA_HID0, HID_EN);
+#endif
 			hdst->current_polling_state = true;
 		}
 	} else {
