a75aecb1e8ed1f5a29717d35e9b6bbafe8ec2678
alm.yuxiang.yang <yuxiang.yang@chino-e.com>
2021-03-14 18:34:41 +0800

    S6302-299:bringup[1-1]: update firmware in kernel(tianma-ili7806s)
    Description:
    bringup[1-1]: update firmware in kernel(tianma-ili7806s)
    
    Solution:
    bringup[1-1]: update firmware in kernel(tianma-ili7806s)
    
    Change-Id: If1580a41b2b353cdaa52b9b0c1d4df783b0946d6
    St-result: PASS

--- drivers/input/touchscreen/ili9882h_sky_spi/firmware/ilitek_v3_fw.h
+++ drivers/input/touchscreen/ili9882h_sky_spi/firmware/ilitek_v3_fw.h
@@ -93,7 +93,7 @@ static unsigned char CTPM_FW_TM[] = {
 #define TM_INI_REQUEST_PATH1		"mp_tm_7806s.ini"
 #define TM_FW_REQUEST_PATH1		"ILITEK_FW_TM1"
 static unsigned char CTPM_FW_TM1[] = {
-	#include "tianma/ili7806s/440_CZ3_TM_TM_652_LongH_V0x10.0x28.0x02_AP_0x01_MP_20210305.ili"
+	#include "tianma/ili7806s/440_CZ3_TM_TM_652_LongH_V0x10.0x28.0x03_AP_0x01_MP_20210312.ili"
 };
 
 #endif
