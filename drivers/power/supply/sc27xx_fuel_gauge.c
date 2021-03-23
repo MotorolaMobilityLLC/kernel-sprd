d831a89e4910a6492e16a261dc81d22219157f1a
Yongzhi Chen <yongzhi.chen@spreadtrum.com>
2021-03-23 13:15:46 +0800

    Bug #1557176 power: supply: add bq25910 charger driver
    [root cause  ] add bq25910 charger driver
    [changes     ] add bq25910 charger driver
    [side effects] No
    [self test   ]:Yes
    [download normally]:Yes
    [power on/off normally]:Yes
    [do common repository/branch inspection]:Yes
    [is there dependence]:Yes
    [confirm dependent commit]:Yes
    [board]:common
    [test case]:N/A
    [reviewers   ] jinfeng/jingchao
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I66f72521ec0c283a99e8661f67418e6f8e528baf
    Signed-off-by: Yongzhi Chen <yongzhi.chen@spreadtrum.com>

--- drivers/power/supply/sc27xx_fuel_gauge.c
+++ drivers/power/supply/sc27xx_fuel_gauge.c
@@ -229,16 +229,16 @@ static int sc27xx_fgu_get_vbat_ocv(struct sc27xx_fgu_data *data, int *val);
 static int sc27xx_fgu_get_vbat_vol(struct sc27xx_fgu_data *data, int *val);
 
 static const char * const sc27xx_charger_supply_name[] = {
-	"sc2731_charger",
-	"sc2720_charger",
-	"sc2721_charger",
-	"sc2723_charger",
-	"sc2703_charger",
 	"fan54015_charger",
 	"bq2560x_charger",
 	"bq25890_charger",
+	"bq25910_charger",
+	"sc2720_charger",
+	"sc2721_charger",
+	"sc2703_charger",
+	"sc2731_charger",
 	"eta6937_charger",
-	"ump9620_charger",
+	"sc2723_charger",
 	"aw32257",
 };
 
