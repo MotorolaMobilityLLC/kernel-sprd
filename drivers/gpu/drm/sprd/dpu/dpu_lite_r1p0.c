5ec06f0808b906cf2dc354cccd502c8a0ced2aa2
ponny.wu <ponny.wu@unisoc.com>
2021-03-26 08:25:53 +0000

    Bug #1561327 drm: sprd: add switch-case fallthough statement to avoid gcc cpmpile error
    [root cause  ] gcc compile error
    [changes     ] fallthough
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:none
    [confirm dependent commit]:none
    [board]:sc9832e
    [test case]:display ok
    [reviewers   ] kevin.tang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    add switch-case fallthough statement to avoid gcc cpmpile error
    
    Change-Id: I0dcfbb0df809133efbb1e96ad99213d99f4ffae0
    Signed-off-by: Pony Wu <Pony.Wu@unisoc.com>

--- drivers/gpu/drm/sprd/dpu/dpu_lite_r1p0.c
+++ drivers/gpu/drm/sprd/dpu/dpu_lite_r1p0.c
@@ -320,21 +320,25 @@ static u32 dpu_img_ctrl(u32 format, u32 blending)
 	case DRM_FORMAT_RGBA8888:
 		/* RGBA8888 -> ABGR8888 */
 		reg_val |= BIT_DPU_LAY_DATA_ENDIAN_B3B2B1B0;
+		fallthrough;
 	case DRM_FORMAT_ABGR8888:
 		/* rb switch */
 		reg_val |= BIT_DPU_LAY_RGBUV_SWITCH;
+		fallthrough;
 	case DRM_FORMAT_ARGB8888:
 		reg_val |= BIT_DPU_LAY_FORMAT_ARGB8888;
 		break;
 	case DRM_FORMAT_XBGR8888:
 		/* rb switch */
 		reg_val |= BIT_DPU_LAY_RGBUV_SWITCH;
+		fallthrough;
 	case DRM_FORMAT_XRGB8888:
 		reg_val |= BIT_DPU_LAY_FORMAT_ARGB8888;
 		break;
 	case DRM_FORMAT_BGR565:
 		/* rb switch */
 		reg_val |= BIT_DPU_LAY_RGBUV_SWITCH;
+		fallthrough;
 	case DRM_FORMAT_RGB565:
 		reg_val |= BIT_DPU_LAY_FORMAT_RGB565;
 		break;
