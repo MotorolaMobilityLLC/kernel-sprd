00559a2516578b771284163e0b8beeb4371df3cf
ontim_lishunyu <shunyu.li@chino-e.com>
2021-04-15 10:31:45 +0800

    <lishunyu><S6301><S6302><lishunyu><S6302>temp 15 to 45,while power on, soc diff >30%,recalculate soc from ocv
    Change-Id: Ie3ec89867f99aeba8e7c6dd7294c8afd241936fa
    (cherry picked from commit ce79180d1cb9f0bb38783bb786e5477d9714b569)

--- drivers/power/supply/sc27xx_fuel_gauge.c
+++ drivers/power/supply/sc27xx_fuel_gauge.c
@@ -531,7 +531,7 @@ static int sc27xx_fgu_get_boot_capacity(struct sc27xx_fgu_data *data, int *cap)
 	bool is_first_poweron = sc27xx_fgu_is_first_poweron(data);
 	int current_ocv,current_cap,delta;
 
-	if(!is_first_poweron && data->bat_temp>200 && data->bat_temp <400)
+	if(!is_first_poweron && data->bat_temp>150 && data->bat_temp<450)
 	{
 		sc27xx_fgu_get_vbat_ocv(data, &current_ocv);
 		current_cap = power_supply_ocv2cap_simple(data->cap_table, data->table_len,
@@ -550,13 +550,13 @@ static int sc27xx_fgu_get_boot_capacity(struct sc27xx_fgu_data *data, int *cap)
 		 if(delta >300)
 		 {
 			data->boot_cap = current_cap*10;
-			dev_err(data->dev, "%s >300;%d;%d;%d;%d;\n",__func__,current_ocv,current_cap*10,*cap,data->bat_temp);
+			dev_err(data->dev, "%s >300;%d;%d;%d;T:%d;\n",__func__,current_ocv,current_cap*10,*cap,data->bat_temp);
 			*cap = current_cap*10;
 			return 0;
 
 		 }	
-	         dev_err(data->dev, "%s <300;%d;%d;%d;%d;\n",__func__,current_ocv,current_cap*10,*cap,data->bat_temp);
-	}
+	         dev_err(data->dev, "%s <300;%d;%d;%d;T:%d;\n",__func__,current_ocv,current_cap*10,*cap,data->bat_temp);
+	}		
 
 	if (is_charger_mode)
 		sc27xx_fgu_get_boot_voltage(data, &data->boot_vol);
