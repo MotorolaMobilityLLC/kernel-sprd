8be817008dc3157793c28e0ec8d303ea6993483c
Ben Dai <ben.dai@unisoc.com>
2021-05-17 14:20:39 +0800

    Bug #1606100 soc: sprd: debug: fix pclint warnings
    [root cause  ] fix pclint warnings
    [changes     ] irq_monitor
    [side effects] unknown
    [self test   ]: pass
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: all
    [test case]: pclint pass
    [reviewers   ] nianfu.bai
    
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I72e6983139e363417cb5053c0a867154f555fc58
    Signed-off-by: Ben Dai <ben.dai@unisoc.com>

--- drivers/soc/sprd/debug/irq/irqs_monitor/irq_monitor.c
+++ drivers/soc/sprd/debug/irq/irqs_monitor/irq_monitor.c
@@ -48,13 +48,14 @@ static bool Processing;
 static struct timespec64 ts_end;
 #ifdef CONFIG_SPRD_CPU_USAGE
 extern ulong sprd_g_irq_ratio;
-static int irqrunt_period;
+static unsigned int irqrunt_period;
 static struct timespec64 ts_ration[IRQ_RUNTIME_STAT_TIMES];
 #endif
 
 static void scan_burst_irq(struct timer_list *t)
 {
-	int i, j, irq_occur_value, index;
+	unsigned i, index;
+	int j, irq_occur_value;
 	unsigned int tmp_kstat_irq;
 	struct irq_desc *desc;
 	struct irqaction *action;
