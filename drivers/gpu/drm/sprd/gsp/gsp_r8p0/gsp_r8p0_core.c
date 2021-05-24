ec8afacf5d5b5df9b5a382b8b4ae169587bd92bd
guanghui.han <guanghui.han@unisoc.com>
2021-05-24 01:44:26 +0000

    Bug #1610560 drm: sprd: Android12 sharkl3 Gsp widevine code
    [root cause  ] Android12 sharkl3 Gsp widevine code
    [changes     ] gsp kernel driver
    [side effects] no
    [self test   ]:no
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9863a
    [test case]:display ok
    [reviewers   ] chen.he
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Android12 sharkl5Pro Gsp widevine code.
    
    Change-Id: Ic2eee4f4566b5bd52f4a811d194fccc8240965d4
    Signed-off-by: Guanghui Han<guanghui.han@unisoc.com>

--- drivers/gpu/drm/sprd/gsp/gsp_r8p0/gsp_r8p0_core.c
+++ drivers/gpu/drm/sprd/gsp/gsp_r8p0/gsp_r8p0_core.c
@@ -30,7 +30,9 @@
 #include "gsp_r8p0_reg.h"
 #include "gsp_r8p0_coef_cal.h"
 #include "../gsp_interface.h"
+#include "gsp_ipc_trusty.h"
 
+static bool tipc_init;
 static int zorder_used[R8P0_IMGL_NUM + R8P0_OSDL_NUM] = {0};
 
 static void print_image_layer_cfg(struct gsp_r8p0_img_layer *layer)
@@ -1494,6 +1496,9 @@ int gsp_r8p0_core_trigger(struct gsp_core *c)
 	struct gsp_r8p0_cfg *cfg = NULL;
 	struct gsp_r8p0_core *core = (struct gsp_r8p0_core *)c;
 	struct R8P0_GSP_GLB_CFG_REG gsp_mod1_cfg_value;
+	unsigned char buf[32];
+	struct gsp_message in_buf;
+	struct gsp_message out_buf;
 
 	if (gsp_core_verify(c)) {
 		GSP_ERR("gsp_r8p0 core trigger params error\n");
@@ -1517,6 +1522,16 @@ int gsp_r8p0_core_trigger(struct gsp_core *c)
 	base = c->base;
 	cfg = (struct gsp_r8p0_cfg *)kcfg->cfg;
 
+	if (!tipc_init && cfg->misc.secure_en == 1) {
+		ret = gsp_tipc_init();
+		if (!ret) {
+			tipc_init = true;
+			gsp_tipc_read(buf, sizeof(buf));
+			GSP_DEBUG("tipc init succsess\n");
+		} else
+			GSP_ERR("tipc init failed\n");
+	}
+
 	gsp_r8p0_coef_gen_and_cfg(core, cfg);
 
 	gsp_r8p0_core_misc_reg_set(c, cfg);
@@ -1531,6 +1546,15 @@ int gsp_r8p0_core_trigger(struct gsp_core *c)
 		return GSP_K_CLK_CHK_ERR;
 	}
 
+	if (cfg->misc.secure_en == 1) {
+		in_buf.cmd = TA_SET_SECURE;
+		gsp_tipc_write(&in_buf, sizeof(in_buf));
+		gsp_tipc_read(&out_buf, sizeof(out_buf));
+		if ((out_buf.cmd == TA_SET_SECURE) &&
+			(out_buf.payload[0] == 1))
+			GSP_DEBUG("TA_SET_SECURE success\n");
+	}
+
 	gsp_r8p0_core_run(c);
 
 	return 0;
@@ -1539,8 +1563,34 @@ int gsp_r8p0_core_trigger(struct gsp_core *c)
 int gsp_r8p0_core_release(struct gsp_core *c)
 {
 	struct gsp_r8p0_core *core = NULL;
+	struct gsp_kcfg *kcfg = NULL;
+	struct gsp_r8p0_cfg *cfg = NULL;
+	struct gsp_message in_buf;
+	struct gsp_message out_buf;
 
 	core = (struct gsp_r8p0_core *)c;
+
+	if (gsp_core_verify(c)) {
+		GSP_ERR("gsp_r8p0 core trigger params error\n");
+		return -1;
+	}
+
+	kcfg = c->current_kcfg;
+	if (gsp_kcfg_verify(kcfg)) {
+		GSP_ERR("gsp_r8p0 trigger invalidate kcfg\n");
+		return -1;
+	}
+
+	cfg = (struct gsp_r8p0_cfg *)kcfg->cfg;
+
+	if (cfg->misc.secure_en == 1) {
+		in_buf.cmd = TA_SET_UNSECURE;
+		gsp_tipc_write(&in_buf, sizeof(in_buf));
+		gsp_tipc_read(&out_buf, sizeof(out_buf));
+		if ((out_buf.cmd == TA_SET_UNSECURE)
+			&& (out_buf.payload[0] == 1))
+			GSP_DEBUG("TA_SET_UNSECURE success\n");
+	}
 
 	return 0;
 }
