ac1e727909a089a2e5d56819e299776c7193d59d
Xuewen Yan <xuewen.yan@unisoc.com>
2021-06-23 17:10:50 +0800

    Bug #1551433 sched: add a module to convert tp into events
    [root cause  ] add trace
    [changes     ] module
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:9863a
    [test case]:systrace
    [reviewers   ]ke.wang
    [change_type ] debug_log
    [tag_product ] common
    
    event:
    sched:sched_overutilized
    sched:sched_pelt_cfs
    sched:sched_pelt_dl
    sched:sched_pelt_irq
    sched:sched_pelt_rt
    sched:sched_pelt_se
    sched:sched_util_est_cfs
    sched:sched_util_est_se
    sched:uclamp_util_cfs
    sched:uclamp_util_se
    
    example:
    <...>-1058 ( 1058) [000] d.h3 401.842881: sched_pelt_irq: cpu=0 load=0 rbl_load=0 util=77 update_time=401877424128
    <...>-1454 ( 1408) [001] d..3 401.843482: sched_util_est_se: cpu=1 path=(null) comm=Binder:1408_3 pid=1454 enqueued=19 ewma=50 util=19
    <...>-1058 ( 1058) [000] d.h3 401.842884: sched_util_est_cfs: cpu=0 path=/ enqueued=343 ewma=0 util=227
    <idle>-0 (-------) [004] d.h3 401.842982: sched_pelt_cfs: cpu=4 path=/foreground load=92 rbl_load=22 util=680 update_time=398462793728
    <...>-1058 ( 1058) [000] d.h3 401.842886: sched_pelt_se: cpu=0 path=(null) comm=kworker/u17:5 pid=2244 load=2373 rbl_load=2373 util=17 update_time=374468623360
    <idle>-0 (-------) [006] d.s3 401.844064: sched_pelt_rt: cpu=6 load=0 rbl_load=0 util=0 update_time=398057872384
    <...>-1058 ( 1058) [000] d.h3 401.842888: uclamp_util_se: pid=2244 comm=kworker/u17:5 cpu=0 util_avg=17 uclamp_avg=17 uclamp_min=0 uclamp_max=1024
    kswapd0-144(  144) [007] d.h2 401.844073: uclamp_util_cfs: cpu=7 util_avg=1024 uclamp_avg=1024 uclamp_min=0 uclamp_max=1024
    <idle>-0 (-------) [006] ..s2 401.844115: sched_overutilized: overutilized=1 span=0xff
    
    Change-Id: I605e507b11972daf29a02cd9746b4f64cc9e4638
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>

---
+++ kernel/sched/sched_events.h
@@ -0,0 +1,280 @@
+// SPDX-License-Identifier: GPL-2.0
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM sched
+
+#define MAX_SPAN_SIZE		128
+
+#if !defined(_SCHED_EVENTS_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _SCHED_EVENTS_H
+
+#define PATH_SIZE		64
+#define __SPAN_SIZE		(round_up(NR_CPUS, 4)/4)
+#define SPAN_SIZE		(__SPAN_SIZE > MAX_SPAN_SIZE ? MAX_SPAN_SIZE : __SPAN_SIZE)
+
+#include <linux/tracepoint.h>
+
+#define RBL_LOAD_ENTRY		rbl_load
+#define RBL_LOAD_MEMBER		runnable_load_avg
+#define RBL_LOAD_STR		"rbl_load"
+
+TRACE_EVENT(sched_pelt_cfs,
+
+	TP_PROTO(int cpu, char *path, const struct sched_avg *avg),
+
+	TP_ARGS(cpu, path, avg),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__array(char,		path,	PATH_SIZE)
+		__field(unsigned long,	load)
+		__field(unsigned long,	RBL_LOAD_ENTRY)
+		__field(unsigned long,	util)
+		__field(unsigned long long, update_time)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		strlcpy(__entry->path, path, PATH_SIZE);
+		__entry->load		= avg->load_avg;
+		__entry->RBL_LOAD_ENTRY	= avg->RBL_LOAD_MEMBER;
+		__entry->util		= avg->util_avg;
+		__entry->update_time    = avg->last_update_time;
+	),
+
+	TP_printk("cpu=%d path=%s load=%lu " RBL_LOAD_STR "=%lu util=%lu update_time=%llu",
+		  __entry->cpu, __entry->path, __entry->load,
+		  __entry->RBL_LOAD_ENTRY, __entry->util, __entry->update_time)
+);
+
+DECLARE_EVENT_CLASS(sched_pelt_rq_template,
+
+	TP_PROTO(int cpu, const struct sched_avg *avg),
+
+	TP_ARGS(cpu, avg),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__field(unsigned long,	load)
+		__field(unsigned long,	RBL_LOAD_ENTRY)
+		__field(unsigned long,	util)
+		__field(unsigned long long, update_time)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		__entry->load		= avg->load_avg;
+		__entry->RBL_LOAD_ENTRY	= avg->RBL_LOAD_MEMBER;
+		__entry->util		= avg->util_avg;
+		__entry->update_time    = avg->last_update_time;
+	),
+
+	TP_printk("cpu=%d load=%lu " RBL_LOAD_STR "=%lu util=%lu update_time=%llu",
+		  __entry->cpu, __entry->load,
+		  __entry->RBL_LOAD_ENTRY, __entry->util, __entry->update_time)
+);
+
+DEFINE_EVENT(sched_pelt_rq_template, sched_pelt_rt,
+	TP_PROTO(int cpu, const struct sched_avg *avg),
+	TP_ARGS(cpu, avg));
+
+DEFINE_EVENT(sched_pelt_rq_template, sched_pelt_dl,
+	TP_PROTO(int cpu, const struct sched_avg *avg),
+	TP_ARGS(cpu, avg));
+
+DEFINE_EVENT(sched_pelt_rq_template, sched_pelt_irq,
+	TP_PROTO(int cpu, const struct sched_avg *avg),
+	TP_ARGS(cpu, avg));
+
+TRACE_EVENT(sched_pelt_se,
+
+	TP_PROTO(int cpu, char *path, char *comm, int pid, const struct sched_avg *avg),
+
+	TP_ARGS(cpu, path, comm, pid, avg),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__array(char,		path,	PATH_SIZE)
+		__array(char,		comm,	TASK_COMM_LEN)
+		__field(int,		pid)
+		__field(unsigned long,	load)
+		__field(unsigned long,	RBL_LOAD_ENTRY)
+		__field(unsigned long,	util)
+		__field(unsigned long long, update_time)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		strlcpy(__entry->path, path, PATH_SIZE);
+		strlcpy(__entry->comm, comm, TASK_COMM_LEN);
+		__entry->pid		= pid;
+		__entry->load		= avg->load_avg;
+		__entry->RBL_LOAD_ENTRY	= avg->RBL_LOAD_MEMBER;
+		__entry->util		= avg->util_avg;
+		__entry->update_time    = avg->last_update_time;
+	),
+
+	TP_printk("cpu=%d path=%s comm=%s pid=%d load=%lu " RBL_LOAD_STR
+		  "=%lu util=%lu update_time=%llu",  __entry->cpu, __entry->path,
+		  __entry->comm, __entry->pid, __entry->load,
+		  __entry->RBL_LOAD_ENTRY, __entry->util, __entry->update_time)
+);
+
+TRACE_EVENT(sched_overutilized,
+
+	TP_PROTO(int overutilized, char *span),
+
+	TP_ARGS(overutilized, span),
+
+	TP_STRUCT__entry(
+		__field(int,	overutilized)
+		__array(char,	span,	SPAN_SIZE)
+	),
+
+	TP_fast_assign(
+		__entry->overutilized	= overutilized;
+		strlcpy(__entry->span, span, SPAN_SIZE);
+	),
+
+	TP_printk("overutilized=%d span=0x%s",
+		  __entry->overutilized, __entry->span)
+);
+
+TRACE_EVENT(sched_util_est_se,
+
+	TP_PROTO(int cpu, char *path, char *comm, int pid,
+		 const struct sched_avg *avg),
+
+	TP_ARGS(cpu, path, comm, pid, avg),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__array(char,		path,	PATH_SIZE)
+		__array(char,		comm,	TASK_COMM_LEN)
+		__field(int,		pid)
+		__field(unsigned int,	enqueued)
+		__field(unsigned int,	ewma)
+		__field(unsigned long,	util)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		strlcpy(__entry->path, path, PATH_SIZE);
+		strlcpy(__entry->comm, comm, TASK_COMM_LEN);
+		__entry->pid		= pid;
+		__entry->enqueued	= avg->util_est.enqueued & 0x7fffffff;
+		__entry->ewma		= avg->util_est.ewma;
+		__entry->util		= avg->util_avg;
+	),
+
+	TP_printk("cpu=%d path=%s comm=%s pid=%d enqueued=%u ewma=%u util=%lu",
+		  __entry->cpu, __entry->path, __entry->comm, __entry->pid,
+		  __entry->enqueued, __entry->ewma, __entry->util)
+);
+
+TRACE_EVENT(sched_util_est_cfs,
+
+	TP_PROTO(int cpu, char *path, const struct sched_avg *avg),
+
+	TP_ARGS(cpu, path, avg),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__array(char,		path,	PATH_SIZE)
+		__field(unsigned int,	enqueued)
+		__field(unsigned int,	ewma)
+		__field(unsigned long,	util)
+	),
+
+	TP_fast_assign(
+		__entry->cpu		= cpu;
+		strlcpy(__entry->path, path, PATH_SIZE);
+		__entry->enqueued	= avg->util_est.enqueued;
+		__entry->ewma		= avg->util_est.ewma;
+		__entry->util		= avg->util_avg;
+	),
+
+	TP_printk("cpu=%d path=%s enqueued=%u ewma=%u util=%lu",
+		  __entry->cpu, __entry->path, __entry->enqueued,
+		  __entry->ewma, __entry->util)
+);
+
+#ifdef CONFIG_UCLAMP_TASK
+
+struct rq;
+
+TRACE_EVENT_CONDITION(uclamp_util_se,
+
+	TP_PROTO(bool is_task, struct task_struct *p, struct rq *rq),
+
+	TP_ARGS(is_task, p, rq),
+
+	TP_CONDITION(is_task),
+
+	TP_STRUCT__entry(
+		__field(pid_t,		pid)
+		__array(char,		comm,   TASK_COMM_LEN)
+		__field(int,		cpu)
+		__field(unsigned long,	util_avg)
+		__field(unsigned long,	uclamp_avg)
+		__field(unsigned long,	uclamp_min)
+		__field(unsigned long,	uclamp_max)
+	),
+
+	TP_fast_assign(
+		__entry->pid            = p->pid;
+		memcpy(__entry->comm, p->comm, TASK_COMM_LEN);
+		__entry->cpu            = rq->cpu;
+		__entry->util_avg       = p->se.avg.util_avg;
+		__entry->uclamp_avg     = uclamp_rq_util_with(rq, p->se.avg.util_avg, NULL);
+		__entry->uclamp_min     = rq->uclamp[UCLAMP_MIN].value;
+		__entry->uclamp_max     = rq->uclamp[UCLAMP_MAX].value;
+		),
+
+	TP_printk("pid=%d comm=%s cpu=%d util_avg=%lu uclamp_avg=%lu uclamp_min=%lu uclamp_max=%lu",
+		  __entry->pid, __entry->comm, __entry->cpu,
+		  __entry->util_avg, __entry->uclamp_avg,
+		  __entry->uclamp_min, __entry->uclamp_max)
+);
+
+TRACE_EVENT_CONDITION(uclamp_util_cfs,
+
+	TP_PROTO(bool is_root, int cpu, struct cfs_rq *cfs_rq),
+
+	TP_ARGS(is_root, cpu, cfs_rq),
+
+	TP_CONDITION(is_root),
+
+	TP_STRUCT__entry(
+		__field(int,		cpu)
+		__field(unsigned long,	util_avg)
+		__field(unsigned long,	uclamp_avg)
+		__field(unsigned long,	uclamp_min)
+		__field(unsigned long,	uclamp_max)
+	),
+
+	TP_fast_assign(
+		__entry->cpu            = cpu;
+		__entry->util_avg       = cfs_rq->avg.util_avg;
+		__entry->uclamp_avg     = uclamp_rq_util_with(rq_of(cfs_rq), cfs_rq->avg.util_avg, NULL);
+		__entry->uclamp_min     = rq_of(cfs_rq)->uclamp[UCLAMP_MIN].value;
+		__entry->uclamp_max     = rq_of(cfs_rq)->uclamp[UCLAMP_MAX].value;
+		),
+
+	TP_printk("cpu=%d util_avg=%lu uclamp_avg=%lu uclamp_min=%lu uclamp_max=%lu",
+		  __entry->cpu, __entry->util_avg, __entry->uclamp_avg,
+		  __entry->uclamp_min, __entry->uclamp_max)
+);
+#else
+#define trace_uclamp_util_se(is_task, p, rq) (while (false) {})
+#define trace_uclamp_util_se_enabled() false
+#define trace_uclamp_util_cfs(is_root, cpu, cfs_rq) (while (false) {})
+#define trace_uclamp_util_cfs_enabled() false
+#endif /* CONFIG_UCLAMP_TASK */
+
+#endif /* _SCHED_EVENTS_H */
+
+/* This part must be outside protection */
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE sched_events
+#include <trace/define_trace.h>
