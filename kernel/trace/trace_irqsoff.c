60710e267ede0a32e40d9e730e9ce21d7dd99797
weihongbin <weihongbin@chino-e.com>
2021-07-20 14:52:17 +0800

    EKmoto_e40: debug: solve deadlock problem
    Description:
    From 898148c951f15432d1b7f5c342bf30b74b020011 Mon Sep 17 00:00:00 2001
    From: Ben Dai <ben.dai@unisoc.com>
    Date: Tue, 06 Apr 2021 20:23:48 +0800
    Subject: [PATCH] Bug #1532115 soc: sprd: debug: slove deadlock problem
    
    [root cause  ] slove deadlock problem
    [changes     ] eirqsoff
    [side effects] unknown
    [self test   ]: pass
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: none
    [board]: all
    [test case]: boot normally
    [reviewers   ] nianfu.bai
    [change_type ] feature_bugfix
    [tag_product ] common
    
    St-result: PASS
    
    Change-Id: I55c87423238a9d5d3d21af2e7b541f45d92f43b8

--- kernel/trace/trace_irqsoff.c
+++ kernel/trace/trace_irqsoff.c
@@ -444,15 +444,15 @@ void start_critical_timings(void)
 {
 	if (preempt_trace() || irq_trace())
 		start_critical_timing(CALLER_ADDR0, CALLER_ADDR1);
-	start_eirqsoff_timing(CALLER_ADDR0, CALLER_ADDR1);
-	start_epreempt_timing(CALLER_ADDR0, CALLER_ADDR1);
+	continue_eirqsoff_timing();
+	continue_epreempt_timing();
 }
 EXPORT_SYMBOL_GPL(start_critical_timings);
 
 void stop_critical_timings(void)
 {
-	stop_eirqsoff_timing(CALLER_ADDR0, CALLER_ADDR1);
-	stop_epreempt_timing(CALLER_ADDR0, CALLER_ADDR1);
+	pause_eirqsoff_timing();
+	pause_epreempt_timing();
 	if (preempt_trace() || irq_trace())
 		stop_critical_timing(CALLER_ADDR0, CALLER_ADDR1);
 }
