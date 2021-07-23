2b2ce15452c7995e6f8271a2f30fe5b44ca66e63
weihongbin <weihongbin@chino-e.com>
2021-07-23 17:55:24 +0800

    Revert "EKmoto_e40: debug: solve deadlock problem"
    This reverts commit 7f87262f5b3f844eaf20cf2ffcb3193a1af45287.
    
    Change-Id: I3129de3fbeb51b3fd05b092516601256e9443b48

--- drivers/soc/sprd/debug/irq/eirqsoff/trace_eirqsoff.c
+++ drivers/soc/sprd/debug/irq/eirqsoff/trace_eirqsoff.c
@@ -1,4 +1,4 @@
-#define pr_fmt(fmt) "sprd_eirqsoff warning: " fmt
+#define pr_fmt(fmt) "Warning: sprd_dbg: " fmt
 #include <linux/debugfs.h>
 #include <linux/ftrace.h>
 #include <linux/kallsyms.h>
@@ -11,66 +11,39 @@
 
 #define DEFAULT_WARNING_INTERVAL (30*NSEC_PER_MSEC)
 
-extern void show_stack(struct task_struct *task, unsigned long *sp);
-
-static u64 __read_mostly warning_interval = DEFAULT_WARNING_INTERVAL;
-
-static DEFINE_PER_CPU(u64, eirqsoff_start_timestamp);
-static DEFINE_PER_CPU(u64, eirqsoff_pause_timestamp);
-static DEFINE_PER_CPU(u64, eirqsoff_paused_interval);
+static int __read_mostly trace_ready;
+static DEFINE_PER_CPU(unsigned int, eirqsoff_is_tracing);
+static DEFINE_PER_CPU(unsigned long long, eirqsoff_start_timestamp);
 
 static DEFINE_PER_CPU(pid_t, eirqsoff_pid);
 static DEFINE_PER_CPU(unsigned long, eirqsoff_ip);
 static DEFINE_PER_CPU(unsigned long, eirqsoff_parent_ip);
 
+static unsigned long long __read_mostly warning_interval;
+
 #ifdef CONFIG_PREEMPT_TRACER
-static u64 __read_mostly epreempt_interval = DEFAULT_WARNING_INTERVAL<<2;
 static DEFINE_PER_CPU(unsigned int, epreempt_is_tracing);
-static DEFINE_PER_CPU(u64, epreempt_start_timestamp);
-static DEFINE_PER_CPU(u64, epreempt_paused_interval);
-static DEFINE_PER_CPU(u64, epreempt_pause_timestamp);
+static DEFINE_PER_CPU(unsigned long long, epreempt_start_timestamp);
 
 static DEFINE_PER_CPU(pid_t, epreempt_pid);
 static DEFINE_PER_CPU(unsigned long, epreempt_ip);
 static DEFINE_PER_CPU(unsigned long, epreempt_parent_ip);
+
+static unsigned long long __read_mostly epreempt_interval;
 #endif
-
-
-static void notrace clear_eirqsoff_data(void)
-{
-	__this_cpu_write(eirqsoff_pid, 0);
-	__this_cpu_write(eirqsoff_ip, 0);
-	__this_cpu_write(eirqsoff_parent_ip, 0);
-	__this_cpu_write(eirqsoff_start_timestamp, 0);
-	__this_cpu_write(eirqsoff_pause_timestamp, 0);
-	__this_cpu_write(eirqsoff_paused_interval, 0);
-}
-
-static void notrace show_eirqsoff_info(u64 start_time, u64 interval)
-{
-	u64 start_timestamp_ms = do_div(start_time, NSEC_PER_SEC);
-	u64 interval_us = do_div(interval, NSEC_PER_MSEC);
-
-	pr_warn("Detected Process %d disable interrupt %lld.%06lldms "
-		"from %lld.%09llds\n",
-		__this_cpu_read(eirqsoff_pid),
-		interval, interval_us,
-		start_time, start_timestamp_ms);
-
-	pr_warn("disable at:\n");
-	print_ip_sym(__this_cpu_read(eirqsoff_parent_ip));
-	print_ip_sym(__this_cpu_read(eirqsoff_ip));
-	show_stack(NULL, NULL);
-}
 
 void notrace start_eirqsoff_timing(unsigned long ip, unsigned long parent_ip)
 {
+	unsigned long long current_clock;
+	pid_t current_pid;
+
 	if (!irqs_disabled())
-		/* Should never into here! */
 		return;
 
-	/* Don't trace idle thread */
 	if (current->pid == 0)
+		return;
+
+	if (__this_cpu_read(eirqsoff_is_tracing))
 		return;
 
 	if (__this_cpu_read(eirqsoff_start_timestamp))
@@ -79,40 +52,35 @@ void notrace start_eirqsoff_timing(unsigned long ip, unsigned long parent_ip)
 	if (oops_in_progress)
 		return;
 
-	__this_cpu_write(eirqsoff_pid, current->pid);
+	current_pid = current->pid;
+	__this_cpu_write(eirqsoff_pid, current_pid);
 	__this_cpu_write(eirqsoff_ip, ip);
 	__this_cpu_write(eirqsoff_parent_ip, parent_ip);
-	__this_cpu_write(eirqsoff_start_timestamp, sched_clock());
-}
+	current_clock = sched_clock();
+	__this_cpu_write(eirqsoff_start_timestamp, current_clock);
 
-void notrace pause_eirqsoff_timing(void)
-{
-	if (!__this_cpu_read(eirqsoff_start_timestamp))
-		return;
+	__this_cpu_write(eirqsoff_is_tracing, 1);
 
-	__this_cpu_write(eirqsoff_pause_timestamp, sched_clock());
-}
-
-void notrace continue_eirqsoff_timing(void)
-{
-	u64 pause_ts = __this_cpu_read(eirqsoff_pause_timestamp);
-
-	if (!__this_cpu_read(eirqsoff_start_timestamp) || !pause_ts)
-		return;
-
-	__this_cpu_add(eirqsoff_paused_interval, sched_clock() - pause_ts);
 }
 
 void notrace stop_eirqsoff_timing(unsigned long ip, unsigned long parent_ip)
 {
-	u64 start_timestamp, stop_timestamp;
-	u64 interval;
+	unsigned long long stop_timestamp;
+	unsigned long long start_timestamp;
+	unsigned long long start_timestamp_ms;
+	unsigned long long interval;
+	unsigned long long interval_us;
+
 	if (!irqs_disabled())
 		return;
 
-	if (!__this_cpu_read(eirqsoff_start_timestamp)) {
-		clear_eirqsoff_data();
-	}
+	if (unlikely(!trace_ready))
+		return;
+
+	if (!__this_cpu_read(eirqsoff_is_tracing))
+		return;
+
+	__this_cpu_write(eirqsoff_is_tracing, 0);
 
 	if (!oops_in_progress) {
 
@@ -120,47 +88,46 @@ void notrace stop_eirqsoff_timing(unsigned long ip, unsigned long parent_ip)
 		start_timestamp = __this_cpu_read(eirqsoff_start_timestamp);
 
 		interval = stop_timestamp - start_timestamp;
-		interval -= __this_cpu_read(eirqsoff_paused_interval);
 
 		if (interval > warning_interval) {
-			show_eirqsoff_info(start_timestamp, interval);
+
+			start_timestamp_ms = do_div(start_timestamp, NSEC_PER_SEC);
+			interval_us = do_div(interval, NSEC_PER_MSEC);
+
+			pr_warn("irqsoff: Process %d detected Process %d disable interrupt "
+				"%lld.%06lldms from %lld.%09llds\n",
+				current->pid,
+				__this_cpu_read(eirqsoff_pid),
+				interval,
+				interval_us,
+				start_timestamp,
+				start_timestamp_ms);
+			pr_warn("disable at:\n");
+			print_ip_sym(__this_cpu_read(eirqsoff_parent_ip));
+			print_ip_sym(__this_cpu_read(eirqsoff_ip));
+			dump_stack();
 		}
 	}
-	clear_eirqsoff_data();
+	__this_cpu_write(eirqsoff_start_timestamp, 0);
 }
 
 
 #ifdef CONFIG_PREEMPT_TRACER
-static void notrace clear_epreempt_data(void)
-{
-	__this_cpu_write(epreempt_pid, 0);
-	__this_cpu_write(epreempt_ip, 0);
-	__this_cpu_write(epreempt_parent_ip, 0);
-	__this_cpu_write(epreempt_start_timestamp, 0);
-	__this_cpu_write(epreempt_pause_timestamp, 0);
-	__this_cpu_write(epreempt_paused_interval, 0);
-}
-
-static void notrace show_epreempt_info(u64 start_time, u64 interval)
-{
-	u64 start_timestamp_ms = do_div(start_time, NSEC_PER_SEC);
-	u64 interval_us = do_div(interval, NSEC_PER_MSEC);
-
-	pr_warn("Detected Process %d disable preempt %lld.%06lldms "
-		"from %lld.%09llds\n",
-		__this_cpu_read(epreempt_pid),
-		interval, interval_us,
-		start_time, start_timestamp_ms);
-
-	pr_warn("disable at:\n");
-	print_ip_sym(__this_cpu_read(epreempt_parent_ip));
-	print_ip_sym(__this_cpu_read(epreempt_ip));
-	show_stack(NULL, NULL);
-}
-
 void notrace start_epreempt_timing(unsigned long ip, unsigned long parent_ip)
 {
+	unsigned long long current_clock;
+	pid_t current_pid;
+
 	if (current->pid == 0)
+		return;
+
+	if (__this_cpu_read(eirqsoff_is_tracing))
+		return;
+
+	if (!preempt_count())
+		return;
+
+	if (__this_cpu_read(epreempt_is_tracing))
 		return;
 
 	if (__this_cpu_read(epreempt_start_timestamp))
@@ -169,39 +136,35 @@ void notrace start_epreempt_timing(unsigned long ip, unsigned long parent_ip)
 	if (oops_in_progress)
 		return;
 
-	__this_cpu_write(epreempt_pid, current->pid);
+	current_pid = current->pid;
+	__this_cpu_write(epreempt_pid, current_pid);
 	__this_cpu_write(epreempt_ip, ip);
 	__this_cpu_write(epreempt_parent_ip, parent_ip);
-	__this_cpu_write(epreempt_start_timestamp, sched_clock());
+	current_clock = sched_clock();
+	__this_cpu_write(epreempt_start_timestamp, current_clock);
+
+	__this_cpu_write(epreempt_is_tracing, 1);
+
 }
 
-void notrace pause_epreempt_timing(void)
-{
-	if (!__this_cpu_read(epreempt_start_timestamp))
-		return;
-
-	__this_cpu_write(epreempt_pause_timestamp, sched_clock());
-}
-
-void notrace continue_epreempt_timing(void)
-{
-	u64 pause_ts = __this_cpu_read(epreempt_pause_timestamp);
-
-	if (!__this_cpu_read(epreempt_start_timestamp) || !pause_ts)
-		return;
-
-	__this_cpu_add(epreempt_paused_interval, sched_clock() - pause_ts);
-}
 
 void notrace stop_epreempt_timing(unsigned long ip, unsigned long parent_ip)
 {
-	u64 start_timestamp, stop_timestamp;
-	u64 interval;
 
-	if (!__this_cpu_read(epreempt_start_timestamp)) {
-		clear_epreempt_data();
- 		return;
-	}
+	unsigned long long stop_timestamp;
+	unsigned long long start_timestamp;
+	unsigned long long start_timestamp_ms;
+	unsigned long long interval;
+	unsigned long long interval_us;
+
+	if (unlikely(!trace_ready))
+		return;
+
+	if (!__this_cpu_read(epreempt_is_tracing))
+		return;
+
+	if (!preempt_count())
+		return;
 
 	__this_cpu_write(epreempt_is_tracing, 0);
 
@@ -210,13 +173,27 @@ void notrace stop_epreempt_timing(unsigned long ip, unsigned long parent_ip)
 		start_timestamp = __this_cpu_read(epreempt_start_timestamp);
 
 		interval = stop_timestamp - start_timestamp;
-		interval -= __this_cpu_read(epreempt_paused_interval);
 
 		if (interval > epreempt_interval) {
-			show_epreempt_info(start_timestamp, interval);
+
+			start_timestamp_ms = do_div(start_timestamp, NSEC_PER_SEC);
+			interval_us = do_div(interval, NSEC_PER_MSEC);
+
+			pr_warn("irqsoff: Process %d detected Process %d disable preempt "
+				"%lld.%06lldms from %lld.%09llds\n",
+				current->pid,
+				 __this_cpu_read(epreempt_pid),
+				interval,
+				interval_us,
+				start_timestamp,
+				start_timestamp_ms);
+			pr_warn("disable at:\n");
+			print_ip_sym(__this_cpu_read(epreempt_parent_ip));
+			print_ip_sym(__this_cpu_read(epreempt_ip));
+			dump_stack();
 		}
 	}
-	clear_epreempt_data();
+	__this_cpu_write(epreempt_start_timestamp, 0);
 
 }
 
@@ -228,18 +205,19 @@ static int notrace eirqsoff_interval_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int
-notrace eirqsoff_interval_open(struct inode *inodep, struct file *filep)
+static int notrace eirqsoff_interval_open(struct inode *inodep,
+					  struct file *filep)
 {
 	single_open(filep, eirqsoff_interval_show, NULL);
 	return 0;
 }
 
-static ssize_t
-notrace eirqsoff_interval_write(struct file *filep, const char __user *buf,
-				size_t len, loff_t *ppos)
+static ssize_t notrace eirqsoff_interval_write(struct file *filep,
+					       const char __user *buf,
+					       size_t len,
+					       loff_t *ppos)
 {
-	u64 interval;
+	unsigned long long interval;
 	int err;
 
 	if (len <= 5 || len >= 11)
@@ -268,8 +246,27 @@ const struct file_operations eirqsoff_interval_fops = {
 
 static int __init trace_eirqsoff_init(void)
 {
-	debugfs_create_file("warning_interval", 0644, sprd_debugfs_entry(IRQ),
-			    NULL, &eirqsoff_interval_fops);
+	int cpu;
+
+	for_each_possible_cpu(cpu) {
+		per_cpu(eirqsoff_start_timestamp, cpu) = 0;
+		per_cpu(eirqsoff_is_tracing, cpu) = 0;
+#ifdef CONFIG_PREEMPT_TRACER
+		per_cpu(epreempt_start_timestamp, cpu) = 0;
+		per_cpu(epreempt_is_tracing, cpu) = 0;
+#endif
+	}
+	warning_interval = DEFAULT_WARNING_INTERVAL;
+#ifdef CONFIG_PREEMPT_TRACER
+	epreempt_interval = DEFAULT_WARNING_INTERVAL<<2;
+#endif
+	trace_ready = 1;
+
+	debugfs_create_file("warning_interval",
+			    0644,
+			    sprd_debugfs_entry(IRQ),
+			    NULL,
+			    &eirqsoff_interval_fops);
 	return 0;
 }
 
