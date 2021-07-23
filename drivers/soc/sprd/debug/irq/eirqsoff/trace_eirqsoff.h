c5b373e047f068b8920cfcb394997bd1eea0268f
weihongbin <weihongbin@chino-e.com>
2021-07-23 17:55:24 +0800

    Revert "EKmoto_e40: debug: solve deadlock problem"
    This reverts commit 7f87262f5b3f844eaf20cf2ffcb3193a1af45287.
    
    Change-Id: I3129de3fbeb51b3fd05b092516601256e9443b48

--- drivers/soc/sprd/debug/irq/eirqsoff/trace_eirqsoff.h
+++ drivers/soc/sprd/debug/irq/eirqsoff/trace_eirqsoff.h
@@ -2,39 +2,45 @@
 #define __SPRD_EIRQSOFF_H
 
 #ifdef CONFIG_SPRD_EIRQSOFF
-void start_eirqsoff_timing(unsigned long ip, unsigned long parent_ip);
-void stop_eirqsoff_timing(unsigned long ip, unsigned long parent_ip);
-void pause_eirqsoff_timing(void);
-void continue_eirqsoff_timing(void);
+extern void notrace
+start_eirqsoff_timing(unsigned long ip, unsigned long parent_ip);
+extern void notrace
+stop_eirqsoff_timing(unsigned long ip, unsigned long parent_ip);
 #ifdef CONFIG_PREEMPT_TRACER
-void start_epreempt_timing(unsigned long ip, unsigned long parent_ip);
-void stop_epreempt_timing(unsigned long ip, unsigned long parent_ip);
-void pause_epreempt_timing(void);
-void continue_epreempt_timing(void);
-#else /* !CONFIG_PREEMPT_TRACER */
-#define start_epreempt_timing(ip, parent_ip)	do { } while (0)
-#define stop_epreempt_timing(ip, parent_ip)	do { } while (0)
-#define pause_epreempt_timing()			do { } while (0)
-#define continue_epreempt_timing()		do { } while (0)
+extern void notrace
+start_epreempt_timing(unsigned long ip, unsigned long parent_ip);
+extern void notrace
+stop_epreempt_timing(unsigned long ip, unsigned long parent_ip);
+#else
+#define start_epreempt_timing(ip, parent_ip)  \
+do { } while (0)
+#define stop_epreempt_timing(ip, parent_ip)   \
+do { } while (0)
 #endif
 #ifdef CONFIG_HARDLOCKUP_DETECTOR_OTHER_CPU
-void start_irqsoff_panic_timing(void);
-void stop_irqsoff_panic_timing(void);
+extern void notrace
+start_irqsoff_panic_timing(void);
+extern void notrace
+stop_irqsoff_panic_timing(void);
 #else
-#define start_irqsoff_panic_timing()		do { } while (0)
-#define stop_irqsoff_panic_timing()		do { } while (0)
+#define start_irqsoff_panic_timing()  \
+do { } while (0)
+#define stop_irqsoff_panic_timing()   \
+do { } while (0)
 #endif
-#else /* !CONFIG_SPRD_EIRQSOFF */
-#define start_eirqsoff_timing(ip, parent_ip)	do { } while (0)
-#define stop_eirqsoff_timing(ip, parent_ip)	do { } while (0)
-#define pause_eirqsoff_timing()			do { } while (0)
-#define continue_eirqsoff_timing()		do { } while (0)
-#define start_epreempt_timing(ip, parent_ip)	do { } while (0)
-#define stop_epreempt_timing(ip, parent_ip)	do { } while (0)
-#define pause_epreempt_timing()			do { } while (0)
-#define continue_epreempt_timing()		do { } while (0)
-#define start_irqsoff_panic_timing()		do { } while (0)
-#define stop_irqsoff_panic_timing()		do { } while (0)
+#else
+#define start_eirqsoff_timing(ip, parent_ip)  \
+do { } while (0)
+#define stop_eirqsoff_timing(ip, parent_ip)   \
+do { } while (0)
+#define start_irqsoff_panic_timing()  \
+do { } while (0)
+#define stop_irqsoff_panic_timing()   \
+do { } while (0)
+#define start_epreempt_timing(ip, parent_ip)  \
+do { } while (0)
+#define stop_epreempt_timing(ip, parent_ip)   \
+do { } while (0)
 #endif
 
 #endif
