224f892f4c269604019031d29a2ae9d09feef53c
ontim_lishunyu <shunyu.li@chino-e.com>
2021-07-30 15:15:43 +0800

    <lishunyu><moto_e20> stop charge
    Change-Id: I6adf33859c3355820e35c8bd317bd15c614f522a
    (cherry picked from commit 3257fc0f74e2e10f19ba7df913d45e7bfb25d963)

--- drivers/power/supply/cx7601-charger.c
+++ drivers/power/supply/cx7601-charger.c
@@ -189,9 +189,9 @@ static int  cx7601_enable_powerpath(struct cx7601_charger_info *info, bool en)
 	dev_err(info->dev,"%s; %d;\n", __func__,en);
 
 	if(en)
-		ret=cx7601_charger_set_vindpm(info, 4400);
+		ret=cx7601_charger_set_vindpm(info, 4520);
 	else	
-		ret=cx7601_charger_set_vindpm(info, 5400);
+		ret=cx7601_charger_set_vindpm(info, 5080);
 
 	return ret;
 }
@@ -898,52 +898,8 @@ static void cx7601_charger_work(struct work_struct *data)
 {
 	struct cx7601_charger_info *info =
 		container_of(data, struct cx7601_charger_info, work);
-	int limit_cur, cur, ret;
 	bool present = cx7601_charger_is_bat_present(info);
 
-	mutex_lock(&info->lock);
-
-	if (info->limit > 0 && !info->charging && present) {
-		/* set current limitation and start to charge */
-		switch (info->usb_phy->chg_type) {
-		case SDP_TYPE:
-			limit_cur = info->cur.sdp_limit;
-			cur = info->cur.sdp_cur;
-			break;
-		case DCP_TYPE:
-			limit_cur = info->cur.dcp_limit;
-			cur = info->cur.dcp_cur;
-			break;
-		case CDP_TYPE:
-			limit_cur = info->cur.cdp_limit;
-			cur = info->cur.cdp_cur;
-			break;
-		default:
-			limit_cur = info->cur.unknown_limit;
-			cur = info->cur.unknown_cur;
-		}
-
-		ret = cx7601_charger_set_limit_current(info, limit_cur);
-		if (ret)
-			goto out;
-
-		ret = cx7601_charger_set_current(info, cur);
-		if (ret)
-			goto out;
-
-		ret = cx7601_charger_start_charge(info);
-		if (ret)
-			goto out;
-
-		info->charging = true;
-	} else if ((!info->limit && info->charging) || !present) {
-		/* Stop charging */
-		info->charging = false;
-		cx7601_charger_stop_charge(info);
-	}
-
-out:
-	mutex_unlock(&info->lock);
 	dev_info(info->dev, "battery present = %d, charger type = %d\n",
 		 present, info->usb_phy->chg_type);
 	cm_notify_event(info->psy_usb, CM_EVENT_CHG_START_STOP, NULL);
