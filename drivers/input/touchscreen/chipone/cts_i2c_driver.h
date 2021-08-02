8ab54a7a4b5670ba934a5132a2a38bdebc33440d
zhangshaohu <Shaohu.zhang@chino-e.com>
2021-08-02 09:47:34 +0800

    Revert "EKCYPRUS-2386: bringup 3th lcd icnl9911c in kernel."
    This reverts commit 56604361ab8c6678ee95690b5ee7c291d280c0f6.
    
    Change-Id: I2b3d4e2bee6b68d2af074e9fae89a185d2399425

--- drivers/input/touchscreen/chipone/cts_i2c_driver.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CTS_I2C_DRIVER__
-#define __CTS_I2C_DRIVER__
-
-#include "cts_core.h"
-
-int cts_suspend(struct chipone_ts_data *cts_data);
-int cts_resume(struct chipone_ts_data *cts_data);
-
-#endif
