4dd81456c59bd0f2cebe358361419c6ca924af6a
Javier Liao <javier.liao@unisoc.com>
2021-08-13 11:49:59 +0800

    Bug #1680315 soc: sprd: pub: optimize code for earlier platform
    [root cause  ] l3/le/pike2 not support pub status mointer
    [changes     ] optimize code for earlier platform
    [side effects] N/A
    [self test   ]: boot pass
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: pike2/l3/le/l5/l5pro/l6/n6pro
    [test case]: cat property and ddr_info
    [reviewers   ] chris.wu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    optimize code for earlier platform (l3/le/pike2) which not support
    pub status mointer.
    
    Change-Id: I54ddd89d219d39cc744f9a4a20934c8b6fe6f031
    Signed-off-by: Javier Liao <javier.liao@unisoc.com>

--- drivers/soc/sprd/pub/dmc_drv.c
+++ drivers/soc/sprd/pub/dmc_drv.c
@@ -94,6 +94,18 @@ static const struct dmc_data pub_dmc_data = {
 	},
 };
 
+static const struct dmc_data pub_dmc_original_data = {
+	.proc_res = 0,
+	.mon_res = INVALID_RES_IDX, /* not support pub status mointor */
+	.size_l_offset = PUB_DMC_SIZE_L_OFFSET,
+	.size_h_offset = PUB_DMC_SIZE_H_OFFSET,
+	.type_offset = PUB_DMC_TYPE_OFFSET,
+	.mr_offset = {
+		PUB_DMC_CS0_MR_OFFSET,
+		PUB_DMC_CS1_MR_OFFSET,
+	},
+};
+
 static struct dmc_drv_data drv_data;
 
 static const char *const ddr_type_to_str[] = {
@@ -429,6 +441,8 @@ static int sprd_dmc_remove(struct platform_device *pdev)
 
 static const struct of_device_id sprd_dmc_of_match[] = {
 	{.compatible = "sprd,pub-dmc", .data = &pub_dmc_data},
+	/* for such earlier platform which not support pub status mointor */
+	{.compatible = "sprd,pub-dmc-original", .data = &pub_dmc_original_data},
 	{},
 };
 
