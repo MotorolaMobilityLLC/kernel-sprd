c2e03e0adb95d4310f5897079b37aba5fbf3e07c
Lei Xie2 <lei.xie2@shand07.spreadtrum.com>
2021-08-16 15:24:46 +0800

    Bug #1677661 drm: sprd: fix screen abnormal issue of opening esd check
    [root cause  ] max read time value may exceed u32 size
    [changes     ] change type to u64
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:ums512_1h10
    [test case]:boot pass
    [reviewers   ] thomas.chai sam.liu
    [change_type ] feature
    [tag_product ] common
    
    fix screen abnormal issue of opening esd check
    
    Change-Id: I861ba8dd89d4cfe8bc4b83cd40bbe26fd6cfe56e
    Signed-off-by: Lei Xie2 <lei.xie2@unisoc.com>
    (cherry picked from commit 2139d19067c1280367b3e2cc49a88935662dbad9)

--- drivers/gpu/drm/sprd/sprd_dsi.c
+++ drivers/gpu/drm/sprd/sprd_dsi.c
@@ -867,7 +867,7 @@ static int sprd_dsi_context_init(struct sprd_dsi *dsi, struct device_node *np)
 	if (!of_property_read_u32(np, "sprd,max-read-time", &tmp))
 		ctx->max_rd_time = tmp;
 	else
-		ctx->max_rd_time = 8000;
+		ctx->max_rd_time = 0x8000;
 
 	if (!of_property_read_u32(np, "sprd,int0_mask", &tmp))
 		ctx->int0_mask = tmp;
