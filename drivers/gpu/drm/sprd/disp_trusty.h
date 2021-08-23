c96dd9cfa7720e1774f2bad8f68d7943a57666f9
Gaofeng.Sheng <Gaofeng.Sheng@unisoc.com>
2021-08-23 21:06:40 +0800

    Bug #1676480 drm: sprd: revert add dpu r4p0 widevine feature
    [root cause  ] add dpu r4p0 widevine feature
    [changes     ] trusty_disp.c
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:none
    [confirm dependent commit]:no
    [board]:ums9230_1h10
    [test case]:widevine case
    [reviewers   ]gaofeng.sheng
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    revert add dpu r4p0 widevine feature
    
    Change-Id: Ifd366f6f77f190ede1904db417d74adc7baac5d0
    Signed-off-by: Gaofeng Sheng <gaofeng.sheng@unisoc.com>

--- drivers/gpu/drm/sprd/disp_trusty.h
+++ drivers/gpu/drm/sprd/disp_trusty.h
@@ -6,17 +6,6 @@
 #ifndef _DISP_TRUSTY_H_
 #define _DISP_TRUSTY_H_
 
-enum {
-	DPU_LITE_R1P0 = 0,
-	DPU_LITE_R2P0,
-	DPU_LITE_R3P0,
-	DPU_R2P0,
-	DPU_R3P0,
-	DPU_R4P0,
-	DPU_R5P0,
-	DPU_R6P0
-};
-
 enum disp_command {
 	TA_REG_SET = 1,
 	TA_REG_CLR,
@@ -24,9 +13,21 @@ enum disp_command {
 	TA_FIREWALL_CLR
 };
 
+struct layer_reg {
+	u32 addr[4];
+	u32 ctrl;
+	u32 size;
+	u32 pitch;
+	u32 pos;
+	u32 alpha;
+	u32 ck;
+	u32 pallete;
+	u32 crop_start;
+};
+
 struct disp_message {
-	u16 version;
-	u16 cmd;
+	u8 cmd;
+	struct layer_reg layer;
 };
 
 int disp_ca_connect(void);
