52c6170b3a6f6299af277ef7f1c187e27c97b46e
qinglin.hu <qinglin.hu@unisoc.com>
2021-09-02 11:28:24 +0000

    Bug #1700168 usb: musb: qh is used after it is release.
    [root cause  ] Code out of sync
    [changes     ] kernel/drivers/usb/musb/musb_host.c
    [side effects] no side effect.
    [self test   ]:USB host features
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums512_h10
    [test case]:USB host function.
    [reviewers   ] ben.li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    The code on Android 12 is not synchronized and needs to be synchronized.
    
    Change-Id: I49dc8e1d13ff6e7854b26c9944847a1cf5e67f04
    Signed-off-by: Qinglin Hu <qinglin.hu@unisoc.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -2938,7 +2938,8 @@ musb_h_disable(struct usb_hcd *hcd, struct usb_host_endpoint *hep)
 	qh->is_ready = 0;
 	if (musb_ep_get_qh(qh->hw_ep, is_in) == qh) {
 		urb = next_urb(qh);
-
+		if (!urb)
+			goto exit;
 		/* make software (then hardware) stop ASAP */
 		if (!urb->unlinked)
 			urb->status = -ESHUTDOWN;
@@ -2962,6 +2963,8 @@ musb_h_disable(struct usb_hcd *hcd, struct usb_host_endpoint *hep)
 		while (!list_empty(&hep->urb_list))
 			musb_giveback(musb, next_urb(qh), -ESHUTDOWN);
 
+		if (musb_ep_get_qh(qh->hw_ep, !is_in) == qh)
+			musb_ep_set_qh(qh->hw_ep, !is_in, NULL);
 		hep->hcpriv = NULL;
 		list_del(&qh->ring);
 		kfree(qh);
