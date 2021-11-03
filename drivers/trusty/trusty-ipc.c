a95cfc8b3edde1d9312d2069c5cb9b382c6074a7
qiwen.dong <qiwen.dong@unisoc.com>
2021-11-03 14:29:35 +0800

    Bug #1748454 trusty: IPC set timeout to wait for memory allocation
    [root cause  ] No memory is allocated during big data transfer
    [changes     ] trusty driver
    [side effects]none
    [self test   ]:build pass/boot pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:all
    [test case]:widevineL1
    [reviewers   ] shijiu.ren
    [change_type ] feature
    [tag_product ] common
    
    add timeout value when no memory is allocated
    
    Change-Id: I46db1eeed304cf2c873ad2650c217f2d9b18e5b3
    Signed-off-by: qiwen.dong <qiwen.dong@unisoc.com>

--- drivers/trusty/trusty-ipc.c
+++ drivers/trusty/trusty-ipc.c
@@ -1221,9 +1221,6 @@ static ssize_t tipc_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 
 	while (copyed_len < total_len) {
 
-		if (copyed_len != 0)
-			timeout = 0;
-
 		txbuf = tipc_chan_get_txbuf_timeout(dn->chan, timeout);
 		if (IS_ERR(txbuf)) {
 			if (copyed_len == 0) {
