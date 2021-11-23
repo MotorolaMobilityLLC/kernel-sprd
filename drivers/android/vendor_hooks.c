BayLibre/android_kernel_common

ANDROID: logbuf: vendor hook for logbuf dump on oops
Initial kernel bootup logs get overwritten after running
for a long time, and there can be debugging scenario where
we need initial ~100s bootup logs for debugging.
'android_vh_initial_logbuf' vendor hook is helping in
achieving this purpose.

Bug: 185182649
Change-Id: I706824aeb566c09ecaf4b5900973d6cee8a2f35b
Signed-off-by: Mukesh Ojha <mojha@codeaurora.org>

drivers/android/vendor_hooks.c
@@ -50,6 +50,7 @@
 #include <trace/hooks/typec.h>
 #include <trace/hooks/gpiolib.h>
 #include <trace/hooks/signal.h>
+#include <trace/hooks/logbuf.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -238,3 +239,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_preset);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_post_init_entity_util_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpm_get_timer);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_typec_tcpm_adj_current_limit);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_initial_logbuf);

MotorolaMobilityLLC/kernel-sprd

Bug: 199236943
(cherry picked from commit 34fde61f723b3387e12116390fa38540b18ced2e)
Signed-off-by: Yogesh Lal <ylal@codeaurora.org>

drivers/android/vendor_hooks.c
@@ -43,6 +43,7 @@
 #include <trace/hooks/module.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/syscall_check.h>
+#include <trace/hooks/logbuf.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -199,3 +200,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_bpf_syscall);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_cpus_allowed_ptr_locked);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_rto_next_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_em_dev_register_pd);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_logbuf);