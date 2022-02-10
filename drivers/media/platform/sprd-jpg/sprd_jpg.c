4868e0af74a2f197a7db932c31a020738322ae57
xu.xiang1 <xu.xiang1@unisoc.com>
2022-02-10 14:47:16 +0800

    Bug #1615623 media: platform: sprd-jpg: add jpg enable/disable in jpg_isr handle for iommu_error
    [root cause  ] add jpg enable/disable and clr the interrupt of iommu
    [changes     ] sprd_jpg kernel
    [side effects] none
    [self test   ]: none
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:common
    [test case]:monkey test
    [reviewers   ] leon li
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I724fcbfc1dae6369dc376b0cbb17036f4306cae4
    Signed-off-by: xu.xiang1 <xu.xiang1@unisoc.com>

--- drivers/media/platform/sprd-jpg/sprd_jpg.c
+++ drivers/media/platform/sprd-jpg/sprd_jpg.c
@@ -40,6 +40,8 @@
 #define pr_fmt(fmt) "sprd-jpg: " fmt
 
 static struct jpg_dev_t hw_dev;
+static struct wakeup_source jpg_wakelock;
+
 static char *jpg_clk_src[] = {
 	"clk_src_76m8",
 	"clk_src_96m",
@@ -62,10 +64,18 @@ static struct jpg_qos_cfg qos_cfg;
 static struct register_gpr regs[ARRAY_SIZE(syscon_name)];
 
 static irqreturn_t jpg_isr(int irq, void *data);
+static irqreturn_t jpg_isr_thread(int irq, void *data);
 
 static irqreturn_t jpg_isr(int irq, void *data)
 {
 	int int_status;
+
+	struct jpg_fh *jpg_fp = hw_dev.jpg_fp;
+
+	if (jpg_fp->is_clock_enabled == 0) {
+		__pm_stay_awake(&jpg_wakelock);
+		return IRQ_WAKE_THREAD;
+	}
 
 	int_status =
 		readl_relaxed((void __iomem *)(hw_dev.sprd_jpg_virt +
@@ -107,6 +117,50 @@ static irqreturn_t jpg_isr(int irq, void *data)
 
 	}
 
+	if (hw_dev.version >= SHARKL5PRO) {
+		int_status =
+			readl_relaxed((void __iomem *)(hw_dev.sprd_jpg_virt +
+				IOMMU_INT_RAW_OFFSET));
+		if (int_status) {
+			writel_relaxed((int_status),
+				(void __iomem *)(hw_dev.sprd_jpg_virt +
+				IOMMU_INT_CLR_OFFSET));
+		}
+		dev_info(hw_dev.jpg_dev, "%s iommu status 0x%x\n", __func__, int_status);
+	}
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t jpg_isr_thread(int irq, void *data)
+{
+	int ret;
+	int int_status;
+
+	ret = jpg_clk_enable(&hw_dev);
+	if (ret == 0) {
+		int_status =
+			readl_relaxed((void __iomem *)(hw_dev.sprd_jpg_virt +
+				GLB_INT_RAW_OFFSET));
+		if (int_status) {
+			writel_relaxed((int_status),
+				(void __iomem *)(hw_dev.sprd_jpg_virt +
+				GLB_INT_CLR_OFFSET));
+		}
+		if (hw_dev.version >= SHARKL5PRO) {
+			int_status =
+				readl_relaxed((void __iomem *)(hw_dev.sprd_jpg_virt +
+					IOMMU_INT_RAW_OFFSET));
+			dev_info(hw_dev.jpg_dev, "jpg_iommu_int raw 0x%x\n", int_status);
+			if (int_status) {
+				writel_relaxed((int_status),
+					(void __iomem *)(hw_dev.sprd_jpg_virt +
+					IOMMU_INT_CLR_OFFSET));
+			}
+		}
+		jpg_clk_disable(&hw_dev);
+	}
+	__pm_relax(&jpg_wakelock);
 	return IRQ_HANDLED;
 }
 
@@ -593,8 +647,8 @@ static int jpg_probe(struct platform_device *pdev)
 
 	/* register isr */
 	ret =
-	    devm_request_irq(&pdev->dev, hw_dev.irq, jpg_isr, 0, "JPG",
-			     &hw_dev);
+		devm_request_threaded_irq(&pdev->dev, hw_dev.irq, jpg_isr,
+			jpg_isr_thread, 0, "JPG", &hw_dev);
 	if (ret) {
 		dev_err(dev, "jpg: failed to request irq!\n");
 		ret = -EINVAL;
