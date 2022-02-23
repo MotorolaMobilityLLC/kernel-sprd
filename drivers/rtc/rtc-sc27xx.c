9dcbf82208d2508c80fc71e81c90e7e5041be992
Hongbin Chen <hongbin.chen@unisoc.com>
2022-02-23 09:24:41 +0800

    Bug #1826574 rtc: sc27xx: A12 qogirn6pro rtc bringup
    [root cause  ] add N6Pro A12 RTC ko
    [changes     ] add compatible
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:sharkl6pro
    [test case]:NA
    [reviewers   ] ling_ling.xu
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    modify of_device_id sprd_rtc_of_match struct to add A12 N6pro rtc ko
    
    Signed-off-by: Hongbin Chen <hongbin.chen@unisoc.com>
    Change-Id: I15f290e4ed67fd808fdc2fa447bd7205efb33796

--- drivers/rtc/rtc-sc27xx.c
+++ drivers/rtc/rtc-sc27xx.c
@@ -695,6 +695,7 @@ static int sprd_rtc_remove(struct platform_device *pdev)
 
 static const struct of_device_id sprd_rtc_of_match[] = {
 	{ .compatible = "sprd,sc2731-rtc", },
+	{ .compatible = "sprd,ump96xx-rtc", },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, sprd_rtc_of_match);
