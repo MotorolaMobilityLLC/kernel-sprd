0610ed3fd30a1ad9f3c15e5f818dd03d01335fbb
xiaosong.ma <xiaosong.ma@unisoc.com>
2022-02-28 14:13:38 +0800

    Bug #1829332 block: zram: Revert allocate CMA pages in zram
    [root cause  ] Revert allocate CMA pages in zram
    [changes     ] Revert allocate CMA pages in zram
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:all
    [test case]:yes
    [reviewers   ] yuming.han
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I5f7619144814389c3512e877762daa5bb3aa4937
    Signed-off-by: xiaosong.ma <xiaosong.ma@unisoc.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1509,19 +1509,14 @@ compress_again:
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
-#ifdef CONFIG_ARM
 				__GFP_CMA |
-#endif
 				__GFP_MOVABLE);
 	if (!entry) {
 		zcomp_stream_put(zram->comp);
 		atomic64_inc(&zram->stats.writestall);
 		entry = zram_entry_alloc(zram, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
-#ifdef CONFIG_ARM
-				__GFP_CMA |
-#endif
-				__GFP_MOVABLE);
+				__GFP_CMA | __GFP_MOVABLE);
 		if (entry)
 			goto compress_again;
 		return -ENOMEM;
