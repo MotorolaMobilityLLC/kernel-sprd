e992c7874baa049d859e8e6449235ec0c5c0d25e
Di Shen <di.shen@unisoc.com>
2022-02-28 15:40:13 +0800

    Bug #1830893 thermal: add null check for data->ops
    [root cause  ] missing null check
    [changes     ] add null check
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:NA
    [board]: ums9230(T606)
    [test case]:power on/off
    [reviewers   ] Vincent Wang; Jeson Gao
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Null pointer dereference in of_thermal_set_trip_temp used by trip_point_set_
    temp_store due to missing null check. So add null check.
    
    Change-Id: Ifac1dddfc09b5e87937e518c7262ffea6b7f9d22
    Signed-off-by: Di Shen <di.shen@unisoc.com>
    (cherry picked from commit aa3f75b87bc16941a55144646a406a13628f71e3)

--- drivers/thermal/of-thermal.c
+++ drivers/thermal/of-thermal.c
@@ -336,7 +336,7 @@ static int of_thermal_set_trip_temp(struct thermal_zone_device *tz, int trip,
 	if (trip >= data->ntrips || trip < 0)
 		return -EDOM;
 
-	if (data->ops->set_trip_temp) {
+	if (data->ops && data->ops->set_trip_temp) {
 		int ret;
 
 		ret = data->ops->set_trip_temp(data->sensor_data, trip, temp);
