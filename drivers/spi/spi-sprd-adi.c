11efa441387fd3f5603184a2ac557f6a856f49fb
run.mao <run.mao@unisoc.com>
2022-04-05 15:22:02 +0800

    Bug #1839261 spi: sprd: add dmverity reboot method to register reboot
    [root cause  ] save dm-verity flag
    [changes     ] save dm-verity flag
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all
    [test case]:reboot mode test
    [reviewers   ] ling_ling.xu
    
    Signed-off-by: run.mao <run.mao@unisoc.com>
    [change_type ] feature
    [tag_product ] specific
    
    Change-Id: I47c279004bdffc7dfbcd54ed75907a0e0fa1a313

--- drivers/spi/spi-sprd-adi.c
+++ drivers/spi/spi-sprd-adi.c
@@ -121,6 +121,7 @@
 
 /* Definition of PMIC reset status register */
 #define HWRST_STATUS_SECURITY		0x02
+#define HWRST_STATUS_SECBOOT		0x03
 #define HWRST_STATUS_RECOVERY		0x20
 #define HWRST_STATUS_NORMAL		0x40
 #define HWRST_STATUS_ALARM		0x50
@@ -444,6 +445,8 @@ static int sprd_adi_restart_handler(struct notifier_block *this,
 		reboot_mode = HWRST_STATUS_SPRDISK;
 	else if (!strncmp(cmd, "tospanic", 8))
 		reboot_mode = HWRST_STATUS_SECURITY;
+	else if (!strncmp(cmd, "dm-verity", 9))
+		reboot_mode = HWRST_STATUS_SECBOOT;
 	else if (!strncmp(cmd, "factorytest", 11))
 		reboot_mode = HWRST_STATUS_FACTORYTEST;
 	else
