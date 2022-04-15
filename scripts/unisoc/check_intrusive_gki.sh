ce5ada7dc5f4ef23085635eb4e8d96a4b805e434
Jian.Gong <Jian.Gong@unisoc.com>
2022-04-15 19:20:27 +0800

    Bug #1671255 scripts: unisoc: add intrusive check script for kernel5.15
    [root cause  ] add intrusive check script for kernel5.15
    [changes     ] check_intrusive_gki.sh
    [side effects] no
    [self test   ]: no
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sprd
    [test case]: na
    [reviewers   ] Orson.Zhai
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ib0f5bce25aec2fd638517def0c012777925debc3
    Signed-off-by: Jian Gong <Jian.Gong@unisoc.com>

---
+++ scripts/unisoc/check_intrusive_gki.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+
+compile_for_clang()
+{
+    local global=$1
+
+    make LLVM=1 LLVM=1 DEPMOD=depmod DTC=${DTC_BIN} O=$out gki_defconfig
+    ${ROOT_DIR}/${KERNEL_DIR}/scripts/config --file ${out}/.config \
+       -e LTO_CLANG \
+       -d LTO_NONE \
+       -e LTO_CLANG_THIN \
+       -d LTO_CLANG_FULL \
+       -e THINLTO \
+       -d CONFIG_LOCALVERSION_AUTO
+
+    make LLVM=1 LLVM=1 DEPMOD=depmod DTC=${DTC_BIN} O=$out $global -j64
+}
+
+copy_specify_dir()
+{
+    local flag=$1
+    local dir=$2
+
+    cd $out
+    for ((i=0;i<$flag;i++))
+    do
+        if [ -f ${PreArrarydir[i]} ]; then
+             cp --parents ${PreArrarydir[i]} $dir
+        else
+            unset PreArrarydir[i]
+        fi
+    done
+}
+
+export MAIN_SCRIPT_DIR=$(readlink -f $(dirname $0)/../..)
+export ROOT_DIR=$(dirname $MAIN_SCRIPT_DIR)
+export KERNEL_DIR=${MAIN_SCRIPT_DIR##*/}
+
+echo "= ROOT_DIR: $ROOT_DIR"
+echo "= KERNEL_DIR: $KERNEL_DIR"
+
+CLANG_VERSION=`cat ${ROOT_DIR}/${KERNEL_DIR}/build.config.constants | awk -F "=" '{print $2}'`
+DTC_BIN=${ROOT_DIR}/build/kernel/build-tools/path/linux-x86/dtc
+CLANG_BIN=${ROOT_DIR}/prebuilts/clang/host/linux-x86/clang-${CLANG_VERSION}/bin
+merge_check=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty=short|grep "^Merge:")
+set -e
+patch_file=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty="format:" --name-only --diff-filter=AMCR)
+patch_commit=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty="format:%H")
+aosp_commit=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git merge-base korg/sprdlinux5.15 korg/android13-5.15)
+set +e
+
+# set the common sysroot
+sysroot_flags+="--sysroot=${ROOT_DIR}/build/kernel/build-tools/sysroot "
+
+# add openssl (via boringssl) and other prebuilts into the lookup path
+cflags+="-I${ROOT_DIR}/prebuilts/kernel-build-tools/linux-x86/include "
+
+# add openssl and further prebuilt libraries into the lookup path
+ldflags+="-Wl,-rpath,${ROOT_DIR}/prebuilts/kernel-build-tools/linux-x86/lib64 "
+ldflags+="-L ${ROOT_DIR}/prebuilts/kernel-build-tools/linux-x86/lib64 "
+
+# Have host compiler use LLD and compiler-rt.
+LLD_COMPILER_RT="-fuse-ld=lld --rtlib=compiler-rt"
+ldflags+=${LLD_COMPILER_RT}
+
+export PATH=${CLANG_BIN}:$PATH
+export PATH=${ROOT_DIR}/prebuilts/kernel-build-tools/linux-x86/bin:$PATH
+export HOSTCFLAGS="$sysroot_flags $cflags"
+export HOSTLDFLAGS="$sysroot_flags $ldflags"
+temp_def="${MAIN_SCRIPT_DIR}/./tmp_intrusive_check/"
+out="$temp_def/out"
+patch_prefile_dir="$temp_def/tmp"
+aosp_prefile_dir="$temp_def/aosp"
+return_val=0
+check_cfile_flag=0
+check_hfile_flag=0
+check_ifile_flag=0
+
+declare -a Arrarydir
+declare -a PreArrarydir
+declare -a PreArraryfile
+
+if [ -n "${merge_check}" ]; then
+    echo "Skip the intrusive check(merge commit) ." >&1
+    exit 0
+else
+set -e
+    for file in $patch_file; do
+        if [[ "${file##*.}"x == "c"x ]]; then
+            cfile=${file/%".c"/".o"}
+            Arrarydir[$check_cfile_flag]=$cfile
+            let check_cfile_flag+=1
+        elif [[ "${file##*.}"x == "h"x  ]]; then
+            let check_hfile_flag+=1
+            break
+        fi
+    done
+
+fi
+
+if [ -d $temp_def ]; then
+    rm -rf $temp_def
+fi
+
+if [ ! -d $patch_prefile_dir ]; then
+    mkdir -p $patch_prefile_dir
+fi
+
+if [ ! -d $aosp_prefile_dir ]; then
+    mkdir -p $aosp_prefile_dir
+fi
+
+# checkout to aops commit
+echo "===============================================" >&1
+echo "= check for aosp commit:$aosp_commit" >&1
+git checkout $aosp_commit
+compile_for_clang vmlinux
+if [ $check_hfile_flag -ne 0 ]; then
+    cd $out
+    totalfile=`find . \( -path "./scripts" -o -path "./tools" \) -prune -o -type f  -name "*.o" \
+            |grep -v "x86" | grep -v '^\./\.' | grep -v "vmlinux" | grep -v "scripts" | grep -v "tools" \
+                |grep -v "usr" | sed 's/\.\///g' | sed 's/\.o/\.c/g'`
+    cd $MAIN_SCRIPT_DIR
+    for file in $totalfile
+    do
+        if [ -f $file ]; then
+            predir=${file/%".c"/".i"}
+            PreArrarydir[$check_ifile_flag]=$predir
+            let check_ifile_flag+=1
+            global+="$predir "
+        fi
+    done
+    make -i LLVM=1 LLVM=1 DEPMOD=depmod DTC=${DTC_BIN} O=$out $global -j64
+    copy_specify_dir $check_ifile_flag $aosp_prefile_dir
+
+elif [ $check_cfile_flag -ne 0 ]; then
+    for ((i=0;i<check_cfile_flag;i++))
+    do
+        if [ ! -f $out/${Arrarydir[i]} ]; then
+            unset Arrarydir[i]
+        else
+            filedir=${Arrarydir[i]}
+            predir=${filedir/%".o"/".i"}
+            PreArrarydir[$check_ifile_flag]=$predir
+            global+="$predir "
+            let check_ifile_flag+=1
+        fi
+    done
+    make -i LLVM=1 LLVM=1 DEPMOD=depmod DTC=${DTC_BIN} O=$out $global -j64
+    copy_specify_dir $check_ifile_flag $aosp_prefile_dir
+fi
+
+# Checkout to patch commit
+echo "========================================================" >&1
+echo "= check for current patch commit:$patch_commit" >&1
+cd $MAIN_SCRIPT_DIR
+git checkout $patch_commit
+if [ ${#PreArrarydir[@]} -ne 0 ]; then
+    compile_for_clang vmlinux
+    for predir in ${PreArrarydir[*]}
+    do
+        global_1+="$predir "
+    done
+    make LLVM=1 LLVM=1 DEPMOD=depmod DTC=${DTC_BIN} O=$out $global_1 -j64
+    cd $out
+    for predir in ${PreArrarydir[*]}
+    do
+        if [ -n "$predir"  ] && [ -f $predir ]; then
+            cp --parents $predir $patch_prefile_dir
+        fi
+    done
+else
+    echo "========================================================" >&1
+    echo "= Modify file list:" >&1
+    for file in $patch_file
+    do
+        echo "   $file" >&1
+    done
+    echo "The above file is not required for gki intrusive check." >&1
+    echo "The gki intrusive check---------------------------pass." >&1
+    rm -rf ${temp_def}
+    exit 0
+fi
+
+set +e
+
+# Compare file for intrusive check
+for ((i=0;i<check_ifile_flag;i++))
+do
+    if [ -n "${PreArrarydir[i]}" ]; then
+        diff $patch_prefile_dir/${PreArrarydir[i]} $aosp_prefile_dir/${PreArrarydir[i]} > /dev/null
+        return_val=$?
+        if [ $return_val -eq 0 ] || [[ ${PreArrarydir[i]} == "init/version.i"  ]]; then
+            unset PreArrarydir[i]
+        fi
+    fi
+done
+
+echo "========================================================" >&1
+echo "= The result of gki intrusive check:" >&1
+if [ ${#PreArrarydir[@]} -ne 0  ]; then
+    echo "the following file has been intrusively modified:" >&2
+    for file in ${PreArrarydir[*]}
+    do
+        cfile=${filedir/%".i"/".c"}
+        echo "   $file"
+    done
+    echo "Error: gki intrusive check---------------------------fail." >&1
+else
+    echo "The gki intrusive check---------------------------pass." >&1
+fi
+
+rm -rf ${temp_def}
+exit $return_val
