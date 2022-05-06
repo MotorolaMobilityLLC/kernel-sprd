486d94f9d4de05ebc8e5e077155508f1a644f3ce
Yuqiang Li <yuqiang.li@unisoc.com>
2022-05-06 10:11:15 +0800

    Bug #1853027 unisoc_platform: add camera power domain
    [root cause  ] add power domain
    [changes     ] unisoc_platform mm domain
    [side effects]no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:na
    [confirm dependent commit]:na
    [board]:ums512
    [test case]:camera func
    [reviewers   ] yangyang.liu
    
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I1927a681283c6f0c4aa9cd3cb409c9d6315ebaa9
    Signed-off-by: Yuqiang Li <yuqiang.li@unisoc.com>

---
+++ drivers/unisoc_platform/domain/sprd_camsys_domain.h
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Spreatrum camera pd driver header
+ * Copyright (C) 2021 Spreadtrum, Inc.
+ * Author: Hongjian Wang <hongjian.wang@spreadtrum.com>
+ */
+
+
+#ifndef SPRD_CAMSYS_DOMAIN_H
+#define SPRD_CAMSYS_DOMAIN_H
+
+#include <linux/pm_domain.h>
+
+enum {
+       _E_PW_OFF = 0,
+       _E_PW_ON  = 1,
+};
+
+enum sprd_campw_id {
+	CAM_PW_SHARKL5PRO,
+	CAM_PW_MAX
+};
+
+struct register_gpr {
+	struct regmap *gpr;
+	uint32_t reg;
+	uint32_t mask;
+};
+
+struct camsys_power_info;
+
+struct camsys_power_ops {
+	long (*sprd_campw_init)(struct platform_device *pdev, struct camsys_power_info *pw_info);
+	int (*sprd_cam_domain_eb)(struct camsys_power_info *domain);
+	int (*sprd_cam_domain_disable)(struct camsys_power_info *domain);
+	int (*sprd_cam_pw_on)(struct camsys_power_info *domain);
+	int (*sprd_cam_pw_off)(struct camsys_power_info *domain);
+};
+
+struct camsys_power_info {
+	atomic_t inited;
+	struct mutex mlock;
+	struct generic_pm_domain pd;
+	struct camsys_power_ops *ops;
+
+	union {
+		struct {
+			uint8_t mm_qos_ar;
+			uint8_t mm_qos_aw;
+
+			struct clk *cam_mm_eb;
+			struct clk *cam_mm_ahb_eb;
+
+			struct clk *cam_ahb_clk;
+			struct clk *cam_ahb_clk_parent;
+			struct clk *cam_ahb_clk_default;
+
+			struct clk *cam_mtx_clk;
+			struct clk *cam_mtx_clk_parent;
+			struct clk *cam_mtx_clk_default;
+
+			struct clk *isppll_clk;
+
+			struct register_gpr regs[6];
+		} l5pro;
+	} u;
+};
+
+extern  struct camsys_power_ops camsys_power_ops_l5pro;
+
+int sprd_mm_pw_notify_register(struct notifier_block *nb);
+int sprd_mm_pw_notify_unregister(struct notifier_block *nb);
+#endif
