633e7ed47a6358f67f87672b5c68bcde1ec7ef8d
Di Shen <di.shen@unisoc.com>
2022-05-12 23:39:36 +0800

    Bug #1867836 unisoc_platform: thermal: add unisoc thermal control
    [root cause  ] add unisoc thermal control
    [changes     ] add unisoc thermal control
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:na
    [board]:ums512
    [test case]:ipa test
    [reviewers   ] Jeson Gao; Ke Wang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    1. add sysctl control table for thermal, the sys interface as follow:
       /proc/sys/unisoc_thermal/**
    
    2. add vendor hook function for thermal;
    
    3. add user power range control using vh and user can set it in the sys:
       /proc/sys/unisoc_thermel/user_power_range
    
    4. add soc thermal enable ctl using vh, and user can enable/disable it as:
       /proc/sys/unisoc_thermal/thm_enable
    
    5. optimize the target and request freq from thermal to improve performance.
    
    Change-Id: I80045f73675e14e4738f18ef6689f49a85141002
    Signed-off-by: Di Shen <di.shen@unisoc.com>

---
+++ drivers/unisoc_platform/thermal/sprd_thermal_ctl.c
@@ -0,0 +1,236 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * SPRD thermal control
+ *
+ * Copyright (C) 2022 Unisoc corporation. http://www.unisoc.com
+ */
+
+#define pr_fmt(fmt) "sprd_thm_ctl: " fmt
+
+#include <linux/cpu_cooling.h>
+#include <linux/cpufreq.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/sysctl.h>
+#include <trace/hooks/thermal.h>
+
+#define MAX_CLUSTER_NUM	3
+
+static unsigned int sysctl_thm_enable = 1;
+static unsigned int sysctl_user_power_range;
+
+struct ctl_table thermal_table[] = {
+	{
+		.procname	= "thm_enable",
+		.data		= &sysctl_thm_enable,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler   = proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "user_power_range",
+		.data		= &sysctl_user_power_range,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler   = proc_dointvec,
+	},
+	{ },
+};
+
+struct ctl_table thermal_base_table[] = {
+	{
+		.procname	= "unisoc_thermal",
+		.mode		= 0555,
+		.child		= thermal_table,
+	},
+	{ },
+};
+
+struct sprd_thermal_ctl {
+	struct cpufreq_policy	*policy;
+	int			cluster_id;
+	struct list_head	node;
+};
+
+static LIST_HEAD(thermal_policy_list);
+
+static struct ctl_table_header *hdr;
+
+static void unisoc_thermal_register(void *data, struct cpufreq_policy *policy)
+{
+	struct sprd_thermal_ctl *thm_ctl;
+
+	list_for_each_entry(thm_ctl, &thermal_policy_list, node)
+		if (thm_ctl->policy == policy)
+			return;
+
+	list_for_each_entry(thm_ctl, &thermal_policy_list, node)
+		if (thm_ctl->policy == NULL) {
+			thm_ctl->policy = policy;
+			break;
+		}
+}
+
+static void unisoc_thermal_unregister(void *data, struct cpufreq_policy *policy)
+{
+	struct sprd_thermal_ctl *thm_ctl;
+
+	list_for_each_entry(thm_ctl, &thermal_policy_list, node) {
+		if (thm_ctl->policy == policy) {
+			thm_ctl->policy = NULL;
+			break;
+		}
+	}
+}
+
+/* thermal power throttle control */
+static void unisoc_enable_thermal_power_throttle(void *data, bool *enable, bool *override)
+{
+	if (!sysctl_thm_enable)
+		*enable = false;
+
+	if (sysctl_user_power_range)
+		*override = true;
+}
+
+/* modify IPA power_range by user_power_range */
+static void unisoc_thermal_power_cap(void *data, unsigned int *power_range)
+{
+	if (sysctl_user_power_range && sysctl_user_power_range < *power_range)
+		*power_range = sysctl_user_power_range;
+}
+
+/* modify thermal target frequency */
+static inline
+struct cpufreq_policy *find_cpufreq_policy(struct cpufreq_policy *curr)
+{
+	struct sprd_thermal_ctl *thm_ctl;
+
+	list_for_each_entry(thm_ctl, &thermal_policy_list, node)
+		if ((curr->cdev->id + 1) == thm_ctl->policy->cdev->id)
+			return thm_ctl->policy;
+
+	return NULL;
+}
+
+static void unisoc_modify_thermal_target_freq(void *data,
+		struct cpufreq_policy *policy, unsigned int *target_freq)
+{
+	unsigned int curr_max_freq;
+	struct cpufreq_policy *cpufreq_policy_find;
+
+	curr_max_freq = cpufreq_quick_get_max(policy->cpu);
+
+	if (curr_max_freq <= *target_freq) {
+		*target_freq = curr_max_freq;
+		return;
+	}
+
+	cpufreq_policy_find = find_cpufreq_policy(policy);
+	if (cpufreq_policy_find &&
+		cpufreq_policy_find->max != cpufreq_policy_find->min)
+			*target_freq = curr_max_freq;
+
+}
+
+/* modify request frequency */
+static void unisoc_modify_thermal_request_freq(void *data,
+		struct cpufreq_policy *policy, unsigned long *request_freq)
+{
+	*request_freq = cpufreq_quick_get_max(policy->cpu);
+}
+
+/* thermal temperature debug */
+static int thermal_temp_debug(struct thermal_zone_device *tz)
+{
+	int crit_temp = 0, warn_temp;
+	int ret = -EPERM;
+	int count;
+	enum thermal_trip_type type;
+
+	for (count = 0; count < tz->trips; count++) {
+		ret = tz->ops->get_trip_type(tz, count, &type);
+		if (!ret && type == THERMAL_TRIP_CRITICAL) {
+			ret = tz->ops->get_trip_temp(tz, count,
+				&crit_temp);
+			warn_temp = crit_temp - 5000;
+			if (!ret && tz->temperature > warn_temp)
+				pr_alert_ratelimited("tz id=%d type=%s temperature reached %d\n",
+					tz->id, tz->type, tz->temperature);
+			break;
+		}
+	}
+
+	return ret;
+}
+
+static void unisoc_get_thermal_zone_device(void *data, struct thermal_zone_device *tz)
+{
+	thermal_temp_debug(tz);
+}
+
+
+static int sprd_thermal_ctl_init(void)
+{
+	int i;
+	struct sprd_thermal_ctl *thm_ctl, *tmp_thm_ctl;
+
+	for (i = 0; i < MAX_CLUSTER_NUM; i++) {
+		thm_ctl = kzalloc(sizeof(*thm_ctl), GFP_KERNEL);
+
+		if (thm_ctl) {
+			thm_ctl->policy = NULL;
+			list_add(&thm_ctl->node, &thermal_policy_list);
+		} else {
+			goto fail;
+		}
+	}
+
+	hdr = register_sysctl_table(thermal_base_table);
+	register_trace_android_vh_thermal_register(unisoc_thermal_register, NULL);
+	register_trace_android_vh_thermal_unregister(unisoc_thermal_unregister, NULL);
+	register_trace_android_vh_enable_thermal_power_throttle(unisoc_enable_thermal_power_throttle, NULL);
+	register_trace_android_vh_thermal_power_cap(unisoc_thermal_power_cap, NULL);
+	register_trace_android_vh_modify_thermal_target_freq(unisoc_modify_thermal_target_freq, NULL);
+	register_trace_android_vh_modify_thermal_request_freq(unisoc_modify_thermal_request_freq, NULL);
+	register_trace_android_vh_get_thermal_zone_device(unisoc_get_thermal_zone_device, NULL);
+
+	return 0;
+
+fail:
+	list_for_each_entry_safe(thm_ctl, tmp_thm_ctl, &thermal_policy_list, node) {
+		list_del(&thm_ctl->node);
+		kfree(thm_ctl);
+	}
+
+	return -ENOMEM;
+}
+
+static void sprd_thermal_ctl_exit(void)
+{
+	struct sprd_thermal_ctl *thm_ctl, *tmp_thm_ctl;
+
+	unregister_trace_android_vh_thermal_register(unisoc_thermal_register, NULL);
+	unregister_trace_android_vh_thermal_unregister(unisoc_thermal_unregister, NULL);
+
+	list_for_each_entry_safe(thm_ctl, tmp_thm_ctl, &thermal_policy_list, node) {
+		list_del(&thm_ctl->node);
+		kfree(thm_ctl);
+	}
+
+	unregister_trace_android_vh_enable_thermal_power_throttle(unisoc_enable_thermal_power_throttle, NULL);
+	unregister_trace_android_vh_thermal_power_cap(unisoc_thermal_power_cap, NULL);
+	unregister_trace_android_vh_modify_thermal_target_freq(unisoc_modify_thermal_target_freq, NULL);
+	unregister_trace_android_vh_modify_thermal_request_freq(unisoc_modify_thermal_request_freq, NULL);
+	unregister_trace_android_vh_get_thermal_zone_device(unisoc_get_thermal_zone_device, NULL);
+	unregister_sysctl_table(hdr);
+}
+
+module_init(sprd_thermal_ctl_init);
+module_exit(sprd_thermal_ctl_exit);
+
+MODULE_DESCRIPTION("for sprd thermal control");
+MODULE_LICENSE("GPL");
