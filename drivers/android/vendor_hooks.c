maltesermailo/android_kernel_pantah

ANDROID: vendor_hooks: Add hook in wakeup functionality
Need this verndor hook to support wakeup functionality
for sdio cards.

Bug: 231668342
Change-Id: I1d1a34d6d02f4558edf56eccdd09df57d6232c05
Signed-off-by: Sarthak Garg <sartgarg@codeaurora.org>
Signed-off-by: kamasali <quic_kamasali@quicinc.com>

drivers/android/vendor_hooks.c
@@ -314,3 +314,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_mmc_sdio_pm_flag_set);

drivers/mmc/core/sdio.c
@@ -15,6 +15,8 @@
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/sdio_ids.h>
 
+#include <trace/hooks/mmc.h>
+
 #include "core.h"
 #include "card.h"
 #include "host.h"
@@ -1094,6 +1096,8 @@ out:
 	mmc_release_host(host);
 
 	host->pm_flags &= ~MMC_PM_KEEP_POWER;
+	trace_android_rvh_mmc_sdio_pm_flag_set(host);
+
 	return err;
 }
 

include/trace/hooks/mmc.h
@@ -7,6 +7,7 @@
 #define _TRACE_HOOK_MMC_H
 #include <trace/hooks/vendor_hooks.h>
 struct blk_mq_queue_data;
+struct mmc_host;
 
 /*
  * Following tracepoints are not exported in tracefs and provide a
@@ -15,6 +16,10 @@ struct blk_mq_queue_data;
 DECLARE_HOOK(android_vh_mmc_check_status,
 	TP_PROTO(const struct blk_mq_queue_data *bd, int *ret),
 	TP_ARGS(bd, ret));
+
+DECLARE_HOOK(android_rvh_mmc_sdio_pm_flag_set,
+	TP_PROTO(struct mmc_host *host),
+	TP_ARGS(host));
 
 #endif /* _TRACE_HOOK_MMC_H */
 /* This part must be outside protection */

MotorolaMobilityLLC/kernel-sprd

Signed-off-by: Kamasali Satyanarayan <quic_kamasali@quicinc.com>

drivers/android/vendor_hooks.c
@@ -316,3 +316,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_blk_rq_ctx_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmap_region);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_try_to_unmap_one);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_node_memcgs);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_sdio_pm_flag_set);

drivers/mmc/core/sdio.c
@@ -15,6 +15,8 @@
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/sdio_ids.h>
 
+#include <trace/hooks/mmc.h>
+
 #include "core.h"
 #include "card.h"
 #include "host.h"
@@ -1094,6 +1096,8 @@ out:
 	mmc_release_host(host);
 
 	host->pm_flags &= ~MMC_PM_KEEP_POWER;
+	trace_android_vh_mmc_sdio_pm_flag_set(host);
+
 	return err;
 }
 

include/trace/hooks/mmc.h
@@ -7,6 +7,7 @@
 #define _TRACE_HOOK_MMC_H
 #include <trace/hooks/vendor_hooks.h>
 struct blk_mq_queue_data;
+struct mmc_host;
 
 /*
  * Following tracepoints are not exported in tracefs and provide a
@@ -15,6 +16,10 @@ struct blk_mq_queue_data;
 DECLARE_HOOK(android_vh_mmc_check_status,
 	TP_PROTO(const struct blk_mq_queue_data *bd, int *ret),
 	TP_ARGS(bd, ret));
+
+DECLARE_HOOK(android_vh_mmc_sdio_pm_flag_set,
+	TP_PROTO(struct mmc_host *host),
+	TP_ARGS(host));
 
 #endif /* _TRACE_HOOK_MMC_H */
 /* This part must be outside protection */