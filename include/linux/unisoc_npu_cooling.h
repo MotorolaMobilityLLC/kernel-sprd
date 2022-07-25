f158510c75e72a0974a6ac6f8bdb8cc6e174fddf
Di Shen <di.shen@unisoc.com>
2022-07-25 20:23:20 +0800

    Bug #1940935 unisoc_platform: thermal: add npu cooling device
    [root cause  ]: add npu cooling device for N6P-515
    [changes     ]: add npu cooling device
    [side effects]: no
    [self test   ]: yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:na
    [board]:ums9620
    [test case]:sysfs check
    [reviewers]:Jeson Gao; Ke Wang
    [change_type ] feature
    [tag_product ] common
    
    commit_template_version:v1
    
    Change-Id: Ie42e79dc02efa711955b60fbb0cc2075a21edec1
    Signed-off-by: Di Shen <di.shen@unisoc.com>

---
+++ include/linux/unisoc_npu_cooling.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2020 Spreadtrum Communications Inc.
+ */
+
+#ifndef _UNISOC_NPU_COOLING_H_
+#define _UNISOC_NPU_COOLING_H_
+
+int npu_cooling_device_register(struct devfreq *npudev);
+int npu_cooling_device_unregister(void);
+
+#endif
+
