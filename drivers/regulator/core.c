52fb5b3894b726cad2e541b5263fd74b3fd21c43
xianjun.zeng <xianjun.zeng@unisoc.com>
2022-09-27 13:40:18 +0800

    Bug #2026927 regulator: Fix WCDMA calibration APC error for pike2
    [root cause  ]: Fix WCDMA calibration APC error for pike2
    [changes     ]: bsp/kernel/kernel5.4/regulator
    [side effects]: no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: 7731e
    [test case]: check the feature
    [reviewers]: ke.xu
    
    commit_template_version:v1
    
    Signed-off-by: xianjun.zeng <xianjun.zeng@unisoc.com>
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Change-Id: I47aba93d6146300ab5775485213c924a108d6362
    Signed-off-by: xianjun.zeng <xianjun.zeng@unisoc.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -5805,7 +5805,25 @@ static int __init regulator_init_complete(void)
 	 */
 	if (of_have_populated_dt())
 		has_full_constraints = true;
+#ifdef CONFIG_SPRD_PIKE2_SLEEP
+	/*
+	 * Regulators may had failed to resolve their input supplies
+	 * when were registered, either because the input supply was
+	 * not registered yet or because its parent device was not
+	 * bound yet. So attempt to resolve the input supplies for
+	 * pending regulators before trying to disable unused ones.
+	 */
+	class_for_each_device(&regulator_class, NULL, NULL,
+			      regulator_register_resolve_supply);
 
+	/* If we have a full configuration then disable any regulators
+	 * we have permission to change the status for and which are
+	 * not in use or always_on.  This is effectively the default
+	 * for DT and ACPI as they have full constraints.
+	 */
+	class_for_each_device(&regulator_class, NULL, NULL,
+			      regulator_late_cleanup);
+#else
 	/*
 	 * We punt completion for an arbitrary amount of time since
 	 * systems like distros will load many drivers from userspace
@@ -5819,6 +5837,7 @@ static int __init regulator_init_complete(void)
 	 */
 	schedule_delayed_work(&regulator_init_complete_work,
 			      msecs_to_jiffies(30000));
+#endif
 
 	return 0;
 }
