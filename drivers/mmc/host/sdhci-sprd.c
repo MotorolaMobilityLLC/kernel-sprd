7c139a8bb8caaf03fe6a33465238811a5392e904
Wenchao Chen <wenchao.chen@unisoc.com>
2022-10-09 19:32:57 +0800

    Bug #2044341 mmc: host: support hs400 tuning
    [root cause  ]: support hs400 tuning
    [changes     ]: support hs400 tuning
    [side effects]: na
    [self test   ]: build ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:all board
    [test case]:test ok
    [reviewers]:zhenxiong.lai yuelin.tang
    
    commit_template_version:v1
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I5b1512d4e285bf5dae589bafab77b34502806d86
    Signed-off-by: Wenchao Chen <wenchao.chen@unisoc.com>

--- drivers/mmc/host/sdhci-sprd.c
+++ drivers/mmc/host/sdhci-sprd.c
@@ -731,6 +731,8 @@ static int sdhci_sprd_execute_tuning(struct mmc_host *mmc, u32 opcode)
 	if (host->flags & SDHCI_HS400_TUNING) {
 		p[mmc->ios.timing] &= ~SDHCI_SPRD_CMD_DLY_MASK;
 		p[mmc->ios.timing] |= (final_phase << 8);
+		p[MMC_TIMING_MMC_HS400] &= ~SDHCI_SPRD_CMD_DLY_MASK;
+		p[MMC_TIMING_MMC_HS400] |= (final_phase << 8);
 	} else {
 		p[mmc->ios.timing] &= ~(SDHCI_SPRD_CMD_DLY_MASK |
 					SDHCI_SPRD_POSRD_DLY_MASK);
@@ -744,6 +746,10 @@ static int sdhci_sprd_execute_tuning(struct mmc_host *mmc, u32 opcode)
 	sdhci_writel(host, p[mmc->ios.timing], SDHCI_SPRD_REG_32_DLL_DLY);
 	err = 0;
 
+	if (host->flags & SDHCI_HS400_TUNING) {
+		pr_info("%s: update HS400 delay: 0x%08x\n", mmc_hostname(mmc),
+			p[MMC_TIMING_MMC_HS400]);
+	}
 out:
 	host->flags &= ~SDHCI_HS400_TUNING;
 	kfree(ranges);
