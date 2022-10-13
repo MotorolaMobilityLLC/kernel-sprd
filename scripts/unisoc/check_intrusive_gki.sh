e7e39d79162fccbbe601a381931e6e24e8526750
Cixi Geng <cixi.geng1@unisoc.com>
2022-10-14 01:28:22 +0800

    Bug #2048105 scripts: unisoc: change the aosp branch
    [root cause  ]: gki released branch changed
    [changes     ]: kernel
    [side effects]: no
    [self test   ]: ikt
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: sprd
    [confirm dependent commit]: yes
    [board]: sprd
    [test case]: build
    [reviewers]: Cixi Geng
    
    commit_template_version:v1
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ic681a6f0d4f72b14be3ca7239c02f1428ac1a382
    Signed-off-by: Cixi Geng <cixi.geng1@unisoc.com>

--- scripts/unisoc/check_intrusive_gki.sh
+++ scripts/unisoc/check_intrusive_gki.sh
@@ -98,9 +98,11 @@ DTC_BIN=${ROOT_DIR}/build/kernel/build-tools/path/linux-x86/dtc
 CLANG_BIN=${ROOT_DIR}/prebuilts/clang/host/linux-x86/clang-${CLANG_VERSION}/bin
 merge_check=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty=short|grep "^Merge:")
 set -e
+sprd_branch="korg/sprdlinux5.15"
+aosp_branch="korg/android13-5.15-2022-09"
 patch_file=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty="format:" --name-only --diff-filter=AMCR)
 patch_commit=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git log -1 --pretty="format:%H")
-aosp_commit=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git merge-base korg/sprdlinux5.15 korg/android13-5.15)
+aosp_commit=$(git --git-dir=${MAIN_SCRIPT_DIR}/.git merge-base ${sprd_branch} ${aosp_branch})
 set +e
 
 # set the common sysroot
