7a2a0e30522ed081731b18b0dbcea6c256207fd2
xiaojian11 <xiaojian11@longcheer.com>
2022-11-22 16:49:00 +0800

    [issue][SDCARD][CR]     [rootcause]: Description Failed to initialize the SD card     [solution]: Example Modify the CMD6 timeout period     [sideeffect]: None     [other]: None
    Change-Id: I62ce596738a167a40ec116ca53c5be8e4e53f3d5

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -747,8 +747,8 @@ void mmc_set_data_timeout(struct mmc_data *data, const struct mmc_card *card)
 	 * value. For the cards tested, 600ms has proven enough. If necessary,
 	 * this value can be increased if other problematic cards require this.
 	 */
-	if (mmc_card_long_read_time(card) && data->flags & MMC_DATA_READ) {
-		data->timeout_ns = 600000000;
+	if ((mmc_card_long_read_time(card)||mmc_card_sd(card)) && data->flags & MMC_DATA_READ) {
+		data->timeout_ns = 3000000000;
 		data->timeout_clks = 0;
 	}
 
