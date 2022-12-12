38d4c5a8f5d3114e7a0a7f58e28f35a07da68898
guangangwen10 <guangangwen10@longcheer.com>
2022-12-12 11:03:33 +0800

    [issue][BSP][DEV][CR] Plugout sdcard the phone sysdump [rootcause]: sdcard crash [solution]: sdcard crash [sideeffect]: None [other]: None
    Change-Id: I2f8707b32f3a4c7e6c4acc3d25fed0e7ccae34f9

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1195,7 +1195,8 @@ static int _mmc_sd_suspend(struct mmc_host *host)
 
 	mmc_claim_host(host);
 
-	if (mmc_card_suspended(host->card))
+//	if (mmc_card_suspended(host->card))
+	if (!host->card && mmc_card_suspended(host->card))
 		goto out;
 
 	if (!mmc_host_is_spi(host))
