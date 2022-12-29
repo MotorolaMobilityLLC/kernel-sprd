2887df963a366f12c8b64a1ff98cc031839716aa
Carson Wu <carson.wu@unisoc.com>
2022-12-29 14:33:08 +0800

    Bug #2109456 mfd: add sc2720 for A13 K515 pike2 bringup
    [root cause  ]: add sc2720
    [changes     ]: sc2720
    [side effects]: no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: yes
    [confirm dependent commit]: yes
    [board]: 7731e
    [test case]: poweron
    [reviewers]: ke.xu
    
    commit_template_version:v1
    
    Signed-off-by: Carson Wu <carson.wu@unisoc.com>
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    Change-Id: Ia6031cc8519c434f63ba75b13480fda2010c6f39

--- drivers/mfd/sprd-pmic-spi.c
+++ drivers/mfd/sprd-pmic-spi.c
@@ -20,6 +20,9 @@
 #define SPRD_PMIC_INT_RAW_STATUS	0x4
 #define SPRD_PMIC_INT_EN		0x8
 
+#define SPRD_SC2720_IRQ_BASE		0xc0
+#define SPRD_SC2720_IRQ_NUMS		9
+#define SPRD_SC2720_SLAVE_ID            0x0
 #define SPRD_SC2721_IRQ_BASE		0xc0
 #define SPRD_SC2721_IRQ_NUMS		11
 #define SPRD_SC2721_SLAVE_ID            0x0
@@ -43,6 +46,7 @@
 #define SPRD_SC2731_EIC_BASE		0x300
 #define SPRD_SC2730_EIC_BASE		0x280
 #define SPRD_SC2721_EIC_BASE		0x280
+#define SPRD_SC2720_EIC_BASE		0x280
 #define SPRD_UMP9620_EIC_BASE		0x280
 
 /* PMIC charger detection definition */
@@ -88,6 +92,13 @@ struct sprd_pmic_data {
  * base address and irq number, we should save irq number and irq base
  * in the device data structure.
  */
+static const struct sprd_pmic_data sc2720_data = {
+	.slave_id = SPRD_SC2720_SLAVE_ID,
+	.irq_base = SPRD_SC2720_IRQ_BASE,
+	.eic_base = SPRD_SC2720_EIC_BASE,
+	.num_irqs = SPRD_SC2720_IRQ_NUMS,
+};
+
 static const struct sprd_pmic_data sc2721_data = {
 	.slave_id = SPRD_SC2721_SLAVE_ID,
 	.irq_base = SPRD_SC2721_IRQ_BASE,
@@ -400,6 +411,7 @@ static int sprd_pmic_resume(struct device *dev)
 static SIMPLE_DEV_PM_OPS(sprd_pmic_pm_ops, sprd_pmic_suspend, sprd_pmic_resume);
 
 static const struct of_device_id sprd_pmic_match[] = {
+	{ .compatible = "sprd,sc2720", .data = &sc2720_data },
 	{ .compatible = "sprd,sc2721", .data = &sc2721_data },
 	{ .compatible = "sprd,sc2731", .data = &sc2731_data },
 	{ .compatible = "sprd,sc2730", .data = &sc2730_data },
@@ -411,6 +423,7 @@ static const struct of_device_id sprd_pmic_match[] = {
 MODULE_DEVICE_TABLE(of, sprd_pmic_match);
 
 static const struct spi_device_id sprd_pmic_spi_ids[] = {
+	{ .name = "sc2720", .driver_data = (unsigned long)&sc2720_data },
 	{ .name = "sc2721", .driver_data = (unsigned long)&sc2721_data },
 	{ .name = "sc2731", .driver_data = (unsigned long)&sc2731_data },
 	{ .name = "sc2730", .driver_data = (unsigned long)&sc2730_data },
