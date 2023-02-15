b82288c7970c70bea8143acddc07398129a7e29f
Enlin Mu <enlin.mu@unisoc.com>
2023-02-15 16:44:01 +0800

    Bug #2104353 arm: add hardinfo for arm32
    [root cause  ]: na
    [changes     ]:na
    [side effects]:na
    [self test   ]:na
    [download normally]:na
    [power on/off normally]:na
    [do common repository/branch inspection]:na
    [is there dependence]:na
    [confirm dependent commit]:na
    [board]:arm32
    [test case]:na
    [reviewers]:Hao_hao.Wang
    
    commit_template_version:v1
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: I5e8ac4dab2917675e6ac8cbff91da7a463ec4a72
    Signed-off-by: Enlin Mu <enlin.mu@unisoc.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -1319,7 +1319,11 @@ static int c_show(struct seq_file *m, void *v)
 		seq_printf(m, "CPU revision\t: %d\n\n", cpuid & 15);
 	}
 
-	seq_printf(m, "Hardware\t: %s\n", machine_name);
+	if (of_flat_dt_get_cpuinfo_hw())
+		seq_printf(m, "Hardware\t: %s\n", of_flat_dt_get_cpuinfo_hw());
+	else
+		seq_printf(m, "Hardware\t: %s\n", machine_name);
+
 	seq_printf(m, "Revision\t: %04x\n", system_rev);
 	seq_printf(m, "Serial\t\t: %s\n", system_serial);
 
