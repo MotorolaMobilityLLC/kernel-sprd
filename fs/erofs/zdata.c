ce80623f0d334aa41e9fad9e941e4059b2125324
xiaohui10 <xiaohui10@longcheer.com>
2023-04-14 00:15:18 +0800

    [issue][frameworks/base][CR]IO-EROFS [rootcause]: IO-EROFS [solution]: IO-EROFS [sideeffect]: None
    Change-Id: Ie56c4793bda4cf8210250b44e77c8a3098c8f86a
    (cherry picked from commit ae2d1280b913eb3b07c8e5ddb96040fdb877167b)

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -8,6 +8,8 @@
 #include "compress.h"
 #include <linux/prefetch.h>
 
+#include <linux/cpuhotplug.h>
+#include <uapi/linux/sched/types.h>
 #include <trace/events/erofs.h>
 
 /*
@@ -34,24 +36,134 @@ typedef tagptr1_t compressed_page_t;
 static struct workqueue_struct *z_erofs_workqueue __read_mostly;
 static struct kmem_cache *pcluster_cachep __read_mostly;
 
-void z_erofs_exit_zip_subsystem(void)
+#ifdef CONFIG_EROFS_FS_PCPU_KTHREAD
+static struct kthread_worker __rcu **z_erofs_pcpu_workers;
+
+static void erofs_destroy_percpu_workers(void)
 {
-	destroy_workqueue(z_erofs_workqueue);
-	kmem_cache_destroy(pcluster_cachep);
+	struct kthread_worker *worker;
+	unsigned int cpu;
+
+	for_each_possible_cpu(cpu) {
+		worker = rcu_dereference_protected(
+					z_erofs_pcpu_workers[cpu], 1);
+		rcu_assign_pointer(z_erofs_pcpu_workers[cpu], NULL);
+		if (worker)
+			kthread_destroy_worker(worker);
+	}
+	kfree(z_erofs_pcpu_workers);
 }
 
-static inline int z_erofs_init_workqueue(void)
+static struct kthread_worker *erofs_init_percpu_worker(int cpu)
 {
-	const unsigned int onlinecpus = num_possible_cpus();
-	const unsigned int flags = WQ_UNBOUND | WQ_HIGHPRI | WQ_CPU_INTENSIVE;
+	struct kthread_worker *worker =
+		kthread_create_worker_on_cpu(cpu, 0, "erofs_worker/%u", cpu);
 
-	/*
-	 * no need to spawn too many threads, limiting threads could minimum
-	 * scheduling overhead, perhaps per-CPU threads should be better?
-	 */
-	z_erofs_workqueue = alloc_workqueue("erofs_unzipd", flags,
-					    onlinecpus + onlinecpus / 4);
-	return z_erofs_workqueue ? 0 : -ENOMEM;
+	if (IS_ERR(worker))
+		return worker;
+	if (IS_ENABLED(CONFIG_EROFS_FS_PCPU_KTHREAD_HIPRI)) {
+		struct sched_param sp = { .sched_priority = 1 };
+		WARN_ON_ONCE(sched_setscheduler_nocheck(worker->task, SCHED_FIFO, &sp) != 0);
+	} else {
+		struct sched_attr attr = {
+			.sched_policy = SCHED_NORMAL,
+			.sched_nice = 0,
+		};
+		WARN_ON_ONCE(sched_setattr_nocheck(worker->task, &attr) != 0);
+	}
+	return worker;
+}
+
+static int erofs_init_percpu_workers(void)
+{
+	struct kthread_worker *worker;
+	unsigned int cpu;
+
+	z_erofs_pcpu_workers = kcalloc(num_possible_cpus(),
+			sizeof(struct kthread_worker *), GFP_ATOMIC);
+	if (!z_erofs_pcpu_workers)
+		return -ENOMEM;
+
+	for_each_online_cpu(cpu) {	/* could miss cpu{off,on}line? */
+		worker = erofs_init_percpu_worker(cpu);
+		if (!IS_ERR(worker))
+			rcu_assign_pointer(z_erofs_pcpu_workers[cpu], worker);
+	}
+	return 0;
+}
+#else
+static inline void erofs_destroy_percpu_workers(void) {}
+static inline int erofs_init_percpu_workers(void) { return 0; }
+#endif
+
+#if defined(CONFIG_HOTPLUG_CPU) && defined(CONFIG_EROFS_FS_PCPU_KTHREAD)
+static DEFINE_SPINLOCK(z_erofs_pcpu_worker_lock);
+static enum cpuhp_state erofs_cpuhp_state;
+
+static int erofs_cpu_online(unsigned int cpu)
+{
+	struct kthread_worker *worker, *old;
+
+	worker = erofs_init_percpu_worker(cpu);
+	if (IS_ERR(worker))
+		return PTR_ERR(worker);
+
+	spin_lock(&z_erofs_pcpu_worker_lock);
+	old = rcu_dereference_protected(z_erofs_pcpu_workers[cpu],
+			lockdep_is_held(&z_erofs_pcpu_worker_lock));
+	if (!old)
+		rcu_assign_pointer(z_erofs_pcpu_workers[cpu], worker);
+	spin_unlock(&z_erofs_pcpu_worker_lock);
+	if (old)
+		kthread_destroy_worker(worker);
+	return 0;
+}
+
+static int erofs_cpu_offline(unsigned int cpu)
+{
+	struct kthread_worker *worker;
+
+	spin_lock(&z_erofs_pcpu_worker_lock);
+	worker = rcu_dereference_protected(z_erofs_pcpu_workers[cpu],
+			lockdep_is_held(&z_erofs_pcpu_worker_lock));
+	rcu_assign_pointer(z_erofs_pcpu_workers[cpu], NULL);
+	spin_unlock(&z_erofs_pcpu_worker_lock);
+
+	synchronize_rcu();
+	if (worker)
+		kthread_destroy_worker(worker);
+	return 0;
+}
+
+static int erofs_cpu_hotplug_init(void)
+{
+	int state;
+
+	state = cpuhp_setup_state_nocalls(CPUHP_AP_ONLINE_DYN,
+			"fs/erofs:online", erofs_cpu_online, erofs_cpu_offline);
+	if (state < 0)
+		return state;
+
+	erofs_cpuhp_state = state;
+	return 0;
+}
+
+static void erofs_cpu_hotplug_destroy(void)
+{
+	if (erofs_cpuhp_state)
+		cpuhp_remove_state_nocalls(erofs_cpuhp_state);
+}
+#else /* !CONFIG_HOTPLUG_CPU || !CONFIG_EROFS_FS_PCPU_KTHREAD */
+static inline int erofs_cpu_hotplug_init(void) { return 0; }
+static inline void erofs_cpu_hotplug_destroy(void) {}
+#endif
+
+void z_erofs_exit_zip_subsystem(void)
+{
+	erofs_cpu_hotplug_destroy();
+	erofs_destroy_percpu_workers();
+	destroy_workqueue(z_erofs_workqueue);
+	kmem_cache_destroy(pcluster_cachep);
 }
 
 static void z_erofs_pcluster_init_once(void *ptr)
@@ -79,17 +191,41 @@ static void z_erofs_pcluster_init_always(struct z_erofs_pcluster *pcl)
 
 int __init z_erofs_init_zip_subsystem(void)
 {
+	int err;
+
 	pcluster_cachep = kmem_cache_create("erofs_compress",
 					    Z_EROFS_WORKGROUP_SIZE, 0,
 					    SLAB_RECLAIM_ACCOUNT,
 					    z_erofs_pcluster_init_once);
-	if (pcluster_cachep) {
-		if (!z_erofs_init_workqueue())
-			return 0;
-
-		kmem_cache_destroy(pcluster_cachep);
+	if (pcluster_cachep == NULL) {
+		err = -ENOMEM;
+		goto out_error_cache_create;
 	}
-	return -ENOMEM;
+
+	z_erofs_workqueue = alloc_workqueue("erofs_worker",
+			WQ_UNBOUND | WQ_HIGHPRI, num_possible_cpus());
+	if (!z_erofs_workqueue) {
+		err = -ENOMEM;
+		goto out_error_workqueue_init;
+	}
+
+	err = erofs_init_percpu_workers();
+	if (err)
+		goto out_error_pcpu_worker;
+
+	err = erofs_cpu_hotplug_init();
+	if (err < 0)
+		goto out_error_cpuhp_init;
+	return err;
+
+out_error_cpuhp_init:
+	erofs_destroy_percpu_workers();
+out_error_pcpu_worker:
+	destroy_workqueue(z_erofs_workqueue);
+out_error_workqueue_init:
+	kmem_cache_destroy(pcluster_cachep);
+out_error_cache_create:
+	return err;
 }
 
 enum z_erofs_collectmode {
@@ -697,6 +833,14 @@ err_out:
 	goto out;
 }
 
+static void z_erofs_vle_unzip_wq(struct work_struct *work);
+#ifdef CONFIG_EROFS_FS_PCPU_KTHREAD
+static void z_erofs_decompressqueue_kthread_work(struct kthread_work *work)
+{
+	z_erofs_vle_unzip_wq((struct work_struct *)work);
+}
+#endif
+
 static void z_erofs_vle_unzip_kickoff(void *ptr, int bios)
 {
 	tagptr1_t t = tagptr_init(tagptr1_t, ptr);
@@ -707,8 +851,29 @@ static void z_erofs_vle_unzip_kickoff(void *ptr, int bios)
 	if (!atomic_add_return(bios, &io->pending_bios)) {
 		if (!background)
 			complete(&io->u.done);
-		else
-			queue_work(z_erofs_workqueue, &io->u.work);
+		else {
+			/* Use workqueue and sync decompression for atomic contexts only */
+			if (in_atomic() || irqs_disabled()) {
+#ifdef CONFIG_EROFS_FS_PCPU_KTHREAD
+				struct kthread_worker *worker;
+
+				rcu_read_lock();
+				worker = rcu_dereference(
+					z_erofs_pcpu_workers[raw_smp_processor_id()]);
+				if (!worker) {
+					INIT_WORK(&io->u.work, z_erofs_vle_unzip_wq);
+					queue_work(z_erofs_workqueue, &io->u.work);
+				} else {
+					kthread_queue_work(worker, &io->u.kthread_work);
+				}
+				rcu_read_unlock();
+#else
+				queue_work(z_erofs_workqueue, &io->u.work);
+#endif
+				return;
+			}
+			z_erofs_vle_unzip_wq(&io->u.work);
+		}
 	}
 }
 
@@ -1137,7 +1302,13 @@ static struct z_erofs_unzip_io *jobqueue_init(struct super_block *sb,
 	/* initialize fields in the allocated descriptor */
 	io = &iosb->io;
 	iosb->sb = sb;
+#ifdef CONFIG_EROFS_FS_PCPU_KTHREAD
+	kthread_init_work(&io->u.kthread_work,
+		z_erofs_decompressqueue_kthread_work);
+#else
 	INIT_WORK(&io->u.work, z_erofs_vle_unzip_wq);
+#endif
+
 out:
 	io->head = Z_EROFS_PCLUSTER_TAIL_CLOSED;
 	return io;
