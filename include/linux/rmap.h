6bb31e6120e37f115d0ac16966057c5d4561c0a9
Lee Jones <joneslee@google.com>
2023-04-18 10:35:38 +0100

    Bug #2289377 Revert "Revert "mm/rmap: Fix anon_vma->degree ambiguity leading to double-reuse""
    [CVE] CVE-2022-42703
    [SPL] 2023-07-05
    
    This reverts commit 4f35cec76058557d9eaec0d501d03c7657eb56b4 and does so
    in an abi-safe way.
    
    This is done by adding the new fields only to the end of the structure
    and this structure is only passed around to other functions as a
    pointer, the internal structure layout is only touched by the core
    kernel, so adding it to the end is safe.
    
    ABI differences manually updated:
    
    Leaf changes summary: 1 artifact changed
    Changed leaf types summary: 1 leaf type changed
    Removed/Changed/Added functions summary: 0 Removed, 0 Changed, 0 Added function
    Removed/Changed/Added variables summary: 0 Removed, 0 Changed, 0 Added variable
    
    'struct anon_vma at rmap.h:29:1' changed:
      type size changed from 704 to 832 (in bits)
      2 data member insertions:
        'unsigned long int num_children', at offset 704 (in bits) at rmap.h:70:1
        'unsigned long int num_active_vmas', at offset 768 (in bits) at rmap.h:72:1
      3493 impacted interfaces
    
    Bug: 260678056
    Bug: 253167854
    Change-Id: Ib1d45625cbc2e0b21330ca3dc2aa7aff34666d31
    Signed-off-by: Lee Jones <joneslee@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    (cherry picked from commit d3e1a50cba092fa9c56fc642ee74f360c4b40a17)
    (cherry picked from commit 5f5e093bbbdb788c3181a7bfd0a53fc21e8fdd1c)

--- include/linux/rmap.h
+++ include/linux/rmap.h
@@ -43,14 +43,6 @@ struct anon_vma {
 	 */
 	atomic_t refcount;
 
-	/*
-	 * Count of child anon_vmas and VMAs which points to this anon_vma.
-	 *
-	 * This counter is used for making decision about reusing anon_vma
-	 * instead of forking new one. See comments in function anon_vma_clone.
-	 */
-	unsigned degree;
-
 	struct anon_vma *parent;	/* Parent of this anon_vma */
 
 	/*
@@ -64,6 +56,18 @@ struct anon_vma {
 
 	/* Interval tree of private "related" vmas */
 	struct rb_root_cached rb_root;
+
+	/*
+	 * Count of child anon_vmas. Equals to the count of all anon_vmas that
+	 * have ->parent pointing to this one, including itself.
+	 *
+	 * This counter is used for making decision about reusing anon_vma
+	 * instead of forking new one. See comments in function anon_vma_clone.
+	 */
+	unsigned long num_children;
+	/* Count of VMAs whose ->anon_vma pointer points to this object. */
+	unsigned long num_active_vmas;
+
 };
 
 /*
