e94ebc8b9e5ef8d8c048dad2360642abc994ae8a
Feng Sun <feng.sun@unisoc.com>
2023-06-13 21:05:04 +0800

    Bug #2273247 unisoc_platform: sprd_hang_trigger.c merge into wdf.c
    [root cause  ]:ko merge 
    [changes     ]:delete sprd_hang_trigger.c,merge into wdf.c
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:all
    [test case]:no
    [reviewers]:hao_hao.wang
    
    commit_template_version:v1
    
    Signed-off-by: Feng Sun <feng.sun@unisoc.com>
    [change_type ] bringup --> temporary
    [tag_product ] common
    
    Change-Id: I8c3476dbf4539ac6aca2e72f1eaf4f54c7822c55

--- drivers/unisoc_platform/hang_debug/aphang.h
+++ drivers/unisoc_platform/hang_debug/aphang.h
@@ -1,16 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __SPRD_HANG_DEBUG_H__
-
 extern int sprd_wdh_atf_init(void);
-#if IS_ENABLED(CONFIG_SPRD_HANG_TRIGGER)
-extern int send_ipi_init(void);
-extern void send_ipi_exit(void);
-#else
-static inline int send_ipi_init(void)
-{
-	return 0;
-}
-static inline void send_ipi_exit(void) {}
-#endif
-
 #endif  /* __SPRD_HANG_DEBUG_H__ */
--- drivers/unisoc_platform/hang_debug/sprd_wdf.c
+++ drivers/unisoc_platform/hang_debug/sprd_wdf.c
@@ -22,6 +22,12 @@
 #include <linux/seq_file.h>
 #include <linux/panic_notifier.h>
 #include <linux/export.h>
+#include <linux/fs.h>
+#include <linux/seq_file.h>
+#include <linux/uaccess.h>
+#include <linux/wait.h>
+#include <linux/hrtimer.h>
+#include <linux/delay.h>
 #include "aphang.h"
 
 #if IS_ENABLED(CONFIG_SPRD_WATCHDOG_FIQ)
@@ -30,6 +36,7 @@
 
 #undef pr_fmt
 #define pr_fmt(fmt) "sprd_wdf: " fmt
+#define INV_CPUS 256
 
 static DEFINE_PER_CPU(struct task_struct *, hang_debug_task_store);
 unsigned int cpu_feed_mask;
@@ -50,6 +57,12 @@ static u64 g_interval = 8;
 static u64 g_timeout = 40;
 static u64 g_pretimeout = 20;
 static int wdt_disable;
+
+static DEFINE_SPINLOCK(thread_lock);
+static struct task_struct *pthread;
+wait_queue_head_t waitq;
+static unsigned int thread_cpu = INV_CPUS;
+static unsigned int thread_state;
 
 static enum hrtimer_restart sprd_wdt_timer_func(struct hrtimer *hrtimer)
 {
@@ -335,6 +348,185 @@ static const struct proc_ops wdt_disable_proc_fops = {
 	.proc_lseek = seq_lseek,
 	.proc_release = single_release,
 };
+
+static int loop_thread_func(void *data)
+{
+	unsigned long flags;
+
+	while (!kthread_should_stop()) {
+		wait_event_interruptible(waitq,
+			thread_state == 1 || kthread_should_stop());
+
+		if (kthread_should_stop())
+			break;
+
+		spin_lock_irqsave(&thread_lock, flags);
+		while (1)
+			cpu_relax();
+		spin_unlock_irqrestore(&thread_lock, flags);
+
+		/* impossible to come here */
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+	}
+	return 0;
+}
+
+static ssize_t thread_read(struct file *file, char __user *user_buf,
+                                 size_t count, loff_t *ppos)
+{
+	char buf[5];
+	size_t len;
+
+	len = sprintf(buf, "%u\n", thread_cpu);
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static ssize_t thread_write(struct file *file,
+			    const char __user *user_buf, size_t count, loff_t *ppos)
+{
+	int ret;
+	unsigned long input;
+
+	if (*ppos < 0)
+		return -EINVAL;
+
+	if (count == 0)
+		return 0;
+
+	 if (*ppos != 0)
+		return 0;
+
+	ret = kstrtoul_from_user(user_buf, count, 10, &input);
+	if (ret)
+		return -EINVAL;
+
+	if (input == thread_cpu)
+		return count;
+
+	if (input > nr_cpu_ids)
+		return -EINVAL;
+
+	/* stop bounded thread on previous cpu*/
+	if (thread_cpu != INV_CPUS && pthread) {
+		kthread_stop(pthread);
+		pthread = NULL;
+	}
+
+	thread_cpu = input;
+
+	if (!pthread && cpu_online(thread_cpu)) {
+		pthread = kthread_create_on_node(loop_thread_func, NULL,
+					cpu_to_node(thread_cpu),
+					"loop_thread/%d", thread_cpu);
+		if (IS_ERR(pthread)) {
+			pr_err("%s: Create thread on %d cpu fail\n",
+				__func__, thread_cpu);
+			return PTR_ERR(pthread);
+		}
+		/* bind thread to the cpu */
+		kthread_bind(pthread, thread_cpu);
+		wake_up_process(pthread);
+	}
+	return count;
+}
+
+static ssize_t thread_trigger_read(struct file *file, char __user *user_buf,
+			     size_t count, loff_t *ppos)
+{
+	char buf[5];
+	size_t len;
+
+	len = sprintf(buf, "%u\n", thread_state);
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static ssize_t thread_trigger_write(struct file *file,
+				const char __user *user_buf, size_t count,
+				loff_t *ppos)
+{
+	int ret;
+	unsigned long input;
+
+	if (*ppos < 0)
+		return -EINVAL;
+
+	if (count == 0)
+		return 0;
+
+	if (*ppos != 0)
+		return 0;
+
+	ret = kstrtoul_from_user(user_buf, count, 10, &input);
+	if (ret)
+		return -EINVAL;
+
+	if (input == thread_state)
+		return count;
+
+	if (input > 2)
+		return -EINVAL;
+
+	thread_state = input;
+	if (input == 1)
+	wake_up_interruptible(&waitq);
+
+	return count;
+}
+
+static const struct proc_ops thread_fops = {
+	.proc_open      = simple_open,
+	.proc_read      = thread_read,
+	.proc_write     = thread_write,
+	.proc_lseek     = default_llseek,
+};
+
+static const struct proc_ops thread_trigger_fops = {
+	.proc_open      = simple_open,
+	.proc_read      = thread_trigger_read,
+	.proc_write     = thread_trigger_write,
+	.proc_lseek     = default_llseek,
+};
+
+#if IS_ENABLED(CONFIG_SPRD_HANG_TRIGGER)
+static int __init send_ipi_init(void)
+{
+	int ret = 0;
+
+	if (!proc_create("thread_cpu", 0660, NULL, &thread_fops)) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	if (!proc_create("thread_trigger", 0660, NULL, &thread_trigger_fops)) {
+		ret = -ENOMEM;
+		goto err_trigger;
+	}
+
+	init_waitqueue_head(&waitq);
+
+	return ret;
+
+err_trigger:
+	remove_proc_entry("thread_cpu", NULL);
+err:
+	return -ENOMEM;
+}
+
+static void __exit send_ipi_exit(void)
+{
+	remove_proc_entry("thread_cpu", NULL);
+	remove_proc_entry("thread_trigger", NULL);
+	if (pthread)
+		kthread_stop(pthread);
+}
+#else
+static int send_ipi_init(void)
+{
+        return 0;
+}
+static void send_ipi_exit(void) {}
+#endif
 
 static int wdf_panic_event(struct notifier_block *self,
 					unsigned long val, void *reason)
