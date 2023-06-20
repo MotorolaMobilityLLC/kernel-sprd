ef0c5f33d218280f8db85360dadb233a2a55ccc1
Xiaosong Ma <xiaosong.ma@unisoc.com>
2023-06-20 11:40:06 +0800

    Bug #2282908 unisoc_platform: mm: change direct_swappiness when direct reclaim path
    [root cause  ]: unisoc enhance reclaim
    [changes     ]:direct_swappiness.c
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:all
    [test case]:yes
    [reviewers]:yuming.han
    
    commit_template_version:v1
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: I38d607d44dedc138fa775b79acb040404b8f605f
    Signed-off-by: Xiaosong Ma <xiaosong.ma@unisoc.com>

--- drivers/unisoc_platform/mm/reclaim/direct_swappiness.c
+++ drivers/unisoc_platform/mm/reclaim/direct_swappiness.c
@@ -7,10 +7,38 @@
 #include <linux/types.h>
 #include <trace/hooks/vmscan.h>
 #include <linux/swap.h>
+#include <linux/sysctl.h>
+
+static int two_hundred = 200;
+int sysctl_direct_swappiness = 60;
+EXPORT_SYMBOL_GPL(sysctl_direct_swappiness);
+
+struct ctl_table mm_table[] = {
+	{
+		.procname	= "direct_swappiness",
+		.data		= &sysctl_direct_swappiness,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= &two_hundred,
+	},
+	{ },
+};
+
+struct ctl_table sys_mm_table[] = {
+	{
+		.procname	= "unisoc_mm",
+		.mode		= 0555,
+		.child		= mm_table,
+	},
+	{ },
+};
 
 static void tune_swappiness(void *data, int *swappiness)
 {
-
+	if (!current_is_kswapd())
+		*swappiness = sysctl_direct_swappiness;
 }
 
 void register_direct_swappiness_vendor_hooks(void)
@@ -25,7 +53,13 @@ void unregister_direct_swappiness_vendor_hooks(void)
 
 int unisoc_enhance_reclaim_init(void)
 {
+	struct ctl_table_header *hdr;
+
 	register_direct_swappiness_vendor_hooks();
+	hdr = register_sysctl_table(sys_mm_table);
+
+	if (unlikely(!hdr))
+		pr_info("failed register_sysctl_table to /proc/sys/unisoc_mm/direct_swappiness!\n");
 
 	pr_info("UNISOC enhance reclaim init succeed!\n");
 	return 0;
