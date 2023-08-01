7f39a132523ecb81aab403d596c0d6185f78785b
Mi Li <mi.li@unisoc.com>
2023-08-01 18:46:19 +0800

    Bug #2186071 add license to unisoc file whose header is empty. SKIP ME ALWAYS
    [root cause  ]: header is empty
    [changes     ]:.c,.h,.dts,.dtsi
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sprd
    [test case]:no
    [reviewers]:orson
    
    commit_template_version:v1
    
    Change-Id: I9dd0da53f802f35b5fd6b60b606e16b39bec1575
    Signed-off-by: Mi Li <mi.li@unisoc.com>

--- drivers/power/supply/rx1619.h
+++ drivers/power/supply/rx1619.h
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * rx1619.h - Unisoc platform header
+ *
+ * Copyright 2022 Unisoc(Shanghai) Technologies Co.Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 const u8 fw_data_tx[13308] = {
 	0x2f, 0xb1, 0xff, 0xff, 0xaf, 0xb0, 0xff, 0xff,
 	0xaf, 0xb0, 0xff, 0xff, 0xaf, 0xb0, 0xff, 0xff,
--- include/dt-bindings/soc/sprd,pike2-regs.h
+++ include/dt-bindings/soc/sprd,pike2-regs.h
@@ -1,3 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * sprd,pike2-regs.h - Unisoc platform header
+ *
+ * Copyright 2022 Unisoc(Shanghai) Technologies Co.Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #define REG_GPU_APB_APB_RST                                                           0x0000
 #define REG_GPU_APB_APB_CLK_CTRL                                                      0x0004
 #define REG_GPU_APB_APB_T820_INT_STS                                                  0x0008
