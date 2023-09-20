d92ffa9d4c81c05dfb14a70b042c158fc1ec76f0
Ziyang Wang <ziyang.wang@unisoc.com>
2023-09-20 15:57:43 +0800

    Bug #2416500 unisoc_platform: net: sipa_dummy.c code review found resource leaks
    [root cause  ]:found resource leaks
    [changes     ]:resource leaks handled
    [side effects]:na
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:n6l
    [test case]:sipa
    [reviewers]:wade.shu
    
    commit_template_version:v1
    
    Signed-off-by: Ziyang Wang <ziyang.wang@unisoc.com>
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I0883a38b89c3e1abdfd432f41aad1726341eb9f8

--- drivers/unisoc_platform/net/sipa_v3/sipa_dummy.c
+++ drivers/unisoc_platform/net/sipa_v3/sipa_dummy.c
@@ -985,6 +985,11 @@ static void sipa_dummy_debugfs_init(void)
 		pr_err("failed to create debugfs dir\n");
 }
 
+static void sipa_dummy_debugfs_remove(void)
+{
+	debugfs_remove_recursive(dummy_debugfs_root);
+}
+
 #if NO_CONNTRACK
 static int
 sipa_ctnetlink_conntrack_event(unsigned int events,
@@ -1066,6 +1071,11 @@ EXPORT_SYMBOL_GPL(sipa_dummy_init);
 void sipa_dummy_exit(void)
 {
 	sipa_dummy_unreg_netdev_notifier();
+	sipa_dummy_debugfs_remove();
+	dev_close(dummy_dev);
+	unregister_netdev(dummy_dev);
+	free_netdev(dummy_dev);
+	dummy_dev = NULL;
 }
 EXPORT_SYMBOL_GPL(sipa_dummy_exit);
 
