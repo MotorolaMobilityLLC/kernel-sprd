b0ba259817e9e9787a7783466f8594c60f9b1f66
chenchangan <chenchangan@longcheer.com>
2023-11-03 21:18:50 +0800

    [issue][zram][EKissue-7]ramboost for test 1
    Change-Id: I69ce4575aa5a591b80f5f6e471d277b0f895ad86

---
+++ drivers/moto_ko/moto_swap/hybridswap/hybridswap_akcompress.c
@@ -0,0 +1,9 @@
+#include "hybridswap.h"
+#include <linux/kernel.h>
+
+
+void ramboost_test_01(void)
+{
+    printk("this test in %s\n", __func__);
+    return;
+}
---
+++ drivers/moto_ko/moto_swap/zram-5.15/zram_drv.c
@@ -0,0 +1,8 @@
+#include <linux/kernel.h>
+#include "zram_drv.h"
+
+void ramboost_test_05(void)
+{
+    printk("this test in %s\n", __func__);
+    return;
+}
