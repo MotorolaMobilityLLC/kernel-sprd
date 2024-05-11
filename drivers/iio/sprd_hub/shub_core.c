e225e48f84ac16806d5c89d0a581e19846d24cb0
Tianmin Yang <tianmin.yang@unisoc.com>
2024-05-11 15:45:17 +0800

    Bug #2651153 iio: sprd_hub: return err num to user when init are not fully finished
    [root cause  ]: can not read complete info in boot sometime
    [changes     ]: return err to user and let user read again
    [side effects]: no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sensorhub board
    [test case]: boot test
    [reviewers]: hongliang.shen
    
    return err to user space when init are not ready.
    
    Change-Id: I8862d1fcba9ce781226ebaa05dc31f63f31f20b1
    Signed-off-by: Tianmin Yang <tianmin.yang@unisoc.com>

--- drivers/iio/sprd_hub/shub_core.c
+++ drivers/iio/sprd_hub/shub_core.c
@@ -855,7 +855,7 @@ static ssize_t sensorlist_show(struct device *dev,
 				struct device_attribute *attr, char *buf)
 {
 	int err;
-	u32 number = 0;
+	int number = 0;
 	u32 len = 0;
 	u8 data[4];
 	u8 *sensorlist = NULL;
@@ -867,6 +867,14 @@ static ssize_t sensorlist_show(struct device *dev,
 	err = shub_sipc_read(sensor, SHUB_GET_PHYSICAL_SENSOR_NUMBER_SUBTYPE, data, sizeof(number));
 	if (err >= 0) {
 		memcpy(&number, data, sizeof(number));
+		dev_info(&sensor->sensor_pdev->dev, "sensor number = %d\n", number);
+	} else {
+		dev_info(&sensor->sensor_pdev->dev,
+				"sipc read sensorlist support number fail, err = %d\n", err);
+		return err;
+	}
+
+	if (number >= 0) {
 		len = number * sizeof(struct sensor_info_t);
 		sensorlist = kzalloc(len, GFP_KERNEL);
 		if (!sensorlist)
@@ -886,9 +894,8 @@ static ssize_t sensorlist_show(struct device *dev,
 			return err;
 		}
 	} else {
-		dev_info(&sensor->sensor_pdev->dev,
-				"sipc read sensorlist support number fail, err = %d\n", err);
-		return err;
+		memcpy(buf, &number, sizeof(number));
+		return 0;
 	}
 
 	if (len > MAX_STRING_SIZE) {
