6fe60660134d297910e2a6f3e8fb1a31ac0e3f81
chenchangan <chenchangan@longcheer.com>
2024-08-07 20:40:57 +0800

    [issue][usb][EKissue-11416]merge unisoc common patch:PATCH20008387,2024-7_8_2702411
    [rootcause]:In order to reduce the power consumption of other modules (such as CPHY) in AVDD1V8, A14 modified the mechanism of AVDD1V8 force on, but only adapted to L5P, which resulted in the AVDD1V8 force on of L6/N6L not being turned on on on the eMMC machine (UFS itself turned on AVDD1V8 force on the UFS machine), causing the eMMC machine to be unable to generate a suspend signal on high speed devices, and the AP was immediately awakened after entering suspend.
    [solution]:In the low-power scenario, it is necessary to turn on all avdd1v8 and its associated electricity
    [sideeffect]: None
    [other]: None
    
    Change-Id: Id98b8eeabe0ce56ea0fe4c3ec2b78241d89dd0eb
    Signed-off-by: chenchangan <chenchangan@longcheer.com>
    (cherry picked from commit cd05862d3b12c05191231874b779beeb129a2b22)

--- drivers/usb/musb/musb_sprd.c
+++ drivers/usb/musb/musb_sprd.c
@@ -1101,6 +1101,7 @@ static bool musb_sprd_lowpower_configuration_onoff(struct sprd_glue *glue, int o
 
 		/* switch hclk to suspend clk source */
 		clk_set_parent(glue->hclk_src_sel, glue->hclk_suspend_src);
+		usb_phy_set_wakeup(glue->xceiv, 1);
 	} else {
 		/* cancel pubsys deep bypass */
 		regmap_read(glue->pubsys_bypass.regmap_ptr,
@@ -1117,6 +1118,7 @@ static bool musb_sprd_lowpower_configuration_onoff(struct sprd_glue *glue, int o
 
 		/* switch hclk to default clk */
 		clk_set_parent(glue->hclk_src_sel, glue->hclk_default_src);
+		usb_phy_set_wakeup(glue->xceiv, 0);
 	}
 
 	return true;
