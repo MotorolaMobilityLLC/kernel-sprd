f6182dc8c18bf1ade7263340772ea2cc12798898
weikang.tang <weikang.tang@chino-e.com>
2025-10-27 15:35:03 +0800

    EKmoto_g35V-3945:bsp:Handle the memory stampede issue of moto_g35v dwc3
    Change-Id: I03ef46f5d2b987411c5deb99c8fdc7d011efbac4
    Reviewed-on: https://gerrit-odm.mot.com/c/home/repo/dev/platform/android/unisoc/bsp/kernel5.15/kernel5.15/+/33650
    Tested-by: weikang tang
    Reviewed-by: zhigang he <zhigang.he@chino-e.com>
    Reviewed-by: weikang tang

--- drivers/usb/dwc3/sprd/gadget.c
+++ drivers/usb/dwc3/sprd/gadget.c
@@ -2493,7 +2493,14 @@ static int dwc3_gadget_pullup(struct usb_gadget *g, int is_on)
 	int			ret;
 
 	is_on = !!is_on;
-	dwc->softconnect = is_on;
+	spin_lock_irqsave(&dwc->lock, flags);
+       if (dwc->softconnect != is_on)
+               dwc->softconnect = is_on;
+	else {
+               spin_unlock_irqrestore(&dwc->lock, flags);
+               return 0;
+       }
+       spin_unlock_irqrestore(&dwc->lock, flags);
 	/*
 	 * Per databook, when we want to stop the gadget, if a control transfer
 	 * is still in process, complete it and get the core into setup phase.
